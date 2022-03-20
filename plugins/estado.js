2ce595e6732438a07649d4be2d41b53c93ab55e2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-20 02:15:13 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/estado.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let picture = './Menu2.jpg'
+let estado =`
+*ミ💖 Hola estimado usuario 💖彡*
+
+*ミ🤖 Estado del Bot 🤖彡*
+*=> Bot activo ✔️*
+*=> Bot uso público ✔️*
+`.trim()
+
+conn.sendHydrated(m.chat, estado, 'The Shadow Borkers - Bot', picture, 'https://github.com/BrunoSobrino/ShadowBotV3', 'GitHub', null, null, [
+['MENÚ PRINCIPAL', '/menu'],
+['MENÚ SIMPLE', '/menusimple'],
+['MENÚ AUDIOS', '/menu2']
+], m)}
+
+handler.command = /^(estado|status|estate|state|stado|stats)$/i
+export default handler
