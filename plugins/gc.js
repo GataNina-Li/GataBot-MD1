b57715754fa6bd9b9dad654f00e9cda4ddfa7307
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-20 01:29:05 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/gc.js
+++ plugins/gc.js
@@ -1,6 +1,26 @@
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
-https://chat.whatsapp.com/GBZtMyNEnAx1hOWd0SMFU1
-`.trim(), wm, media, [['Back To Menu', '.menu']], m)
-handler.command = /^linkgc$/i
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
+*➤ Grupos oficiales del Bot:*
+*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+
+*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+
+*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
+
+*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
+
+*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
+
+*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa
+
+*7.-* https://chat.whatsapp.com/HylyPaLc49b9DWF67K8pdc
+
+*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGpX7rQIw
+
+*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYmKwp7
+
+*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSUZLM9
+`.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄', '.menu']], m)
+handler.command = /^linkgc|grupos$/i
 export default handler
