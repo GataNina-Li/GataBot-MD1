017695cd85d94e878419129c7992b7e8080d1cea
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-16 14:21:37 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-add2.js
+++ plugins/owner-add2.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
             attrs: {},
             content: [{ tag: 'participant', attrs: { jid } }]
         }))})
-  if (response[users] == 408) throw `*El numero se salio recientemente*\n*La unica manera de añadirlo es por medio del enlace del grupo. Usa ${usedPrefix}link para obtener el enlace*`
+  try {
     const pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
     const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
     const add = getBinaryNodeChild(response, 'add')
@@ -42,6 +42,9 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
         mentions: conn.parseMention(teks)
 })
 await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, await conn.getName(m.chat), 'Hey!! Hola, me presento, soy The Mystic - Bot, y soy un Bot para WhatsApp, una persona del grupo utilizo el comando para añadirte al grupo, pero no pude agregarte, asi que te mando la invitacion para que te agregues, te esperamos!!', jpegThumbnail)
+} catch (e) {
+console.error(e)
+throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙰𝙽̃𝙰𝙳𝙸𝚁 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝚀𝚄𝙴 𝙸𝙽𝙶𝚁𝙴𝚂𝙾, 𝙴𝚂𝚃𝙾 𝙿𝚄𝙴𝙳𝙴 𝙾𝙲𝚄𝚁𝚁𝙸𝚁 𝙿𝙾𝚁 𝚀𝚄𝙴 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝚂𝙰𝙻𝙸𝙾 𝚁𝙴𝙲𝙸𝙴𝙽𝚃𝙴𝙼𝙴𝙽𝚃𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙾 𝚃𝙸𝙴𝙽𝙴 𝙲𝙾𝙽𝙵𝚁𝙸𝙶𝚄𝚁𝙰𝙳𝙾 𝚂𝚄 𝙿𝚁𝙸𝚅𝙰𝙲𝙸𝙳𝙰𝙳 𝙿𝙰𝚁𝙰 𝙰𝙽̃𝙰𝙳𝙸𝚁𝙻𝙾 𝙰 𝙶𝚁𝚄𝙿𝙾𝚂, 𝚃𝙴 𝙰𝙲𝙾𝙽𝚂𝙴𝙹𝙰𝙼𝙾𝚂 𝙴𝙽𝚅𝙸𝙰𝙻𝙴 𝙻𝙰 𝙸𝙽𝚅𝙸𝚃𝙰𝙲𝙸𝙾𝙽 𝙼𝙰𝙽𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴!!*'}
 }
 handler.command = /^(agregar|añadir|\+)$/i
 handler.group = true
