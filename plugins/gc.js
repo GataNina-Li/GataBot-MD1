b6e52badfb42a0fcc53fc78347ea5c3c87ae62e2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-15 19:13:39 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/gc.js
+++ plugins/gc.js
@@ -1,4 +1,4 @@
-let media = './src/avatar_contact.png'
+let media = './Menu2.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
