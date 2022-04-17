84b708a95a614fe812cd80c498bcabb23d2550b9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-17 09:55:46 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/gc-kick.js
@@ -0,0 +1,17 @@
+import { areJidsSameUser } from '@adiwajshing/baileys'
+let handler = async (m, { conn, participants }) => {
+let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
+let kickedUser = []
+for (let user of users)
+if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
+const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+kickedUser.concat(res)
+await delay(1 * 1000)
+}}
+handler.help = ['kick @user']
+handler.tags = ['group']
+handler.command = /^(kick|sacar|\-)$/i
+handler.group = true
+handler.botAdmin = true
+export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
