c4efd229530f50e104e59e60c0e76fc668ee47a2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-17 09:19:58 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-add2.js
+++ plugins/owner-add2.js
@@ -4,7 +4,8 @@ import fetch from 'node-fetch'
  */
 const { getBinaryNodeChild, getBinaryNodeChildren } = (await import('@adiwajshing/baileys')).default
 let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
-  if (!text) throw! `*Ingrese un numero o corrobore que el numero ingresado este escrito correctamente y en formato internacional*\n*Ejemplo:*\n\n*${usedPrefix + command + ' ' + global.owner[0]}*`
+if (!text) throw! `*Ingrese un numero o corrobore que el numero ingresado este escrito correctamente y en formato internacional*\n*Ejemplo:*\n\n*${usedPrefix + command + ' ' + global.owner[0]}*`
+try {
   let _participants = participants.map(user => user.jid)
   let users = (await Promise.all(
     text.split(',')
@@ -26,7 +27,22 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
             tag: 'add',
             attrs: {},
             content: [{ tag: 'participant', attrs: { jid } }]
-}))})}
+}))})
+    const pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+    const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
+    const add = getBinaryNodeChild(response, 'add')
+    const participant = getBinaryNodeChildren(add, 'participant')
+    for (const user of participant.filter(item => item.attrs.error == 403)) {
+        const content = getBinaryNodeChild(user, 'add_request')
+        const invite_code = content.attrs.code
+        const invite_code_exp = content.attrs.expiration
+        let teks = `*No fue posible añadir a @${jid.split('@')[0]}*\n*Enviando invitacion a su privado...*`
+        m.reply(teks, null, {
+        mentions: conn.parseMention(teks)
+})}
+} catch (e) {
+throw m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙰𝙽̃𝙰𝙳𝙸𝚁 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝚀𝚄𝙴 𝙸𝙽𝙶𝚁𝙴𝚂𝙾, 𝙴𝚂𝚃𝙾 𝙿𝚄𝙴𝙳𝙴 𝙾𝙲𝚄𝚁𝚁𝙸𝚁 𝙿𝙾𝚁𝚀𝚄𝙴 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝚂𝙰𝙻𝙸𝙾 𝚁𝙴𝙲𝙸𝙴𝙽𝚃𝙴𝙼𝙴𝙽𝚃𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙾 𝚃𝙸𝙴𝙽𝙴 𝙲𝙾𝙽𝙵𝚁𝙸𝙶𝚄𝚁𝙰𝙳𝙾 𝚂𝚄 𝙿𝚁𝙸𝚅𝙰𝙲𝙸𝙳𝙰𝙳 𝙿𝙰𝚁𝙰 𝙰𝙽̃𝙰𝙳𝙸𝚁𝙻𝙾 𝙰 𝙶𝚁𝚄𝙿𝙾𝚂, 𝚃𝙴 𝙰𝙲𝙾𝙽𝚂𝙴𝙹𝙰𝙼𝙾𝚂 𝙴𝙽𝚅𝙸𝙰𝙻𝙴 𝙻𝙰 𝙸𝙽𝚅𝙸𝚃𝙰𝙲𝙸𝙾𝙽 𝙼𝙰𝙽𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴!!*')}
+}
 handler.command = /^(agregar|añadir|\+)$/i
 handler.group = true
 handler.admin = true
