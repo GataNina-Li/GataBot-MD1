dc3cdc7a35463d17f1f730251d97e75a8a3498ca
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-21 10:39:33 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -1,7 +1,7 @@
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, command }) => {
     let yh = global.loli
     let url = yh[Math.floor(Math.random() * yh.length)]
-    conn.sendButton(m.chat, 'Random Loli', 'Lolinya Kaak', url, [['/loli', 'Get Again']], m)
+    conn.sendButton(m.chat, `ʀᴀɴᴅᴏᴍ ʟᴏʟɪ - ɪѕ ᴄᴜᴛᴇ 🥺`.trim(), author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
   }
   handler.command = /^(loli)$/i
   handler.tags = ['anime']
