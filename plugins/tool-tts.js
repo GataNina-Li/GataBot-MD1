26f985b93c5ad66cbee201bcb808d954b728fa11
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-25 04:02:17 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/tool-tts.js
+++ plugins/tool-tts.js
@@ -2,7 +2,7 @@ import gtts from 'node-gtts'
 import { readFileSync, unlinkSync } from 'fs'
 import { join } from 'path'
 
-const defaultLang = 'id'
+const defaultLang = 'es'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
   let lang = args[0]
@@ -30,7 +30,7 @@ handler.command = /^g?tts$/i
 
 export default handler
 
-function tts(text, lang = 'id') {
+function tts(text, lang = 'es') {
   console.log(lang, text)
   return new Promise((resolve, reject) => {
     try {
