660c72acea725a94ddb52b0af6d0baa34edebb74
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-26 23:25:35 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,8 @@
 function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
+handler.command = /^(owner|creator|creador|propietario)$/i
 export default handler
