2c4a87bfacf1d223ed089406a59a725e9dac8b9f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-28 22:14:19 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,12 +1,12 @@
 import { mediafiredl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file*`
 let res = await mediafiredl(args[0])
 let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
 let caption = `
 *𝙽𝙾𝙼𝙱𝚁𝙴:* ${filename}
 *𝙿𝙴𝚂𝙾:* ${filesizeH}
-*TIPO:* ${ext}
+*𝚃𝙸𝙿𝙾:* ${ext}
 `.trim()
 m.reply(caption)
 conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
