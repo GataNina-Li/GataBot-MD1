3fb549cb2b978d45868639eead18f59db6d82486
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-30 18:33:46 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fun-apakah2.js
+++ plugins/fun-apakah2.js
@@ -1,12 +1,13 @@
 let handler = async (m, { command, text }) => m.reply(`
-*Pertanyaan:* ${command} ${text}
-*Jawaban:* ${['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin'].getRandom()}
-  `.trim(), null, m.mentionedJid ? {
-  mentions: m.mentionedJid
+*⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
+  
+*𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
+*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'].getRandom()}
+`.trim(), null, m.mentionedJid ? {
+mentions: m.mentionedJid
 } : {})
 
-handler.help = ['apakah <teks>?']
+handler.help = ['pregunta <texto>?']
 handler.tags = ['kerang']
-handler.command = /^apakah$/i
-
+handler.command = /^pregunta|preguntas|apakah$/i
 export default handler
