e51593371cc94feee17f7105c5c3ed1f93b15b8f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-30 22:17:47 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/maker-gay.js
+++ plugins/maker-gay.js
@@ -4,11 +4,11 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
 }), 'error.png', '*🏳️‍🌈 𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙶𝙰𝚈 🏳️‍🌈*', m)
-conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
+await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true })
 }
-handler.help = ['gay2']
+handler.help = ['mgay']
 handler.tags = ['maker']
-handler.command = /^(gay2)$/i
+handler.command = /^(mgay)$/i
 export default handler
