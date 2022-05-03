307df4e3bc2765407f73e5686fc8c69152e06abc
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-03 18:05:34 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,24 +1,22 @@
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  let { thumbnail, video, title } = await youtubedl(args[0])
-      .catch(async () => await youtubedlv2(args[0]))
-  let link = await video['360p'].download()
-  const isY = /y(es)/gi.test(args[1])
-  const limitedSize = (isPrems || isOwner ? 99 : 70) * 1024
-  let isLimit = limitedSize < video['360p'].fileSize
-  if (!isY) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-*📌Title:* ${title}
-*🗎 Filesize:* ${video['360p'].fileSizeH}
-*${isLimit ? 'Pakai ' : ''}Link:* ${link}
+if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+let { thumbnail, video, title } = await youtubedl(args[0])
+.catch(async () => await youtubedlv2(args[0]))
+let link = await video['360p'].download()
+const isY = /y(es)/gi.test(args[1])
+const limitedSize = (isPrems || isOwner ? 99 : 70) * 1024
+let isLimit = limitedSize < video['360p'].fileSize
+if (!isY) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+*🔥 𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
+*📁 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾:* ${video['360p'].fileSizeH}
 `.trim(), m)
-if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp3', `
-*📌Title:* ${title}
-*🗎 Filesize:* ${video['360p'].fileSizeH}
+await conn.sendFile(m.chat, link, title + '.mp3', `
+*🔥 𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
+*📁 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾:* ${video['360p'].fileSizeH}
 `.trim(), m, null, {
-  asDocument: 0
-})
-}
+asDocument: 0
+})}
 handler.help = ['mp4', 'v'].map(v => 'yt' + v + ` <url> <without message>`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
