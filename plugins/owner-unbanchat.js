3dd8f58f0b95a772d0a48b298ecb61506aeeb854
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-04 16:04:51 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,10 +1,9 @@
 let handler = async (m) => {
-    global.db.data.chats[m.chat].isBanned = false
-    m.reply('Done!')
+global.db.data.chats[m.chat].isBanned = false
+m.reply('Done!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['group', 'owner']
 handler.command = /^unbanchat$/i
-handler.admin = 1
-
+handler.admin = true
 export default handler
