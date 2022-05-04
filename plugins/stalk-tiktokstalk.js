818fad5f215b17504152fe2f944f13f75d119af9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-04 14:26:13 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/stalk-tiktokstalk.js
+++ plugins/stalk-tiktokstalk.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, 'Harap Masukan Username', m)
+if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*', m)
 let res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${text}?apikey=9b817532fadff8fc7cb86862`)
 let res2 = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=9b817532fadff8fc7cb86862`
 let json = await res.json()
@@ -8,13 +8,13 @@ if (res.status !== 200) throw await res.text()
 if (!json.status) throw json
 let thumb = await (await fetch(json.result.user_picture)).buffer()
 let Mystic = `
-Username: ${json.result.username}
-Nickname: ${json.result.nickname}
-Followers: ${json.result.followers}
-Following: ${json.result.followings}
-Likes: ${json.result.likes}
-Video: ${json.result.video}
-Bio: ${json.result.bio}
+*𝚄𝚂𝚄𝙰𝚁𝙸𝙾:* ${json.result.username}
+*𝙽𝙾𝙼𝙱𝚁𝙴:* ${json.result.nickname}
+*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚂:* ${json.result.followers}
+*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚁𝙴𝚂:* ${json.result.followings}
+*𝙻𝙸𝙺𝙴𝚂:* ${json.result.likes}
+*𝚅𝙸𝙳𝙴𝙾𝚂:* ${json.result.video}
+*𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* ${json.result.bio}
 `.trim()
 conn.sendFile(m.chat, res2, 'error.jpg', Mystic, m, false)
 }
