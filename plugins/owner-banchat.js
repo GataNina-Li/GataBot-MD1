fb9fc38286444711fde85794f4001dff9787a02d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-04 16:05:20 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,13 +1,9 @@
 let handler = async (m, { participants }) => {
-    // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-    global.db.data.chats[m.chat].isBanned = true
-    m.reply('Done!')
-    // } else m.reply('Ada nomor host disini...')
+global.db.data.chats[m.chat].isBanned = true
+m.reply('Done!')
 }
 handler.help = ['banchat']
 handler.tags = ['owner', 'group']
 handler.command = /^banchat$/i
-
 handler.admin = true
-
 export default handler
