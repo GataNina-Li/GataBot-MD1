f70b318c638f1fb9089992014b1c273530b4c349
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 10:47:09 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/random-C. Ronaldo.js
+++ plugins/random-C. Ronaldo.js
@@ -4,5 +4,7 @@ let res = await axios("https://meme-api.herokuapp.com/gimme/Cristianoronaldo")
 let json = res.data
 let url = json.url
 conn.sendButton(m.chat, "*Siiiuuuuuu*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['cristianoronaldo']
+handler.tags = ['internet']
 handler.command = /^(cristianoronaldo)$/i
 export default handler
