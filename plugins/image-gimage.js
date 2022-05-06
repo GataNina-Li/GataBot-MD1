bbd6a7a800bcf42d8da9cf720e9e5ec15733ce31
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 03:22:06 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -4,13 +4,14 @@ if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
 const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
-conn.sendHydrated(m.chat,`
-*GOOGLE IMAGEN*
-🔎 *Result:* ${text}
-🌎 *Source:* Google
-`, author, link, link, '🔗 URL', null, null, [['Next', `.image ${text}`],[null,null],[null,null]],m)
+conn.sendHydrated(m.chat, `
+🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}
+🌎 *𝙱𝚄𝙲𝙰𝙳𝙾𝚁:* Google
+`, author, link, link, '🔗 𝚄𝚁𝙻', null, null, [
+['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/imagen ${text}`]
+], m)
 }
-handler.help = ['gimage <query>', 'image <query>']
+handler.help = ['gimage <query>', 'imagen <query>']
 handler.tags = ['internet', 'tools']
-handler.command = /^(gimage|image)$/i
+handler.command = /^(gimage|image|imagen)$/i
 export default handler
