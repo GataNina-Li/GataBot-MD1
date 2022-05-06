7bb86858715f849405c3ec5234e55ec2553b5478
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 06:00:03 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -5,7 +5,7 @@ let { thumbnail, video, title } = await youtubedl(args[0])
 .catch(async () => await youtubedlv2(args[0]))
 let link = await video['360p'].download()
 const isY = /y(es)/gi.test(args[1])
-const limitedSize = (isPrems || isOwner ? 99 : 70) * 1024
+const limitedSize = (isPrems || isOwner ? 350 : 200) * 3074
 let isLimit = limitedSize < video['360p'].fileSize
 if (!isY) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
 *🔥 𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
