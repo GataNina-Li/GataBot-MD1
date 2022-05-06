7a7eb559c985c4e47d2c5cf87ffdce6c8225808f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 05:52:49 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/fun-txt2.js
@@ -0,0 +1,68 @@
+import { format } from 'util'
+// let path = require('path')
+import { spawn } from 'child_process'
+
+// Font By MFarelS:V
+let fontPath = 'src/font/Zahraaa.ttf'
+let handler = async (m, { conn, args }) => {
+    if (!global.support.convert &&
+        !global.support.magick &&
+        !global.support.gm) return handler.disabled = true // Disable if doesnt support
+    let inputPath = 'src/kertas/magernulis1.jpg'
+    let d = new Date()
+    let tgl = d.toLocaleDateString('id-Id')
+    let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
+    let teks = args.join` `
+    // conn.reply(m.chat, util.format({fontPath, inputPath, outputPath, tgl, hari, teks}), m)
+    let bufs = []
+    const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
+        'convert',
+        inputPath,
+        '-font',
+        fontPath,
+        '-size',
+        '1024x784',
+        '-pointsize',
+        '20',
+        '-interline-spacing',
+        '1',
+        '-annotate',
+        '+806+78',
+        hari,
+        '-font',
+        fontPath,
+        '-size',
+        '1024x784',
+        '-pointsize',
+        '18',
+        '-interline-spacing',
+        '1',
+        '-annotate',
+        '+806+102',
+        tgl,
+        '-font',
+        fontPath,
+        '-size',
+        '1024x784',
+        '-pointsize',
+        '20',
+        '-interline-spacing',
+        '-7.5',
+        '-annotate',
+        '+344+142',
+        teks,
+        'jpg:-'
+    ]
+    spawn(_spawnprocess, _spawnargs)
+        .on('error', e => m.reply(format(e)))
+        .on('close', () => {
+            conn.sendFile(m.chat, Buffer.concat(bufs), 'txt.jpg', '✅ Es mejor de lo que escribes tú ✍🏻', m)
+        })
+        .stdout.on('data', chunk => bufs.push(chunk))
+}
+handler.help = ['txt2 <text>']
+handler.tags = ['fun']
+handler.command = ['escribe2', 'txt2'] 
+
+
+export default handler
