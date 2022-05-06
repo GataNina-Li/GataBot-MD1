357c2e889b2f14d0fbc60cab8b599c95868a0345
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 08:32:33 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -1,9 +1,18 @@
+import fetch from 'node-fetch'
 import { lyrics, lyricsv2 } from '@bochilteam/scraper'
+import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret`
+if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret ojala*`
 const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
-m.reply(`
+let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
+title: result.author + result.title}))
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.thumbnail.genius) throw json
+
+
+let letratexto =`
 𝐓𝐈𝐓𝐔𝐋𝐎: *${result.title}*
 𝐀𝐔𝐓𝐎𝐑 ${result.author}
 
@@ -12,7 +21,11 @@ ${result.lyrics}
 
 
 𝐔𝐑𝐋 ${result.link}
-`.trim())
+`.trim()
+conn.sendHydrated(m.chat, letratexto, wm, json.thumbnail.genius, null, null, null, null, [
+['𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙰𝚄𝙳𝙸𝙾', `/play.1 ${text}`],
+['𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝚅𝙸𝙳𝙴𝙾', `/play.2 ${text}`]
+], m)
 }
 handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
 handler.tags = ['internet']
