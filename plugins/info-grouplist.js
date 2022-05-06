eece28467caea2240301d4a84ffbbbc03e106cc6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 03:04:09 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -1,12 +1,11 @@
 let handler = async (m, { conn }) => {
-    let txt = ''
-    for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `${await conn.getName(jid)}\n🪪${jid} [${chat?.metadata?.read_only ? 'Left' : 'Joined'}]\n\n`
-    m.reply(`List Groups:
+let txt = ''
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `${await conn.getName(jid)}\n🪪${jid} [${chat?.metadata?.read_only ? 'Left' : 'Joined'}]\n\n`
+m.reply(`*𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙴𝙽 𝙻𝙾𝚂 𝚀𝚄𝙴 𝙴𝚂𝚃𝙰 𝙴𝙻 𝙱𝙾𝚃:*
 ${txt}
 `.trim())
 }
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
-handler.command = /^(group(s|list))$/i
-
+handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos)$/i
 export default handler
