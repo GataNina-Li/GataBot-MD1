41619610723fdb829538fba3134c5e3594dbd201
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 08:41:22 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -1,16 +1,15 @@
-let handler = async (m, {usedPrefix}) => {
-	
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    else who = m.sender
-  let name = conn.getName(who) 
-    m.reply(`
-┌───⊷ *BALANCE* ⊶
-▢ *Nombre* : ${name}
-▢ *Diamantes* : *${global.db.data.users[who].limit}💎*
+let handler = async (m, {usedPrefix}) => {	
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+else who = m.sender
+let name = conn.getName(who) 
+m.reply(`
+┌───⊷ 𝐁𝐀𝐋𝐀𝐍𝐂𝐄 ⊶
+▢ *𝙽𝚘𝚖𝚋𝚛𝚎:* ${name}
+▢ *𝙳𝚒𝚊𝚖𝚊𝚗𝚝𝚎𝚜:* ${global.db.data.users[who].limit}💎
 └──────────────
-*NOTA :* 
-Puedes comprar 💎 diamantes usando los comandos
+*𝙽𝙾𝚃𝙰:* 
+*𝙿𝚞𝚎𝚍𝚎𝚜 𝚌𝚘𝚖𝚙𝚛𝚊𝚛 𝚍𝚒𝚊𝚖𝚊𝚗𝚝𝚎𝚜 💎 𝚞𝚜𝚊𝚗𝚍𝚘 𝚕𝚘𝚜 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜*
 ❏ *${usedPrefix}buy <cantidad>*
 ❏ *${usedPrefix}buyall*`)
 }
