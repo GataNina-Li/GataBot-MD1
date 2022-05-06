699e1ce8b0023b9dd2cd2e74a05bf5e0e30b8d98
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-06 04:21:00 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/gc-tagall.js
@@ -0,0 +1,17 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let teks = `${text ? text : ''}\n┌─ 「 Tag All Member 」\n`
+for (let mem of participants) {
+teks += `│• @${mem.id.split('@')[0]}\n`}
+teks += `└─────\n\n*TagAll by 𝚃𝚑𝚎𝙼𝚢𝚜𝚝𝚒𝚌-𝙱𝚘𝚝*`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.admin = true
+handler.group = true
+export default handler
