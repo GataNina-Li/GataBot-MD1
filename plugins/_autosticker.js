913fb2b8bf46439860a19b251eb3bdefa9acb172
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-07 18:41:11 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -1,5 +1,3 @@
-/*
-
 import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
@@ -18,7 +16,7 @@ let img = await q.download()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
 } else if (/video/.test(mime)) {
-if ((q.msg || q).seconds > 11) return await this.sendButton(m.chat, 'durasi maks 10 detik!', wm, [['matikan stiker otomatis', '.0 stiker']], m)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 𝟽 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
 let img = await q.download()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
@@ -35,5 +33,3 @@ export default handler
 
 const isUrl = (text) => {
 return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}
-
-*/
