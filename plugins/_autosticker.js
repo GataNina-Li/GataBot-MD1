8c10a11a0a28d1f5175009f75ddc52d0be2a7aee
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-07 18:47:39 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -15,7 +15,7 @@ if (/image/g.test(mime)) {
 let img = await q.download?.()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
-} else if (/video/.test(mime)) {
+} else if (/video/g.test(mime)) {
 if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 𝟽 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
 let img = await q.download()
 if (!img) return
