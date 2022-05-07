639971ff8b2e303b33cbc85b5a1a6a9f420b9d91
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-07 18:27:53 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('TheMystic-Bot\nWhatsApp Bot', {
+say('The Mystic - Bot\nWhatsApp - Bot - MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
