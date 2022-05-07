3e4136bfc34477ac41ed42a656f46828308466a9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-07 03:06:38 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/_autosticker.js
@@ -0,0 +1,39 @@
+/*
+
+import { sticker } from '../lib/sticker.js'
+
+let handler = m => m
+
+handler.all = async function (m) {
+let chat = db.data.chats[m.chat]
+let user = db.data.users[m.sender]
+
+if (chat.autosticker && m.isGroup) {
+let q = m
+let stiker = false
+let mime = (q.msg || q).mimetype || ''
+if (/webp/.test(mime)) return
+if (/image/.test(mime)) {
+let img = await q.download()
+if (!img) return
+stiker = await sticker(img, false, packname, author)
+} else if (/video/.test(mime)) {
+if ((q.msg || q).seconds > 11) return await this.sendButton(m.chat, 'durasi maks 10 detik!', wm, [['matikan stiker otomatis', '.0 stiker']], m)
+let img = await q.download()
+if (!img) return
+stiker = await sticker(img, false, packname, author)
+} else if (m.text.split(/\n| /i)[0]) {
+if (isUrl(m.text)) stiker = await sticker(false, m.text.split(/\n| /i)[0], packname, author)
+else return
+}
+if (stiker) {
+await this.sendFile(m.chat, stiker, null, { asSticker: true })
+}}
+return !0
+}
+export default handler
+
+const isUrl = (text) => {
+return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}
+
+*/
