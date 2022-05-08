712555361fdd10a6038b60a9ac3d429b8312ae6c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-07 22:56:26 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/maker-pixel.js
+++ plugins/maker-pixel.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedprefix }) => {
+let handler = async (m, { conn, usedprefix, text }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/pixelate', {
 avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
