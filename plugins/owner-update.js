5bb31ddcc0fcd9fb0307eb0295600649ed5a6b2c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-05-10 04:14:57 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -2,7 +2,6 @@ import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
 if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-require('fs').readdirSync('plugins').map(v=>global.reload('', v))
 conn.reply(m.chat, stdout.toString(), m)
 }}
 handler.help = ['update']
