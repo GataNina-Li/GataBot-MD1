c6fdea7c8ac1135852c0890a6064df0399782ff6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 18:46:57 -0500

    Update info-rapidez.js

--- plugins/info-rapidez.js
+++ plugins/info-rapidez.js
@@ -23,7 +23,6 @@ templateButtons: templateButtons
 }
 conn.sendMessage(m.chat, tm, m)
 }
- }
 // let veloz = `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`.trim() 
 //conn.sendButton(m.chat, `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`, wm, veloz, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', /${command}]], m)
                                                
