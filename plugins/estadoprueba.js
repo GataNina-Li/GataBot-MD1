fd5555979af4d829140975673271b19a3ec1869d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 15:10:37 -0500

    Update estadoprueba.js

--- plugins/estadoprueba.js
+++ plugins/estadoprueba.js
@@ -6,97 +6,46 @@ SI VAS A AÑADIR TUS DATOS O CRÉDITOS, ESTA BIEN. PERO NO QUITEN LOS QUE YA EST
 /** PLEASE BE KIND AND KINDNESS NOT TO MINIMALLY CHANGE GATABOT-MD CREDITS, 
 IF YOU ARE GOING TO ADD YOUR DATA OR CREDITS, IT'S OK. BUT DO NOT REMOVE THOSE THAT ARE ALREADY FROM GATABOT-MD, THANK YOU **/
 
-import '@adiwajshing/baileys'
-import { promises } from 'fs'
-let fetch = require('node-fetch')
-let handler = async (m, { command }) => {
+let handler = async (m, { conn, command, usedPrefix }) => {
+let picture = './Menu2.jpg'
+let name = await conn.getName(m.sender)
 let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) { process.send('uptime')
+_muptime = await new Promise(resolve => { process.once('message', resolve) 
+setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
-bear = "Source Code"
-ye = `Wa.me/${m.sender.split`@`[0]}`
-let esce = `╭━━━━[ *𝙀𝙎𝙏𝘼𝘿𝙊 | 𝙎𝙏𝘼𝙏𝙐𝙎* ]━━━━━⬣
-┃💗 *¡Hola | Hi!* ${ye} 
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃𓃠 *Versión de GataBot-MD*
-┃➥ ${vs}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ღ *Activo Durante | Active During* 
-┃➥ ${uptime}
-┃ღ *Modo | Mode:* 
-┃➥ ${global.opts['self'] ? 'Self' : 'publik'}
-┃ღ *Usuario(s) | Users* 
-┃➥ ${Object.keys(global.db.data.users).length} 
-┃ღ *Chat(s) Prohibido(s) | Forbidden Chats*
-┃➥ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
-┃ღ *Usuario(s) Prohibido(s) | Prohibited Urs*
-┃➥ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
-╰━━━━━━━━━━━━━━━━━━⬣`/*const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: esce,
-           locationMessage: { 
-           jpegThumbnail: await (await fetch(fla + bear)).buffer() },            
-           hydratedFooterText: gt,
-           hydratedButtons: [{*/
-let message = await prepareWAMessageMedia({ image: fs.readFileSync('./media/menus/Menu1.jpg')}, { upload: conn.waUploadToServer })
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           imageMessage: message.imageMessage,
-           hydratedContentText: esce,
-           hydratedFooterText: gt,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '𝙂𝙞𝙩𝙃𝙪𝙗',
-               url: "https://github.com/GataNina-Li/GataBot-MD"
-              }   
-            },
-              {
-              urlButton: {
-               displayText: '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢',
-               url: instagram	           
-             }
-           },
-			     {
-             quickReplyButton: {
-               displayText: '𝙑𝙚𝙡𝙤𝙘𝙞𝙙𝙖𝙙 | 𝙎𝙥𝙚𝙚𝙙',
-               id: '.ping',
-             }
-	   },
-             {
-             quickReplyButton: {
-               displayText: '𝙈𝙚𝙣𝙪́ 𝙘𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪',
-               id: '.allmenu',
-             }
-	   },
-             {
-             quickReplyButton: {
-               displayText: '𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪',
-               id: '.menu',
-             }
-          }]
-        }
-      }
-    }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
+let estado =`
+╭─[ *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* ]
+│ *➤ 𝙷𝙾𝙻𝙰 ${name}*
+│
+│ *ミ🤖 𝙴𝚂𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 🤖彡*
+│ *=> 𝙱𝙾𝚃 𝙰𝙲𝚃𝙸𝚅𝙾 ✅*
+│ *=> 𝙱𝙾𝚃 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 ✅*
+│ *=> 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
+╰───────────────
+`.trim()
+
+const templateButtons = [
+{index: 1, urlButton: {displayText: 'URL 1', url: `link`}},
+{index: 2, urlButton: {displayText: '𝙶𝙸𝚃𝙷𝚄𝙱', url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}},
+{index: 3, quickReplyButton: {displayText: '📮 𝙳𝙾𝙽𝙰𝚁 📮', id: '#donasi'}},
+{index: 4, quickReplyButton: {displayText: '🌹 𝙾𝚆𝙽𝙴𝚁 🌹', id: '#owner'}},
+{index: 5, quickReplyButton: {displayText: '🐾 𝙸𝙽𝙵𝙾𝙱𝙾𝚃 🐾', id: '#infobot'}},
+]
+let tm = {
+text: '',
+footer: `${str}\n${wm}`,
+templateButtons: templateButtons
 }
 
-handler.help = ['estado | status']
-handler.tags = ['info']
-handler.command = /^estado|status|estate|state|stado|stats|botstat(us)?$/i
-handler.exp = 4 
-
+handler.help = ['estado']
+handler.tags = ['main']
+handler.command = /^(est)$/i
 export default handler
 
 function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-	
-    }
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
