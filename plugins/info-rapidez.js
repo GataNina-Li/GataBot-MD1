ebf99409cdae99d245be631e7ac1d15bc752c532
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 18:03:34 -0500

    Update info-rapidez.js

--- plugins/info-rapidez.js
+++ plugins/info-rapidez.js
@@ -1,11 +1,11 @@
 let handler = async m => {
- let txt = `${eg}\n\n*_COMENZANDO PRUEBA DE VELOCIDAD..._*\n*_STARTING SPEED TEST..._*`.trim()
+ let txt = `${eg}*_COMENZANDO PRUEBA DE VELOCIDAD..._*\n*_STARTING SPEED TEST..._*`.trim()
   m.reply(txt)
 }
-//m.reply('🚀')
-//m.reply('🚀🚀')
-//m.reply('🚀🚀🚀')
-//m.reply(`${rg}\n\n*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`)
+m.reply('🚀')
+m.reply('🚀🚀')
+m.reply('🚀🚀🚀')
+m.reply(`${rg}\n\n*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`)
 
 handler.help = ['ping']
 handler.tags = ['info']
