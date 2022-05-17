18f6086f1b58c2f6826f82f59f303b6b61724a49
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 18:24:05 -0500

    Update info-rapidez.js

--- plugins/info-rapidez.js
+++ plugins/info-rapidez.js
@@ -9,8 +9,27 @@ m.reply('🚀')
 m.reply('🚀🚀')
 m.reply('🚀🚀🚀')
  
- let veloz = `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`.trim() 
-conn.sendButton(m.chat, `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`, wm, veloz, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', /${command}]], m)
+ let text = 
+_aquí iria el texto_
+
+
+const templateButtons = [
+{index: 1, urlButton: {displayText: 'URL 1', url: link}},
+{index: 2, urlButton: {displayText: 'URL 2', url: link}},
+{index: 3, quickReplyButton: {displayText: 'Boton 1', id: '#comando 1'}},
+{index: 4, quickReplyButton: {displayText: 'Boton 2', id: '#comando 2'}},
+{index: 5, quickReplyButton: {displayText: 'Boton 3', id: '#comando 3'}},
+]
+let tm = {
+text: null,
+footer: ${text}\n${wm},
+templateButtons: templateButtons
+}
+conn.sendMessage(m.chat, tm, m)
+}
+ 
+// let veloz = `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`.trim() 
+//conn.sendButton(m.chat, `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`, wm, veloz, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', /${command}]], m)
                                                
 //m.reply(`${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`)
 
