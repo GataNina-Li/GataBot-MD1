cb2a3b2e807ac67636859ac7f47574e3e66cc895
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 20:41:43 -0500

    Update info-grouplist.js

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -1,33 +1,18 @@
 let handler = async (m, { conn }) => {
-  
-  const q = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": ""
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
-		}
-	},
-	"participant": "0@s.whatsapp.net"
-} 
-  
 let txt = ''
-for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? '𝙽𝙾 𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴' : '𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴'}]\n\n`
-m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS | IS IN THESE GROUPS*
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n🐈 ${await conn.getName(jid)}\n✦ ${jid} [${chat?.metadata?.read_only ? '❌ *SIN ESTAR AQUÍ | NO*' : '✅ *SIGO AQUÍ | YES*'}]\n\n`
+m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS*
+*IS IN THESE GROUPS:*
 ${txt}
 `.trim())
 
-conn.sendHydrated(m.chat, txt, wm, null, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
-['📮 𝙳𝙾𝙽𝙰𝚁 📮', '/donasi'],
-['🌹 𝙾𝚆𝙽𝙴𝚁 🌹', '/owner']
+conn.sendHydrated(m.chat, txt, wm, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '.menu'],
+['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '/cuentasgb']
 ], m,)
 }
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
-handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos)$/i
+handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos|listadegrupos)$/i
 handler.exp = 30
 export default handler
