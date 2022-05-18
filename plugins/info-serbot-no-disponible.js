fddd9417c0e37a27c640f199dbec44ebe73c76da
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-18 11:53:35 -0500

    Update info-serbot-no-disponible.js

--- plugins/info-serbot-no-disponible.js
+++ plugins/info-serbot-no-disponible.js
@@ -10,14 +10,13 @@ pp = await conn.getProfilePicture(who)
 } finally { }
 await m.reply('╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮')
 let text = `*Las funciones .jadibot .bots .serbot .subbot .getcode No estan disponibles en* ${gt}\n*The functions .jadibot .bots .serbot .subbot .getcode are not available*`.trim()   
-conn.sendButton(m.chat, text, wm, await(await fetch(pp)).buffer(), [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)
-}
-/*conn.reply(m.chat, text, m, {
+//conn.sendButton(m.chat, text, wm, await(await fetch(pp)).buffer(), [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
+conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝚂𝙴𝚁𝙱𝙾𝚃',
 body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',         
-previewType: 1, thumbnail: fs.readFileSync(pp),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   */
-//}
+previewType: 1, thumbnail: fs.readFileSync("./src/avatar_contact.png"),
+sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   
+}
 handler.command = /^(jadibot|serbot|bots|subbots|getcode)/i
 export default handler
