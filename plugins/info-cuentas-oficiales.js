15166b9534ec58a446ab8e9e3bf8f55d4586bd45
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 22:10:38 -0500

    Update info-cuentas-oficiales.js

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -1,5 +1,6 @@
 let media = './media/menus/Menuvid2.mp4'
-let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
+let handler = async (m, { conn, command }) => {
+let str = `
 🔱 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
 🔱 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
@@ -12,8 +13,16 @@ let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 ✅ *YOUTUBE*
 *${yt}*
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.\n
-If you have doubts, suggestions or questions just write on Instagram.
-`.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu', '𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
+*Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*\n
+*If you have doubts, suggestions or questions just write on Instagram.*
+`.trim()
+  
+conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['📮 𝙳𝙾𝙽𝙰𝚁 📮', '/donasi'],
+['🌹 𝙾𝚆𝙽𝙴𝚁 🌹', '/owner'],
+['🐾 𝙸𝙽𝙵𝙾𝙱𝙾𝚃 🐾', '/infobot']
+], m,)}
+
 handler.command = /^cuentasoficiales|gataig|cuentasgb|cuentagb|accounts|gataaccounts|account|iggata|cuentasdegata|cuentasdegatabot$/i
+handler.exp = 35
 export default handler
