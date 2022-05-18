2e79fd9ad793b2a40b07abcc8fe539214c254354
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 22:43:29 -0500

    Delete info-sc.js

--- plugins/info-sc.js
+++
@@ -1,7 +0,0 @@
-let handler = (t) => {
-t.reply('*https://github.com/BrunoSobrino/TheMystic-Bot-MD*')
-}
-handler.command = ['sc','script']
-handler.help = ['sc']
-handler.tags = ['General']
-export default handler
