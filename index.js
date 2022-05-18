a6c7639f4031e48ef31bccf5e9a036a61db6e1a6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-18 08:40:41 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Gata\Bot\MD', {
+say('Gata\nBot\nMD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
