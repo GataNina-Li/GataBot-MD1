395c61c1866c6334a44902d5a035232cb5018809
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-18 13:00:52 -0500

    Update info-serbot-no-disponible.js

--- plugins/info-serbot-no-disponible.js
+++ plugins/info-serbot-no-disponible.js
@@ -8,11 +8,13 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //} catch (e) {
 
 //} finally { }
-await m.reply('╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮')
-let text = `*Las funciones .jadibot .bots .serbot .subbot .getcode No estan disponibles en* ${gt}\n*The functions .jadibot .bots .serbot .subbot .getcode are not available*`.trim()   
-//conn.sendButton(m.chat, text, wm, await(await fetch(pp)).buffer(), [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
+await m.reply(conn.sendButton(m.chat, '╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮', wm, null, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', /${command}], ['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', /menu]], m))
+
+let text = `
+*Las funciones | The functions #jadibot #bots #serbot #subbot #getcode. No estan disponibles en | Are not available in ${gt} 
+`.trim()   
 conn.reply(m.chat, text, m, {
-contextInfo: { externalAdReply :{ mediaUrl: `https://github.com/GataNina-Li/GataBot-MD`, mediaType: 1, description: `git`, 
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
 body: 'Super Bot WhatsApp',         
 previewType: 1, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
