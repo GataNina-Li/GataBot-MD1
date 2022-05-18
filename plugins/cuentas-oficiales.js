76ff12fae153acf593e900c69edee9f1cd1b31e0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 21:16:32 -0500

    Create cuentas-oficiales.js

---
+++ plugins/cuentas-oficiales.js
@@ -0,0 +1,10 @@
+let media = './media/menus/Menuvid2.mp4'
+let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
+𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
+𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
+
+
+
+`.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
+handler.command = /^linkgc|grupos$/i
+export default handler
