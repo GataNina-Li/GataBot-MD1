d22fe1cc217e922584cfbe8d9c5a09cff0ff2959
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 23:11:23 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,26 +1,30 @@
-let media = './Menu2.jpg'
-let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
+let media = './media/menus/Menuvid2.mp4'
+let handler = async (m, { conn, command }) => {
+let str = `
+💕 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝙊𝙎 𝙂𝙍𝙐𝙋𝙊𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
+💞 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝙂𝙍𝙊𝙐𝙋𝙎
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+𓃠 *Versión de ${gt}*
+➥ ${vs}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ 𝙂𝙍𝙐𝙋𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 ${gt}
+🐈 *${nn}*\n
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+🐈 *${nnn}*\n
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+🐈 *https://chat.whatsapp.com/BlasuG7z02d16wEaCf61pa*\n
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+*Por favor, no ingresar con números de Bots, y mantener el respeto.*\n
+*Please, do not enter with Bot numbers, and maintain respect.*
+`.trim()
+  
+conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙂𝙧𝙤𝙪𝙥𝙨 🔰', '.grupos'],
+['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 | 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 💗', '#owner'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)}
 
-*➤ Grupos oficiales del Bot:*
-*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc$/i
+handler.exp = 33
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
-
-*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
-
-*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
-
-*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
-
-*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa
-
-*7.-* https://chat.whatsapp.com/EpzuymKm6lG08k6J2Dwx7F
-
-*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGpX7rQIw
-
-*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYmKwp7
-
-*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSUZLM9
-`.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
-handler.command = /^linkgc|grupos$/i
 export default handler
