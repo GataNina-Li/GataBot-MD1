9d442bc00d5a62a6cd5470eae8fd5b105df251a3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 22:00:31 -0500

    Update info-cuentas-oficiales.js

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -1,10 +1,19 @@
 let media = './media/menus/Menuvid2.mp4'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
-𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
-𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
-
-
-
-`.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
-handler.command = /^linkgc|grupos$/i
+🔱 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
+🔱 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *GITHUB*
+*${md}*
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *INSTAGRAM - ASISTENCIA*
+*${ig}*
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ *YOUTUBE*
+*${yt}*
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.\n
+If you have doubts, suggestions or questions just write on Instagram.
+`.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu', '𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
+handler.command = /^cuentasoficiales|gataig|cuentasgb|cuentagb|accounts|gataaccounts|account|iggata|cuentasdegata|cuentasdegatabot$/i
 export default handler
