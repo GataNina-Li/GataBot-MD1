503fa5db7c20a0c7986c7f549d13f181df9bf29b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-17 20:20:22 -0500

    Update info-grouplist.js

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
-let txt = ''
+let txtt = ''
 for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? '𝙽𝙾 𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴' : '𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴'}]\n\n`
 m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS | IS IN THESE GROUPS*
 ${txt}
 `.trim())
 
-conn.sendHydrated(m.chat, txt, wm, null, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, txtt, wm, null, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['📮 𝙳𝙾𝙽𝙰𝚁 📮', '/donasi'],
 ['🌹 𝙾𝚆𝙽𝙴𝚁 🌹', '/owner']
 ], m,)
