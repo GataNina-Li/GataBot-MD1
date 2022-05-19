7b050c413e5db2489af125fea30a989d4237cf67
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-18 23:59:28 -0500

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -9,6 +9,11 @@ let time = d.toLocaleTimeString(locale, {
       minute: 'numeric',
       second: 'numeric'
     })
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) {
+process.send('uptime')
+let uptime = clockString(_uptime)
 
 wm = global.wm
 vs = global.vs
