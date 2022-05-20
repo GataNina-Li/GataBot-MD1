2b9377938a4abc8a5cb40ab4c31db2c96527124f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-20 17:17:15 -0500

    Update owner-listprem.js

--- plugins/owner-listprem.js
+++ plugins/owner-listprem.js
@@ -1,7 +1,23 @@
+import fs from 'fs'
 let handler = async (m, { conn }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-  conn.reply(m.chat, `*「 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 」*
-` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, { contextInfo: { mentionedJid: prem } })
+
+conn.sendButton(prem, `*╭━━[ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ]━━━⬣*\n*┃*\n*┃💌 + prem.map(v => '- @' + v.replace(/@.+/, '')).join\n┃*\n*╰━━━━━━━━━━━━━━━━━━⬣*`, '✅ *𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇*\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🎁 𝙄𝙣𝙛𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡', '.cuentasgb'],['🐈 𝙈𝙚𝙣𝙪', '.menu']], false, {
+contextInfo: { externalAdReply: {
+title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+body: 'Super Bot WhatsApp', 
+sourceUrl: `https://www.instagram.com/gata_dios`, 
+thumbnail: fs.readFileSync('./media/menus/Menu3.jpg') }}})}
+
+//conn.reply(m.chat, text, m, {
+//contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+//title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+//body: 'Super Bot WhatsApp',         
+//previewType: 1, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+//sourceUrl: `${yt}`}}})  
+
+//  conn.reply(m.chat, `*「 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 」*
+//` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, { contextInfo: { mentionedJid: prem } })
 }
 handler.help = ['premlist']
 handler.tags = ['owner']
