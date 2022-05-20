1f81dad620de0ce3e71882cff4b8e4e5d3dd9f0f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-20 18:08:45 -0500

    Update owner-listprem.js

--- plugins/owner-listprem.js
+++ plugins/owner-listprem.js
@@ -1,8 +1,17 @@
 let handler = async (m, { conn }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
 
-conn.reply(m.chat, `*╭━━[ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ]━━━⬣*\n*┃*\n` + prem.map(v => '*┃* 💎 @' + v.replace(/@.+/, ' 💎')).join`\nhhhhhh`, m, { contextInfo: { mentionedJid: prem } }) 
-                                                                                                                       
+conn.reply(m.chat, `*╭━━[ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ]━━━⬣*\n*┃*\n` + prem.map(v => '*┃* 💎 @' + v.replace(/@.+/, ' 💎')).join``, m, { contextInfo: { mentionedJid: prem } }) 
+   
+  let text = `
+*Las funciones | The functions #jadibot #bots #serbot #subbot #getcode. No estan disponibles en | Are not available in* ${gt}`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+body: 'Super Bot WhatsApp',         
+previewType: 1, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `${yt}`}}})
+  
 }
 handler.help = ['premlist']
 handler.tags = ['owner']
