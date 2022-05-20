e32643bb75fd3efdf3f782563fac608827d44ea1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-20 18:04:15 -0500

    Update owner-listprem.js

--- plugins/owner-listprem.js
+++ plugins/owner-listprem.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
 
-conn.reply(m.chat, `*╭━━[ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ]━━━⬣*\n*┃*\n` + prem.map(v => '*┃* 💎 @' + v.replace(/@.+/, '')).join`\n`, m, { contextInfo: { mentionedJid: prem } }) 
+conn.reply(m.chat, `*╭━━[ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ]━━━⬣*\n*┃*\n` + prem.map(v => '*┃* 💎 @' + v.replace(/@.+/, 'gghhhh')).join`\n`, m, { contextInfo: { mentionedJid: prem } }) 
                                                                                                                        
 }
 handler.help = ['premlist']
