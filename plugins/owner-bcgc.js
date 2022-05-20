15dddc9d31e2dc459866d8c576a3a7ad473c3110
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-19 19:22:24 -0500

    Update owner-bcgc.js

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -15,6 +15,6 @@ m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸
 handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i
-handler.rowner = true
+//handler.rowner = true
 handler.owner = true
 export default handler
