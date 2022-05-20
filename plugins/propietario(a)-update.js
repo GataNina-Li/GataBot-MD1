76e51a22bfc225af0f65caa48599accf634df91b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-19 22:37:31 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -6,6 +6,6 @@ conn.reply(m.chat, stdout.toString(), m)
 }}
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = /^update|actualizar$/i
+handler.command = /^update|actualizar$/i 
 handler.rowner = true
 export default handler
