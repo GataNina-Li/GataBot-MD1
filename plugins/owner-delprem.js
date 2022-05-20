0a6184fd7dfa1c44b1cc9ed7d710bf5dab6c2904
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-20 16:42:17 -0500

    Update owner-delprem.js

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -15,7 +15,6 @@ handler.tags = ['owner']
 handler.command = /^(remove|-|del)prem$/i
 handler.group = true
 handler.rowner = true
-handler.owner = true
 handler.admin = true
 handler.botAdmin = true
 export default handler
