8789f298515f0a71007d1aefa9e5dcdedbb62829
Subbotsito <105924903+Subbotsito@users.noreply.github.com>
2022-05-22 14:50:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url'
 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
-  ['56977774748', 'the whatBots420', true],
+  ['56977774748', 'creador', true],
   ['447441419651'],
   ['50495745934']
 ]
