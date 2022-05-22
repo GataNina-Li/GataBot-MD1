aca9c84047dded3181559370d184fcfc85ca550f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-22 17:06:33 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-unbanchat.js
+++ plugins/propietario(a)-unbanchat.js
@@ -4,8 +4,8 @@ m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽�
 }
 handler.help = ['unbanchat2']
 handler.tags = ['owner']
-handler.command = /^unbanchat2$/i
+handler.command = /^desban1|unbanchat1$/i
 handler.rowner = true
-handler.exp = 500
+handler.exp = 50
 
 export default handler
