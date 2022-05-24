f5b159d0e54cf4cb9b2aa80999b881a35f579029
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 18:30:41 -0500

    Update descargas-ighistorias.js

--- plugins/descargas-ighistorias.js
+++ plugins/descargas-ighistorias.js
@@ -2,14 +2,11 @@
 import axios from 'axios'
 import hx from 'hxz-api' 
 let handler = async (m, { conn, args, usedPrefix, command, text}) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} luisitocomunica*`  
-let res = await hx.igstory(!text)
-if (!res.length) throw '❰ ⚠️ ❱ *Error, por favor vuelva a intentarlo..' 
-  
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} luisitocomunica*`        
 hx.igstory(text).then(async (result) => {
 for (let i of result.medias) {
 if (i.url.includes("mp4")) {            
-conn.sendFile(m.chat, i.url, 'igstory.mp4', gt, m)
+conn.sendFile(m.chat, i.url, 'igstory.mp4', wm, m)
 } else {     
 conn.sendFile(m.chat, i.url, '', '', m)
 }}});
