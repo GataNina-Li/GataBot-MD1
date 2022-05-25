47637cffe5ec1107a335cb858ba381eac948b2e8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 22:38:07 -0500

    Update downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -2,10 +2,26 @@ import { pinterest } from '@bochilteam/scraper'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} Minecraft*`
 const json = await pinterest(text)
-conn.sendFile(m.chat, json.getRandom(), 'error.jpg', `
-*𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰*
-${text}
-`.trim(), m)
+let pinterest = res.getRandom()
+let link = pinterest
+//conn.sendFile(m.chat, json.getRandom(), 'error.jpg', `
+//*𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰*
+//${text}
+//`.trim(), m)
+  
+  conn.sendHydrated(m.chat, `💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}`, `𝙂𝙤𝙤𝙜𝙡𝙚 | ${wm}`, link, link, '☘️ 𝙐𝙍𝙇', null, null, [
+['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`],
+['🔍 𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝙩 ', `#pint ${text}`],
+['🐈 𝙈𝙚𝙣𝙪', `.menu`],  
+], m)
+  
+ // let info = `𝙈𝘼𝙔𝙊𝙍 𝘼 150 𝙈𝘽 𝙀𝙎 𝙋𝙊𝙎𝙄𝘽𝙇𝙀 𝙌𝙐𝙀 𝙉𝙊 𝙎𝙀 𝙀𝙉𝙑𝙄𝙀.
+//𝙂𝙍𝙀𝘼𝙏𝙀𝙍 𝙏𝙃𝘼𝙉 150 𝙈𝘽 𝙈𝘼𝙔 𝙉𝙊𝙏 𝘽𝙀 𝙎𝙀𝙉𝙏.`.trim()  
+//await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+//['𝙈𝙚𝙣𝙪 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨 🌀', '#descargasmenu'],
+//['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
+//['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+//], m,)  
 }
 handler.help = ['pinterest <keyword>']
 handler.tags = ['internet']
