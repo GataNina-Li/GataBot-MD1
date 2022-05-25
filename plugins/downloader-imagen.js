633c120a2f4c722f84bf909c0acc725e7602bdc5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 20:30:27 -0500

    Update downloader-imagen.js

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -8,6 +8,7 @@ conn.sendHydrated(m.chat, `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:*
 🌎 *𝙱𝚄𝙲𝙰𝙳𝙾𝚁:* Google
 `, author, link, link, '🔗 𝚄𝚁𝙻', null, null, [
 ['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/imagen ${text}`]
+['Pinterest', `/pinterest ${text}`]
 ], m)
 }
 handler.help = ['gimage <query>', 'imagen <query>']
