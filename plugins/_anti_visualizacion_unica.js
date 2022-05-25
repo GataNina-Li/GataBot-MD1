d620871eb3d30b9d27e2dc76dfd1a0bac2fef76f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 22:07:24 -0500

    Update _anti_visualizacion_unica.js

--- plugins/_anti_visualizacion_unica.js
+++ plugins/_anti_visualizacion_unica.js
@@ -1,6 +1,6 @@
 let handler = m => m
 
-handler.before = async function (m, { user }) {
+//handler.before = async function (m, { user }) {
         //let q = m.quoted ? m.quoted : m
                if (m.mtype == 'viewOnceMessage') {
                           await this.copyNForward(m.chat, await this.loadMessage(m.chat, m.id), false, { readViewOnce: true })
