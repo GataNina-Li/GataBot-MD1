b324b5bf49d55233a4534f28df3b213494f6c228
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-25 00:02:39 -0500

    Update ytv720.js

--- plugins/ytv720.js
+++ plugins/ytv720.js
@@ -6,7 +6,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command, text }) => {
     if (!text) throw 'Masukkan Link\n\nContoh: .ytv720 https://youtube.com/xxxxxx'
     let { xfar } = 'xfarr-api'
-  let res = await xfar.Youtube(text)
+  let res = await xfar.ytv720(text)
 m.reply('*Tunggu Sebentar...*')
 conn.sendFile(m.chat,res.medias[2].url, '', `Youtube Downloader
 720p
