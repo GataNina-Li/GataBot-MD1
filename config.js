ac0d09a5196f6b2c4e867fb0dc6db2405b1839a6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 21:33:51 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -94,7 +94,7 @@ global.gatabot = md
 global.nn = 'https://chat.whatsapp.com/Hahc7UwSouH9jIDStkT5QW'
 global.nngrupo = nn
 
-global.nnn = 'https://chat.whatsapp.com/JFTOYLdpiTBCluFJvikBxN'
+global.nnn = 'https://chat.whatsapp.com/K3BB4VtvyzyJdnX2jLeurh'
 global.nnngrupo = nnn
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
