31d5770c32c538cc36b67f3bf4022cc86ae96938
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 20:51:41 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -5,7 +5,7 @@ const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
 conn.sendHydrated(m.chat, `💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}
-`, `GOOGLE | ${wm}`, link, link, '🔗 𝚄𝚁𝙻', null, null, [
+`, `*𝙂𝙤𝙤𝙜𝙡𝙚* | ${wm}`, link, link, '✨ 𝙐𝙍𝙇', null, null, [
 ['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`],
 ['🔍 𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝙩 ', `#pinterest ${text}`],
 ['🐈 𝙈𝙚𝙣𝙪', `.menu`],  
