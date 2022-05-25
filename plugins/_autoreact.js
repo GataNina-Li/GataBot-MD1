8e114ba609f2970336c1c598469b6d571f410e02
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 20:20:36 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -7,7 +7,7 @@ key: m.key
     	}
     })	
 }
-handler.customPrefix = /(bot|gatadios|dios|gatabot-md|s|gata dios|gata - bot|gata-bot|gatabot|gata|gatabotmd|gata - bot md)/i
+handler.customPrefix = /(bot|gatadios|dios|gatabot-md|gata dios|gata - bot|gata-bot|gatabot|gata|gatabotmd|gata - bot md)/i
 handler.command = new RegExp
 
 export default handler
