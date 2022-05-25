e59708be21e1bac6b5e9850d649e6b17196d1be9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-24 20:58:39 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -4,8 +4,7 @@ if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
 const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
-conn.sendHydrated(m.chat, `💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}
-`, `𝙂𝙤𝙤𝙜𝙡𝙚 | ${wm}`, link, link, '☘️ 𝙐𝙍𝙇', null, null, [
+conn.sendHydrated(m.chat, `💞 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 | 𝙍𝙚𝙨𝙪𝙡𝙩: ${text}`, `𝙂𝙤𝙤𝙜𝙡𝙚 | ${wm}`, link, link, '☘️ 𝙐𝙍𝙇', null, null, [
 ['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`],
 ['🔍 𝙋𝙞𝙣𝙩𝙚𝙧𝙚𝙨𝙩 ', `#pinterest ${text}`],
 ['🐈 𝙈𝙚𝙣𝙪', `.menu`],  
