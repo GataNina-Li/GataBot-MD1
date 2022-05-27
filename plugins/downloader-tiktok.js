6fc51b01530b0564ab26a8e198e159698eece9f4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-27 16:10:53 -0500

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,17 +2,17 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 let handler = async (m, {command, conn, text, usedPrefix, args}) => {
 if (!text) throw `${mg}𝘿𝙀𝘽𝙀 𝙄𝙉𝙂𝙍𝙀𝙎𝘼𝙍 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙏𝙄𝙆𝙏𝙊𝙆 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://vm.tiktok.com/ZMLEPnruc/?k=1*\n\n𝙔𝙊𝙐 𝙈𝙐𝙎𝙏 𝙀𝙉𝙏𝙀𝙍 𝘼 𝙏𝙄𝙆𝙏𝙊𝙆 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙏𝙃𝙀 𝙑𝙄𝘿𝙀𝙊\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://vm.tiktok.com/ZMLEPnruc/?k=1*`
-if (command == 'tiktokaudio') {
-conn.reply(m.chat, `${eg}PRONTO TENDRA EL VIDEO DE TIKTOK 😸*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
-body: 'Super Bot WhatsApp',         
-previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
-sourceUrl: `https://github.com/GataNina-Li/GataBot-MD`}}})
+//if (command == 'tiktokaudio') {
+//conn.reply(m.chat, `${eg}PRONTO TENDRA EL VIDEO DE TIKTOK 😸*`, m, {
+//contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+//title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+//body: 'Super Bot WhatsApp',         
+//previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+//sourceUrl: `https://github.com/GataNina-Li/GataBot-MD`}}})
            
-let res = await fetch("https://api.dhamzxploit.my.id/api/tiktod/?url="+args[0])
-let json = await res.json()
-conn.sendFile(m.chat, json.result.audio, 'error.mp3', null, m, false, { mimetype: 'mp3/mp4' })}
+//let res = await fetch("https://api.dhamzxploit.my.id/api/tiktod/?url="+args[0])
+//let json = await res.json()
+//conn.sendFile(m.chat, json.result.audio, 'error.mp3', null, m, false, { mimetype: 'mp3/mp4' })}
   
 if (command == 'tiktok') {
 conn.reply(m.chat, `${eg}PRONTO TENDRA EL VIDEO DE TIKTOK 😸*`, m, {
@@ -24,9 +24,9 @@ sourceUrl: `https://github.com/GataNina-Li/GataBot-MD`}}})
   
 let res = await fetch("https://api.dhamzxploit.my.id/api/tiktod/?url="+args[0])
 let json = await res.json()
-conn.sendFile(m.chat, json.result.nowatermark, 'error.mp4', `_𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭_`, m)}
+conn.sendFile(m.chat, json.result.nowatermark, 'error.mp4', `${wm}`, m)}
 }
-handler.help = ['tiktok' , 'tiktokaudio'].map(v => v + ' <link>')
+handler.help = ['tiktok'].map(v => v + ' <link>')
 handler.tags = ['downloader']
-handler.command = ['tiktok', 'tiktokaudio']
+handler.command = ['tiktok']
 export default handler
