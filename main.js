692dc8a2c01c680d76d0a8e93da6e9c292289d67
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-27 22:18:39 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 import './config.js';
 
-import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method 
 import path, { join } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
 import { platform } from 'process'
