1fdd0704ed91ecef6e0c05cd0814b92f88d4edd7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-29 00:04:34 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/descargas-gitclone.js
+++ plugins/descargas-gitclone.js
@@ -21,4 +21,5 @@ await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜�
 handler.help = ['gitclone <url>']
 handler.tags = ['downloader']
 handler.command = /gitclone|clonarepo|clonarrepo|repoclonar/i
+handler.exp = 59
 export default handler
