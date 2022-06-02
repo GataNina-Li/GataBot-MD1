bf2586d4ff09e4da9d03a496193b7c646d191439
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-01 20:12:51 -0500

    Update propietario(a)-unbanchat.js

--- plugins/propietario(a)-unbanchat.js
+++ plugins/propietario(a)-unbanchat.js
@@ -10,12 +10,15 @@ handler.exp = 50
 
 export default handler*/
 
+import db from '../lib/database.js'
+
 let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*')
+    db.data.chats[m.chat].isBanned = false
+    m.reply('Done!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
 handler.command = /^unbanchat$/i
-handler.rowner = true
+handler.owner = true
+
 export default handler
