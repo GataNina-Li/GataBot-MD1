9b5752d1a62b666d7e7440a52f15fe5f345f8783
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-03 14:41:29 -0500

    Update fun-ppt.js

--- plugins/fun-ppt.js
+++ plugins/fun-ppt.js
@@ -1,5 +1,12 @@
 let handler = async (m, { text, usedPrefix }) => {
 let salah = `*_Hola 👋🏻, bienvenido al juego de piedra, papel o tijera_*\n\n*_Opciones Disponibles:_*\n*_piedra, papel o tijera_*\n\n*_${usedPrefix}ppt tijera_*\n\n*_Use las opciones en minusculas como se indica en el ejemplo, no use los _ ni los * para que el Bot lo detecte correctamente_*\n\n_©The Mystic - Bot_`
+
+conn.sendHydrated(m.chat, salah, wm, null, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙈𝙚𝙣𝙪 𝙅𝙪𝙚𝙜𝙤𝙨 | 𝙂𝙖𝙢𝙚𝙨 𝙈𝙚𝙣𝙪 🎡', if (text == 'piedra')],
+['𝙊𝙩𝙧𝙖 𝙫𝙚𝙯 | 𝘼𝙜𝙖𝙞𝙣 🤭',if (text == 'papel')],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', if (text == 'tijera')]
+], m
+                  
 if (!text) throw salah
 var astro = Math.random()
 if (astro < 0.34) {
