becba7b1db01a176b951e798ccd5f43c7ab5bb1e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-02 21:46:54 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 `Sin función`

--- plugins/gc-banchat.js
+++ plugins/gc-banchat.js
@@ -1,4 +1,4 @@
-let handler = async (m, { participants }) => {
+/*let handler = async (m, { participants }) => {
 global.db.data.chats[m.chat].isBanned = true
 m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n\n*—◉ 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚁𝙴𝙰𝙲𝙲𝙸𝙾𝙽𝙰𝚁𝙰 𝙰 𝙽𝙸𝙽𝙶𝚄𝙽 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙷𝙰𝚂𝚃𝙰 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃*')
 }
@@ -6,4 +6,4 @@ handler.help = ['banchat']
 handler.tags = ['group']
 handler.command = /^banchat$/i
 
-export default handler
+export default handler*/
