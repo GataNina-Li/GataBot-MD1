0a4a2b78b33e9c530be3408805d213c66a63f4d2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-02 20:17:17 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/gc-unbanchat.js
+++ plugins/gc-unbanchat.js
@@ -5,6 +5,5 @@ m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄�
 handler.help = ['unbanchat']
 handler.tags = ['group']
 handler.command = /^unbanchat$/i
-handler.group = true
-handler.admin = true
+
 export default handler
