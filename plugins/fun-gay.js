a23204e215641b67f2ccf8d8a39e82b8f3bfce35
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-02 22:47:54 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -4,7 +4,7 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {  
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),   
 }), 'error.png', `*🏳️‍🌈 𝙂𝘼𝙔 𝙂𝘼𝙔!! 🏳️‍🌈*`, m)   
-await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
+await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, { 
 type: 'audioMessage', 
 ptt: true })
 }
