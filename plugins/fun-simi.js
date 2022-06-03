09a8d433372ae66a69f97caf9ae242970b5fa847
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-02 22:49:51 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -5,7 +5,7 @@ let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
 let json = await res.json()
 let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
 //m.reply(`${tes}`) 
-conn.sendHydrated(m.chat, `${tes}`, `𝙎𝙞𝙢𝙎𝙞𝙢𝙞 | ${wm}`, null, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+conn.sendHydrated(m.chat, `${tes}`, `𝙎𝙞𝙢𝙎𝙞𝙢𝙞 | ${wm}`, null, null, null, null, null, [
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m)
 }
