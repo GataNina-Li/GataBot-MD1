5860af809edcd7f6d2ff0511378a089ac52c411a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-03 21:16:19 -0500

    Update fun-ppt.js

--- plugins/fun-ppt.js
+++ plugins/fun-ppt.js
@@ -25,8 +25,8 @@ astro = 'tijera'
 astro = 'papel'
 } 
 if (text == astro) {
-let { money } = global.db.data.users[m.sender]
-let mmoney = global.db.data.users[m.sender].money = 10
+//let { money } = global.db.data.users[m.sender]
+//let mmoney = global.db.data.users[m.sender].money = 10
   
 //m.reply(`*_🔰 Empate!_*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*`)
 conn.sendHydrated(m.chat, `╭━━━━[ 𝙀𝙈𝙋𝘼𝙏𝙀! 🤝 ]━━━━⬣\n┃👤 𝙐𝙨𝙩𝙚𝙙: ${text}\n┃🐱 𝙂𝙖𝙩𝙖𝘽𝙤𝙩: ${astro}\n┃💰 𝙍𝙚𝙘𝙤𝙢𝙥𝙚𝙣𝙨𝙖: $${mmoney} 𝙂𝙖𝙩𝙖𝘾𝙤𝙞𝙣𝙨\n┃💵 𝙎𝙪 𝘿𝙞𝙣𝙚𝙧𝙤: $${money} 𝙂𝙖𝙩𝙖𝘾𝙤𝙞𝙣𝙨\n╰━━━━━━[ ${vs} ]━━━━⬣`, wm, null, yt, '𝙔𝙤𝙪𝙏𝙪𝙗𝙚', null, null, [
