0b21eb6b490bf876edc03ddbf8c74aee64ce20be
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-05 17:03:43 -0500

    Update game-math_answer.js

--- plugins/game-math_answer.js
+++ plugins/game-math_answer.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
 let id = m.chat
 if (!m.quoted) return
 if (m.quoted.sender != conn.user.jid) return
-if (!/^𝙲𝚄𝙰𝙽𝚃𝙾 𝙴𝚂 𝙴𝙻 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴/i.test(m.quoted.text)) return
+if (!/^𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨/i.test(m.quoted.text)) return
 if (!(m.chat in global.math)) return conn.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚂𝙴 𝙰𝙷 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙸𝙳𝙾 𝙰 𝙴𝚂𝙰 𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰*', author, null, [['𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝙹𝚄𝙶𝙰𝚁', '/mates']], m)
 if (m.quoted.id == global.math[id][0].id) {
 let math = global.math[id][1]
