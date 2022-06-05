6069e0f4def35f58fdeb6b5409a5cc17493439fe
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-05 01:42:42 -0500

    Update fun-game-math.js

--- plugins/fun-game-math.js
+++ plugins/fun-game-math.js
@@ -23,7 +23,7 @@ if (!(mode in modes)) return conn.sendHydrated(m.chat, mat, author, null, null, 
 ['𝙼𝙰𝚃𝙴𝚂 𝙼𝙴𝙳𝙸𝚄𝙼', `${usedPrefix + command} medium`], 
 ['𝙼𝙰𝚃𝙴𝚂 𝙷𝙰𝚁𝙳', `${usedPrefix + command} hard`]], m)
 
-let mode = args[0].toLowerCase()
+let mode = args[1].toLowerCase()
 if (!(mode in modes)) return conn.sendHydrated(m.chat, null, author, null, null, null, null, null, [
 ['𝙼𝙰𝚃𝙴𝚂 𝙴𝙰𝚂𝚈', `${usedPrefix + command} easy`], 
 ['𝙼𝙰𝚃𝙴𝚂 𝙼𝙴𝙳𝙸𝚄𝙼', `${usedPrefix + command} medium`], 
