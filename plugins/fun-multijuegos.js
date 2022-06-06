36df2ce112b49fea77581d8273ed5ca2a3b3d10f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-06 15:16:23 -0500

    Update fun-multijuegos.js

--- plugins/fun-multijuegos.js
+++ plugins/fun-multijuegos.js
@@ -94,9 +94,9 @@ conn.sendMessage(m.chat, t, m)
  const templateButtonsReplyMessage = [
 {index: 1, urlButton: {displayText: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', url: 'https://github.com/GataNina-Li/GataBot-MD'}},
 {index: 2, urlButton: {displayText: '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', url: 'https://www.instagram.com/gata_dios/'}},
-{index: 3, quickReplyButton: {displayText: '✨', id: juegos}},
-{index: 4, quickReplyButton: {displayText: '🥳', id: juegos}},
-{index: 5, quickReplyButton: {displayText: '🐈', id: juegos2}},
+{index: 3, quickReplyButton: {displayText: `✨`, id: juegos}},
+{index: 4, quickReplyButton: {displayText: `🥳`, id: juegos}},
+{index: 5, quickReplyButton: {displayText: `🐈`, id: juegos2}},
 ]
  
 let tt = {
@@ -117,9 +117,9 @@ conn.sendMessage(m.chat, tt, m)
  const templateButtonsReplyMessage = [
 {index: 1, urlButton: {displayText: null, url: null}},
 {index: 2, urlButton: {displayText: null, url: null}},
-{index: 3, quickReplyButton: {displayText: '🍭', id: juegos}},
-{index: 4, quickReplyButton: {displayText: '🚀', id: juegos}},
-{index: 5, quickReplyButton: {displayText: '🪁', id: juegos}},
+{index: 3, quickReplyButton: {displayText: `🍭`, id: juegos}},
+{index: 4, quickReplyButton: {displayText: `🚀`, id: juegos}},
+{index: 5, quickReplyButton: {displayText: `🪁`, id: juegos}},
 ]
 let ttt = {
 text: sss,
