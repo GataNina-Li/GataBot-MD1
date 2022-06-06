e748073c434b2c49d228182593be72dd7c278440
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-06 15:09:06 -0500

    Update fun-multijuegos.js

--- plugins/fun-multijuegos.js
+++ plugins/fun-multijuegos.js
@@ -148,7 +148,7 @@ footer: global.wm,
 templateButtons: templateButtonsReplyMessage
   } 
 conn.sendMessage(m.chat, tttt, m)  
-}
+
  }
 handler.help = ['infomenu'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
