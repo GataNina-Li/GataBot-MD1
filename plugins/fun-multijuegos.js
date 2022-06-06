048decc3af4a8ab67f8c12655b8ce6887dc89efa
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-06 14:48:52 -0500

    Update fun-multijuegos.js

--- plugins/fun-multijuegos.js
+++ plugins/fun-multijuegos.js
@@ -104,12 +104,12 @@ footer: global.wm,
 templateButtons: templateButtonsReplyMessage
 } 
 conn.sendMessage(m.chat, tt, m)
-}   
+ 
     
     
  if (command == 'juegos') { 
            let c = `${usedPrefix}`
-           let juegos = `${pickRandom([`${c}reto`,`${c}verdad`])}`
+           //let juegos = `${pickRandom([`${c}reto`,`${c}verdad`])}`
 
            let sss = await `Use los botones`
 
@@ -126,11 +126,11 @@ footer: global.wm,
 templateButtons: templateButtonsReplyMessage
 } 
 conn.sendMessage(m.chat, ttt, m)
-}   
+
       
    if (command == 'juegos') {  
                  let c = `${usedPrefix}`
-                 let juegos = `${pickRandom([`${c}reto`,`${c}verdad`])}`
+                 //let juegos = `${pickRandom([`${c}reto`,`${c}verdad`])}`
          
                  let ssss = await `Use los botones`
 
@@ -152,7 +152,7 @@ conn.sendMessage(m.chat, tttt, m)
 handler.help = ['infomenu'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = ['multijuegos', 'juegos']
-handler.exp = 50
+handler.exp = 500
 export default handler
                               
 const more = String.fromCharCode(8206)
