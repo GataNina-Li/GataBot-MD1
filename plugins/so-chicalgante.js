0e03da677813f2a7769b7c1b0b03a8bcc315af5e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-08 13:17:11 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/so-chicalgante.js
@@ -0,0 +1,14 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+conn.sendFile(m.chat, vn, 'chica lgante.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true
+})
+}
+handler.customPrefix = /Me olvide|ME OLVIDE|me olvide|Me olvidé|me olvidé|ME OLVIDÉ/ 
+handler.command = new RegExp
+export default handler
+
