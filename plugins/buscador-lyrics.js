e1202baa011d27d4e3f8ddd0f03f13f13e5b183c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-08 01:23:22 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -35,5 +35,6 @@ conn.sendHydrated(m.chat, letratexto, wm, json.thumbnail.genius, null, null, nul
 handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
 handler.tags = ['internet']
 handler.command = /^(lirik|lyrics|lyric|letra)$/i
+handler.limit = 2
 handler.exp = 55
 export default handler
