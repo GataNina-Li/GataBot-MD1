2a6b0e6f33627a54f247f89615136213ec7aa17f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-08 15:08:27 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/so-murio el grupo.js
@@ -0,0 +1,17 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 75
+  
+let vn = './media/Murio.m4a'
+conn.sendFile(m.chat, vn, 'Murio.m4a', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Murió el grupo|Murio el grupo|murio el grupo|murió el grupo|Grupo muerto|grupo muerto/
+handler.command = new RegExp
+export default handler
