87d409e6b7a294ffa23cde53372303721decc681
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-08 13:44:02 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-chicalgante.js
+++ plugins/so-chicalgante.js
@@ -3,6 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
 let vn = './media/chica lgante.mp3'
 conn.sendFile(m.chat, vn, 'chica lgante.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -11,6 +12,5 @@ ptt: true
 }
 handler.customPrefix = /Me olvide|ME OLVIDE|me olvide|Me olvidé|me olvidé|ME OLVIDÉ/ 
 handler.command = new RegExp
-handler.money = 100
 export default handler
 
