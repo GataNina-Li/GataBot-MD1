eb02dd4eadb410276ab70c0a67e990c951d75ae6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-07 19:38:27 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -45,6 +45,7 @@ else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾�
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+handler.money = 20
 export default handler
 
 const isUrl = (text) => {
