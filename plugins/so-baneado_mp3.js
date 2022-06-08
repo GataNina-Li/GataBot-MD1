d558f12da33b75e67367512be7fff091742f99b8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-08 15:00:53 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-baneado_mp3.js
+++ plugins/so-baneado_mp3.js
@@ -3,6 +3,8 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 40
+global.db.data.users[m.sender].exp += 20
 let vn = './media/baneado.mp3'
 conn.sendFile(m.chat, vn, 'baneado.mp3', null, m, true, {
 type: 'audioMessage', 
