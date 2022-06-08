1bce7e6efa1dcd74d001d5901bf9a5dd3de726fa
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-08 18:35:25 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-masivo-cancion_mp3.js
+++ plugins/so-masivo-cancion_mp3.js
@@ -1,6 +1,6 @@
 import util from 'util'
 import path from 'path'
-
+ 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 70
