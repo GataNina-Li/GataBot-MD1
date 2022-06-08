02ea0d08681c8b30b02c8cabd8f1c707c622c5e4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-08 01:33:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-shop2.js
+++ plugins/rpg-shop2.js
@@ -1,4 +1,4 @@
-const xpperlimit = 100
+const xpperlimit = 300
 let handler = async (m, { conn, command, args, usedPrefix }) => {
   let count = command.replace(/^buy2/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
@@ -9,17 +9,19 @@ let handler = async (m, { conn, command, args, usedPrefix }) => {
     //conn.reply(m.chat, `
     let gata = `
 ╭━━〔 *DATOS DE COMPRA* 〕━━⬣
-‣ *Compra Efectuada* : +${count} 💎 
-‣ *Ha Gastado* :  -${xpperlimit * count} 𝙂𝘼𝙏𝘼𝘾𝙊𝙄𝙉𝙎
+┃ *Compra Efectuada* : +${count} 💎 
+┃ *Ha Gastado* :  -${xpperlimit * count} 𝙂𝘼𝙏𝘼𝘾𝙊𝙄𝙉𝙎
 ╰━━━━━〔 *𓃠 ${vs}* 〕━━━━⬣`.trim()
     
 await conn.sendHydrated(m.chat, gata, wm, null, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙂𝙍𝘼𝘾𝙄𝘼𝙎!', '.top'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+['💵 𝘾𝙤𝙢𝙥𝙧𝙖𝙧 𝙓10', '.buy2 10'],
+['💸 𝘾𝙤𝙢𝙥𝙧𝙖𝙧 𝙓20', '.buy2 20'],
+['⚡ 𝘾𝙤𝙢𝙥𝙧𝙖𝙧 𝙘𝙤𝙣 𝙀𝙭𝙥𝙚𝙧𝙞𝙚𝙣𝙘𝙞𝙖', '/buy']
 ], m,)
     
   } else //conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m)
-  await conn.sendHydrated(m.chat, `No tiene sufuciente 𝙂𝘼𝙏𝘼𝘾𝙊𝙄𝙉𝙎 para comprar *${count}* Diamantes 💎\n\nLe recomiendo que interactúe con GataBot-MD para Obtener GataCoins, puede ver su experiencia con el comando ${usedPrefix}exp o ${usedPrefix}experiencia`, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+    await conn.sendHydrated(m.chat, `No tiene sufuciente 𝙂𝘼𝙏𝘼𝘾𝙊𝙄𝙉𝙎 para comprar *${count} Diamantes* 💎\n\nLe recomiendo que interactúe con GataBot-MD para Obtener GataCoins, puede ver sus GataCoins con el comando ${usedPrefix}gatacoins o ${usedPrefix}experiencia. También puede comprar con su Experiencia con el Comando ${usedPrefix}buy`, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+['🐈 𝘾𝙤𝙢𝙥𝙧𝙖𝙧 𝙘𝙤𝙣 𝙀𝙭𝙥𝙚𝙧𝙞𝙚𝙣𝙘𝙞𝙖', '/buy'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)
 }
