dea44868deb36c07bdac669f084959d345ae1b60
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-10 17:05:06 -0500

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -16,8 +16,10 @@ let handler = async (m, { conn }) => {
   let chat = global.db.data.chats[m.chat]
   if (!chat.isBanned) return m.reply('*Este chat no está baneado!!*')
   chat.isBanned = false
-  m.reply('*✅ Listo... chat desbaneado!*')
+  m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝙊 🥳\n𝘼𝙃𝙊𝙍𝘼 𝙎𝙄 𝙀𝙎𝙏𝙊𝙔 𝘿𝙄𝙎𝙋𝙊𝙉𝙄𝘽𝙇𝙀.\n\n𝙏𝙃𝙄𝙎 𝘾𝙃𝘼𝙏 𝙒𝘼𝙎 𝙐𝙉𝘽𝘼𝙉 😌\n𝙉𝙊𝙒 𝙄 𝘼𝙈 𝘼𝙑𝘼𝙄𝙇𝘼𝘽𝙇𝙀.`)
 }
 handler.command = /^unbanchat$/i
+handler.botAdmin = true
 handler.admin = true
+
 export default handler
