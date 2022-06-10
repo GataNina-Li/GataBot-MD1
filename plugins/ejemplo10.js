a7afda36fd2d403ef01a04101d7c4bc2ced807e7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-10 17:29:00 -0500

    Create ejemplo10.js

---
+++ plugins/ejemplo10.js
@@ -0,0 +1,182 @@
+import fs from 'fs'
+let handler = async (m, { conn, command, text }) => { 
+let pp = './media/menus/Menu1.mp4' //'./Menu2.jpg'
+if (command == 'nidos') {
+let ejemplo = `🔰 𝐍𝐈𝐃𝐎𝐒 • 𝐆𝐄𝐍𝐄𝐑𝐀𝐂𝐈𝐎𝐍 𝐈 🔰`
+
+await conn.sendHydrated(m.chat, ejemplo, null, pp, null, null, null, null, [
+['Caterpie', `.nido caterpie`],
+['Ekans', `.nido ekans`],
+['Pikachu', `.nido pikachu`]], m,)
+
+await conn.sendHydrated(m.chat, null, wm, null, null, null, null, null, [
+['Jigglypuf', `.nido jigglypuf`],
+['Psyduck', `.nido psyduck`],
+['Growlithe', `.nido growlithe`]], m,) 
+ 
+await conn.sendHydrated(m.chat, null, wm, null, null, null, null, null, [ 
+['Abra', `.nido abra`],
+['Drowzee', `.nido drowzee`],
+['Exeggcute', `.nido exeggcute`]], m,)
+ 
+await conn.sendHydrated(m.chat, null, wm, null, null, null, null, null, [  
+['Cubone', `.nido cubone`],
+['Pinsir', `.nido pinsir`],
+['Eevee', `.nido eevee`]], m,) }
+
+if (command == 'nido') {
+ if (text == 'caterpie') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+  
+if (command == 'nido') {
+ if (text == 'ekans') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'pikachu') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'jigglypuf') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'psyduck') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'growlithe') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'abra') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'drowzee') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'exeggcute') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'cubone') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'pinzir') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}
+
+if (command == 'nido') {
+ if (text == 'eevee') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola`) }
+}}
+ 
+handler.help = ['nidos', 'nido']
+handler.tags = ['ejemplo']
+handler.command = ['nidos', 'nido']
+
+export default  handler
+  
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s, p].map(v => v.toString().padStart(2, 0)).join(':')}
