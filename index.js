767e51e07849c810cf08627cb9c7a2530db96127
The whatBots420 <105924903+Subbotsito@users.noreply.github.com>
2022-06-11 11:10:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('the whatBots420', {
+say('the Lolibot-𝙈𝘿', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
