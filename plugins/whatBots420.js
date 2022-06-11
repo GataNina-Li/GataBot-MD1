437e15c3142461de92e0bb6e02258f7dc19f4681
The whatBots420 <105924903+Subbotsito@users.noreply.github.com>
2022-06-11 09:11:39 -0300

    Update whatBots420.js

--- plugins/whatBots420.js
+++ plugins/whatBots420.js
@@ -4,7 +4,7 @@ let json = await res.json()
 let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
 m.reply(`${tes}`)
 }
-handler.command = ['whatboys420', '_']
+handler.command = ['lolibot MD', '_']
 handler.tags = ['fun']
 handler.help = ['_']
 
