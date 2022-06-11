e636f64baad0386963a04515493c3b577c4e7d7a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-11 02:04:00 -0500

    Update ejemplo10.js

--- plugins/ejemplo10.js
+++ plugins/ejemplo10.js
@@ -17,150 +17,152 @@ conn.sendHydrated(m.chat, ejemplo, null, pp, null, null, null, null, [
 //['Cubone', `.nido cubone`],
 //['Pinsir', `.nido pinsir`],
 //['Eevee', `.nido eevee`]
-], m,) }
+], m,) 
 
 if (command == 'nido') {
  if (text == 'caterpie') {
- await conn.reply(m.chat, `text1`, m, {
+ 
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+  
+ //await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
   
 if (command == 'nido') {
  if (text == 'ekans') {
- await conn.reply(m.chat, `text2`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ //await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'pikachu') {
- await conn.reply(m.chat, `text3`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}}) 
+// await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'jigglypuf') {
- await conn.reply(m.chat, `text4`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
 sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+ //await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'psyduck') {
- await conn.reply(m.chat, `text5`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}}) 
+ //await m.reply(`Resultado 2`)}}
 //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'growlithe') {
- await conn.reply(m.chat, `text6`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+ //await m.reply(`Resultado 2`)}}
  //wait m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'abra') {
- await conn.reply(m.chat, `text7`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}}) 
+// await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'drowzee') {
- await conn.reply(m.chat, `text8`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}}) 
+// await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'exeggcute') {
- await conn.reply(m.chat, `text9`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}}) 
+// await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'cubone') {
- await conn.reply(m.chat, `text10`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}}) 
+// await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'pinzir') {
- await conn.reply(m.chat, `text11`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  
- await m.reply(`Resultado 2`)}}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
+// await m.reply(`Resultado 2`)}}
  //await m.reply(`hola`) }
 
 
 if (command == 'nido') {
  if (text == 'eevee') {
- await conn.reply(m.chat, `text12`, m, {
+conn.reply(m.chat, ejemplo, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}})  }}
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
  //await m.reply(`Resultado 2`)
  //await m.reply(`hola`) }
-
+ }
  
 handler.help = ['nidos', 'nido']
 handler.tags = ['ejemplo']
