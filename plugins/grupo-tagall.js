28f5a225b16dd8ea66468aa45f9a94bddc35b6a6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-11 20:59:42 -0500

    Update and rename gc-tagall.js to grupo-tagall.js

---
+++ plugins/grupo-tagall.js
@@ -0,0 +1,24 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let pesan = args.join` `
+let oi = `*ღ 𝙈𝙀𝙉𝙎𝘼𝙅𝙀:* ${pesan}`
+let teks = `𝙄𝙉𝙑𝙊𝘾𝘼𝙉𝘿𝙊 𝘼𝙇 𝙂𝙍𝙐𝙋𝙊\n\n❏ ${oi}\n\n`
+for (let mem of participants) {
+teks += `┃➥ @${mem.id.split('@')[0]}\n╰━━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`}
+//conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+  
+conn.sendHydrated(m.chat, teks, `𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨 | ${wm}`, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙄𝙣𝙫𝙤𝙘𝙖𝙧 𝙤𝙩𝙧𝙖 𝙫𝙚𝙯 📣', `${usedPrefix + command}`]
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘', '.menu']
+], m, { mentions: participants.map(a => a.id) }) 
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.botAdmin = true
+handler.admin = true
+handler.group = true
+export default handler
