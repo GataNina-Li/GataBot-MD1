723388d6539494a4ea17a574e34b9221f21beb56
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 16:35:21 -0500

    Update and rename gc-kick.js to grupo-kick.js

---
+++ plugins/grupo-kick.js
@@ -0,0 +1,21 @@
+import { areJidsSameUser } from '@adiwajshing/baileys'
+let handler = async (m, { conn, participants }) => {
+if (!global.db.data.settings[conn.user.jid].restrict) throw `${ag} 𝙀𝙎𝙏𝘼 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊\n#on restrict | #off restrict\n𝙀𝙇/𝙇𝘼 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊(𝘼) 𝘿𝙀𝘽𝙀 𝘼𝘾𝙏𝙄𝙑𝘼𝙍 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊\n\n 𝙊𝙒𝙉𝙀𝙍 𝙈𝙐𝙎𝙏 𝘼𝘾𝙏𝙄𝙑𝘼𝙏𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿\n#on restrict | #off restrict`
+let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
+let kickedUser = []
+for (let user of users)
+if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
+const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+kickedUser.concat(res) 
+await delay(1 * 1000)
+}}
+handler.help = ['kick @user']
+handler.tags = ['group']
+handler.command = /^(kick|sacar|\-)$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+
