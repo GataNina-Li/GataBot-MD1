98e81d90209bc4164db4348d271c5899b8874487
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:20:41 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-marica tu_mp3.js
+++ plugins/so-marica tu_mp3.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/cancion.mp3'
 conn.sendFile(m.chat, vn, 'cancion.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -12,5 +15,4 @@ ptt: true
 handler.customPrefix = /Marica tu|cancion1|Marica quien/i
 handler.command = new RegExp
 handler.fail = null
-handler.exp = 100
 export default handler
