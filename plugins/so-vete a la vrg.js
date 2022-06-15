9e770bea6b7a5ce4bb053a96f64b52247d0485b9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:26:22 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-vete a la vrg.js
+++ plugins/so-vete a la vrg.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/vete a la verga.mp3'
 conn.sendFile(m.chat, vn, 'vete a la verga.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -12,6 +15,5 @@ ptt: true
 handler.customPrefix = /vetealavrg|vete a la vrg|vete a la verga/i
 handler.command = new RegExp
 handler.fail = null
-handler.exp = 100
 export default handler
 
