e43ce0e78f82f3aef77ad06864c1f4031b9591af
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:19:59 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-murio el grupo_mp3.js
+++ plugins/so-murio el grupo_mp3.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/Murio.m4a'
 conn.sendFile(m.chat, vn, 'Murio.m4a', null, m, true, {
 type: 'audioMessage', 
