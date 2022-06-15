36deb5b35a44e4556c486016c10db73ffc0b33e9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-15 01:46:53 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -4,16 +4,17 @@ let rtotalreg = Object.values(global.db.data.users).filter(user => user.register
 let user = global.db.data.users[m.sender]
 if (user.registered === true) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙷𝙴𝚈! 𝚈𝙰 𝙴𝚂𝚃𝙰𝚂 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾*\n\n*𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝚀𝚄𝙸𝚃𝙰𝚁 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾? 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}unreg <numero de serie>*\n\n*𝚂𝙸 𝙽𝙾 𝚁𝙴𝙲𝚄𝙴𝚁𝙳𝙰𝚂 𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝙿𝚄𝙴𝙳𝙴𝚂 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}myns*`
 let name = conn.getName(m.sender)
-let age = Math.floor(Math.random() * 41)
+//let age = Math.floor(Math.random() * 40)
+let age = `${pickRandom(['10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}`
 age = parseInt(age)
 user.name = name.trim()
 user.age = age
 user.regTime = + new Date
 user.registered = true
 let sn = createHash('md5').update(m.sender).digest('hex')
-let g = global.db.data.users[m.sender].money += 4000
-let d = global.db.data.users[m.sender].limit += 35
-let e = global.db.data.users[m.sender].exp += 5000
+global.db.data.users[m.sender].money += 4000
+global.db.data.users[m.sender].limit += 35
+global.db.data.users[m.sender].exp += 5000
 let caption = `
 ╭━━━[ *𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 | 𝙑𝙀𝙍𝙄𝙁𝙔* ]━━━━⬣
 ┃ *NOMBRE* 
@@ -23,9 +24,9 @@ let caption = `
 ┃ *${age} años*
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃ *BONO POR VERIFICACIÓN* 
-┃ *$${e} XP*
-┃ *$${g} GATACOINS*
-┃ *$ ${d} DIAMANTES*
+┃ *$5000 XP*
+┃ *$4000 GATACOINS*
+┃ *$35 DIAMANTES*
 ╰━━━━━━━━━━━━━━━━━━⬣`
 //let author = global.author
 conn.sendButton(m.chat, caption, `𝙏𝙐 𝙉𝙐𝙈𝙀𝙍𝙊 𝘿𝙀 𝙎𝙀𝙍𝙄𝙀 𝙏𝙀 𝙎𝙀𝙍𝙑𝙄𝙍𝘼 𝙀𝙉 𝘾𝘼𝙎𝙊 𝙌𝙐𝙀 𝙌𝙐𝙄𝙀𝙍𝘼 𝘽𝙊𝙍𝙍𝘼𝙍 𝙎𝙐 𝙍𝙀𝙂𝙄𝙎𝙏𝙍𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n${usedPrefix}unreg numero de serie\n${wm}`, [['𝘼𝙝𝙤𝙧𝙖 𝙚𝙨𝙩𝙤𝙮 𝙑𝙚𝙧𝙞𝙛𝙞𝙘𝙖𝙙𝙤(𝙖)!!', '/profile']], m)
@@ -36,3 +37,6 @@ handler.help = ['verificar']
 handler.tags = ['xp']
 handler.command = /^(verify|register|verificar)$/i
 export default handler
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
