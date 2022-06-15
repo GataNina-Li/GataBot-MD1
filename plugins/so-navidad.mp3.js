404ad09ae6d3155dfca4e88d2a221675b0dd97c1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:21:24 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-navidad.mp3.js
+++ plugins/so-navidad.mp3.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/navidad.m4a'
 conn.sendFile(m.chat, vn, 'navidad.m4a', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
