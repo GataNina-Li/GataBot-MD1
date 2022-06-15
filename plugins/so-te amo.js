acc6a17e074b6390e8278099794d4bfd9b6dccb3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:28:55 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-te amo.js
+++ plugins/so-te amo.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/Te-amo.mp3'
 conn.sendFile(m.chat, vn, 'Te-amo.mp3', null, m, true, {
 type: 'audioMessage',
@@ -12,7 +15,6 @@ ptt: true
 handler.customPrefix = /te amo|teamo/i
 handler.command = new RegExp
 handler.fail = null
-handler.exp = 100
 export default handler
 
 
