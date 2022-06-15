f52baa055ba2904c752afd041eebf45019c0eb5f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:22:11 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-noche de paz.js
+++ plugins/so-noche de paz.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/Noche.mp3'
 conn.sendFile(m.chat, vn, 'Noche.mp3', null, m, true, {
 type: 'audioMessage', 
