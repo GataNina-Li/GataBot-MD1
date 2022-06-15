71eaeee5231a5273bb67bb498775034de7993619
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:33:17 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-vivan los novios.js
+++ plugins/so-vivan los novios.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/vivan.mp3'
 conn.sendFile(m.chat, vn, 'vivan.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -13,7 +16,7 @@ handler.customPrefix = /vivan!!|vivan los novios|vivanlosnovios/i
 handler.command = new RegExp
 
 handler.fail = null
-handler.exp = 100
+
 export default handler
 
 
