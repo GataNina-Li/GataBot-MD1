7776a4be3bb97388796c833cd54f828e8713d309
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-15 01:54:55 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto'
 let handler = async function (m, { text, usedPrefix }) {
 let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
 let user = global.db.data.users[m.sender]
-if (user.registered === true) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙷𝙴𝚈! 𝚈𝙰 𝙴𝚂𝚃𝙰𝚂 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾*\n\n*𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝚀𝚄𝙸𝚃𝙰𝚁 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾? 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}unreg <numero de serie>*\n\n*𝚂𝙸 𝙽𝙾 𝚁𝙴𝙲𝚄𝙴𝚁𝙳𝙰𝚂 𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝙿𝚄𝙴𝙳𝙴𝚂 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}myns*`
+if (user.registered === true) throw `YA ESTAS REGISTRADO(A)\n\nSI QUIERE ANULAR SU REGISTRO USE ESTE COMANDO\n*${usedPrefix}unreg numero de serie*\n\nSI NO RECUERDA SU NUMERO DE SERIE USE ESTE COMANDO\n*${usedPrefix}myns* [❗𝐈`
 let name = conn.getName(m.sender)
 //let age = Math.floor(Math.random() * 40)
 let age = `${pickRandom(['10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}`
@@ -23,19 +23,19 @@ let caption = `
 ┃ *EDAD* 
 ┃ *${age} años*
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *BONO POR VERIFICACIÓN* 
+┃ *BONO* 
 ┃ *$5000 XP*
 ┃ *$4000 GATACOINS*
 ┃ *$35 DIAMANTES*
 ╰━━━━━━━━━━━━━━━━━━⬣`
 //let author = global.author
-conn.sendButton(m.chat, caption, `𝙏𝙐 𝙉𝙐𝙈𝙀𝙍𝙊 𝘿𝙀 𝙎𝙀𝙍𝙄𝙀 𝙏𝙀 𝙎𝙀𝙍𝙑𝙄𝙍𝘼 𝙀𝙉 𝘾𝘼𝙎𝙊 𝙌𝙐𝙀 𝙌𝙐𝙄𝙀𝙍𝘼 𝘽𝙊𝙍𝙍𝘼𝙍 𝙎𝙐 𝙍𝙀𝙂𝙄𝙎𝙏𝙍𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n${usedPrefix}unreg numero de serie\n${wm}`, [['𝘼𝙝𝙤𝙧𝙖 𝙚𝙨𝙩𝙤𝙮 𝙑𝙚𝙧𝙞𝙛𝙞𝙘𝙖𝙙𝙤(𝙖)!!', '/profile']], m)
+conn.sendButton(m.chat, caption, `𝙏𝙐 𝙉𝙐𝙈𝙀𝙍𝙊 𝘿𝙀 𝙎𝙀𝙍𝙄𝙀 𝙏𝙀 𝙎𝙀𝙍𝙑𝙄𝙍𝘼 𝙀𝙉 𝘾𝘼𝙎𝙊 𝙌𝙐𝙀 𝙌𝙐𝙄𝙀𝙍𝘼 𝘽𝙊𝙍𝙍𝘼𝙍 𝙎𝙐 𝙍𝙀𝙂𝙄𝙎𝙏𝙍𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n${usedPrefix}unreg numero de serie\n${wm}`, [['𝘼𝙝𝙤𝙧𝙖 𝙚𝙨𝙩𝙤𝙮 𝙑𝙚𝙧𝙞𝙛𝙞𝙘𝙖𝙙𝙤(𝙖)!! ✅', '/profile']], m)
 m.reply(`${sn}`) 
 
 }
 handler.help = ['verificar']
 handler.tags = ['xp']
-handler.command = /^(verify|register|verificar)$/i
+handler.command = /^(verify|register|verificar|registrar|verificado|verificada)$/i
 export default handler
 
 function pickRandom(list) {
