05b21a17d5f8bc41adabe52cec46f380fa1e7638
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:11:21 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-diagnosticadocongay_mp3.js
+++ plugins/so-diagnosticadocongay_mp3.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/DiagnosticadoConGay.mp3'
 conn.sendFile(m.chat, vn, 'DiagnosticadoConGay.mp3', null, m, true, {
 type: 'audioMessage',
@@ -12,5 +15,4 @@ ptt: true
 handler.customPrefix = /giagnosticadocongay|diagnosticado con gay|diagnosticado gay|te diagnóstico con gay|diagnóstico gay|te diagnostico con gay|te diagnóstico con gay|te diagnosticó con gay|te diagnostico con gay/i
 handler.command = new RegExp
 handler.fail = null
-handler.exp = 100
 export default handler
