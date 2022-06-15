8826560a7d2021a446c58c7325b91a2cb6b7a26b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-15 01:55:25 -0500

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto'
 let handler = async function (m, { text, usedPrefix }) {
 let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
 let user = global.db.data.users[m.sender]
-if (user.registered === true) throw `YA ESTAS REGISTRADO(A)\n\nSI QUIERE ANULAR SU REGISTRO USE ESTE COMANDO\n*${usedPrefix}unreg numero de serie*\n\nSI NO RECUERDA SU NUMERO DE SERIE USE ESTE COMANDO\n*${usedPrefix}myns* [❗𝐈`
+if (user.registered === true) throw `YA ESTAS REGISTRADO(A)\n\nSI QUIERE ANULAR SU REGISTRO USE ESTE COMANDO\n*${usedPrefix}unreg numero de serie*\n\nSI NO RECUERDA SU NUMERO DE SERIE USE ESTE COMANDO\n*${usedPrefix}myns`
 let name = conn.getName(m.sender)
 //let age = Math.floor(Math.random() * 40)
 let age = `${pickRandom(['10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}`
