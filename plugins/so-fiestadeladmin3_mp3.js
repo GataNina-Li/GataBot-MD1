cf28307983ddf6752e0ad9a31a1e85c1db539e97
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:15:16 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-fiestadeladmin3_mp3.js
+++ plugins/so-fiestadeladmin3_mp3.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/Fiesta1.mp3'
 conn.sendFile(m.chat, vn, 'Fiesta1.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -12,6 +15,5 @@ ptt: true
 handler.customPrefix = /fiesta del admin 3|atención grupo|atencion grupo|aviso importante|fiestadeladmin3|fiesta en casa de uriel/i
 handler.command = new RegExp
 handler.fail = null
-handler.exp = 100
 export default handler
 
