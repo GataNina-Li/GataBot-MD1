5a5751439cfd3602e8ee53c927c46e8468f0c36a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:12:55 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-feliz cumple_mp3.js
+++ plugins/so-feliz cumple_mp3.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/Feliz cumple.mp3'
 conn.sendFile(m.chat, vn, 'Feliz cumple.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -12,5 +15,4 @@ ptt: true
 handler.customPrefix = /feliz cumpleaños|felizcumpleaños|happy birthday/i
 handler.command = new RegExp
 handler.fail = null
-handler.exp = 100
 export default handler
