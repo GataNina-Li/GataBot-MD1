c24aa89478ba38bc9603ddc21e580c30359af289
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-14 23:28:13 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-un pato.js
+++ plugins/so-un pato.js
@@ -3,6 +3,9 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
 let vn = './media/pato.mp3'
 conn.sendFile(m.chat, vn, 'pato.mp3', null, m, true, {
 type: 'audioMessage', 
