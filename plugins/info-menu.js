4e79b91e9cb8c0ab40eefffd94e37e8e0c9120ba
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-15 20:03:24 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/info-menu.js
+++ plugins/info-menu.js
@@ -80,8 +80,8 @@ let pp = './media/menus/Menuvid1.mp4'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 let username = conn.getName(who)
-let user = global.db.data.users[m.sender]
-user.registered = false
+//let user = global.db.data.users[m.sender]
+//user.registered = false
 
 let menu = `
 💗 *¡HOLA | HI!* ${username}
@@ -119,7 +119,7 @@ conn.sendHydrated(m.chat, menu, wm, pp, 'https://github.com/GataNina-Li/GataBot-
 handler.help = ['infomenu'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^(infomenu)$/i
-handler.register = true
+//handler.register = true
 export default handler
 
 const more = String.fromCharCode(8206)
