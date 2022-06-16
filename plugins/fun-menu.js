ceb6b414242d45d5bfd462e8d6dad27815a097da
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-15 20:02:18 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/fun-menu.js
+++ plugins/fun-menu.js
@@ -65,8 +65,8 @@ let pp = './media/menus/Menuvid1.mp4'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 let username = conn.getName(who)
-let user = global.db.data.users[m.sender]
-user.registered = false
+//let user = global.db.data.users[m.sender]
+//user.registered = false
 
 let menu = `
 💗 *¡HOLA | HI!* ${username}
@@ -124,7 +124,7 @@ conn.sendHydrated(m.chat, menu, wm, pp, 'https://github.com/GataNina-Li/GataBot-
 handler.help = ['infomenu'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^(juegosmenu)$/i
-handler.register = true
+//handler.register = true
 handler.exp = 50
 export default handler
 
