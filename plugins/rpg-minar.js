8443632497ed85ce05c767feed86d3abe65cf466
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-16 00:04:55 -0500

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -1,4 +1,4 @@
-import { performance } from 'perf_hooks'
+//import { performance } from 'perf_hooks'
 let handler = async (m, { conn, isPrems}) => { //lastmiming
 let minar = `${pickRandom(['Que pro 😎 has minado','Genial!! Obtienes','WOW!! eres un(a) gran Minero(a), Obtienes','Felicidades!! Ahora tienes','⛏️⛏️⛏️ Obtienes'])}`
 //let boost = `${pickRandom(['https://us.123rf.com/450wm/gioiak2/gioiak21707/gioiak2170700462/82888173-el-casco-el-pico-y-la-pala-del-minero-aislados-en-fondo-negro-ilustraci%C3%B3n-3d.jpg?ver=6','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}`  
@@ -31,7 +31,8 @@ minutes = (minutes < 10) ? "0" + minutes : minutes
 seconds = (seconds < 10) ? "0" + seconds : seconds
 
 return minutes + " m y " + seconds + " s " 
-  
+}  
+
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
-}
+
