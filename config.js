4d7832e353453667d0c277cfe4246fbe495f9382
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-16 01:27:59 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -140,7 +140,7 @@ global.wait = '*⌛ _Cargando..._ ▬▬▬▭*'
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
-global.multiplier = 120 // Cuanto más alto, más difícil subir de nivel | The higher, The harder levelup
+global.multiplier = 200 // Cuanto más alto, más difícil subir de nivel | The higher, The harder levelup
 
 global.rpg = {
   emoticon(string) {
