b7188423f303194cb6131557f37e1e5c14fa1a8a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-15 20:03:48 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -17,7 +17,7 @@ teks += `╰━━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
 conn.sendHydrated(m.chat, teks, `𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨 | ${wm}`, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
 ['𝙄𝙣𝙫𝙤𝙘𝙖𝙧 𝙤𝙩𝙧𝙖 𝙫𝙚𝙯 📣', `${usedPrefix + command}`],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘', '.menu']
-], m, { mentions: participants.map(a => a.id) }) 
+], m, { mentions: participants.map(a => a.id) })  
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
