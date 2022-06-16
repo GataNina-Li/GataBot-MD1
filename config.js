a5eb065d108a40eff5d08e4e7518f226e848f8af
diego16sdlg <101654213+diego16sdlg@users.noreply.github.com>
2022-06-16 00:33:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url'
 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
-  ['972557295696', 'GataBot-MD 💖🐈', true],
+  ['79605005720', 'GataBot-MD 💖🐈', true],
   ['48459061284'],
   ['51961088604'],
   ['51902257100'],
