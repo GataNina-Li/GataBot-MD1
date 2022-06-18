ce0458c47cdd59f1d4633f42690c6b362bab0da7
The whatBots420 <105924903+Subbotsito@users.noreply.github.com>
2022-06-18 14:20:48 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -88,6 +88,9 @@ global.gatabot = gt
 global.yt = 'https://youtube.com/channel/UC9b3UIVnVb5eQt7_Y-Qz-3A'
 global.youtube = yt
 
+global.ig = 'https://www.instagram.com/gata_dios'
+global.gatadiosig = ig
+
 global.md = 'https://github.com/Subbotsito/The-LoliBot-MD'
 global.gatabot = md
 
