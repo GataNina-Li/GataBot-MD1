80c61155eb9a1f4d7e8022fc04ee535b7b26ffa6
continuars <107812849+continuars@users.noreply.github.com>
2022-06-19 14:54:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url'
 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
-  ['593993684821', 'GataBot-MD 💖🐈', true],
+  ['529515782470', 'GataBot-MD 💖🐈', true],
   ['593968585383'],
   ['51941819582'],
   ['51930583082'],
