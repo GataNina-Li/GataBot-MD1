99cd92dbbe2e9c8e0b07da39e45d945557372079
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-06-26 01:21:43 -0300

    Update rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -2,32 +2,42 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 
 let handler = async (m, { conn }) => {
-	let name = conn.getName(m.sender)
-    let user = global.db.data.users[m.sender]
+let { role } = global.db.data.users[m.sender]
+let name = conn.getName(m.sender)
+let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
         throw `
-┌───⊷ *NIVEL*
-▢ Nombre : *${name}*
-▢ Nivel : *${user.level}*
-▢ XP : *${user.exp - min}/${xp}*
-└──────────────
+╭━━━[ *𝙉𝙄𝙑𝙀𝙇 | 𝙇𝙀𝙑𝙀𝙇* ]━━━━⬣
+┃ *NOMBRE | NAME*
+┃ ${name}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *NIVEL:* *${user.level}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *RANGO:* ${role}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *XP:* *${user.exp - min}/${xp}*
+╰━━━〔 *𓃠 ${vs}* 〕━━━━━⬣
 
-Te falta *${max - user.exp}* de *XP* para subir de nivel
+*Te falta ${max - user.exp} de XP para subir de nivel*
 `.trim()
     }
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-        let teks = `🎊 Bien hecho ${conn.getName(m.sender)}    Nivel: ${user.level}`
+        let teks = `Bien hecho! ${conn.getName(m.sender)} Nivel: ${user.level}`
         let str = `
-┌─⊷ *LEVEL UP*
-▢ Nivel anterior : *${before}*
-▢ Nivel actual : *${user.level}*
-*Fecha:* ${new Date().toLocaleString('id-ID')}
-└──────────────
+╭━━━[ *𝙉𝙄𝙑𝙀𝙇 | 𝙇𝙀𝙑𝙀𝙇* ]━━━━⬣
+┃ *NIVEL ANTERIOR:* *${before}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *NIVEL ACTUAL:* *${user.level}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *RANGO:* ${role}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ *FECHA:* *${new Date().toLocaleString('id-ID')}*
+╰━━━〔 *𓃠 ${vs}* 〕━━━━━⬣
 
-*_Cuanto más interactúes con los bots, mayor será tu nivel_*
+*_Cuanto más interactúes con GataBot-MD, mayor será tu nivel!!_*
 `.trim()
         try {
             const img = await levelup(teks, user.level)
