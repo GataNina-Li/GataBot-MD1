2bb7d362559956a539728c8733762f97c4f53dcb
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-06-26 07:29:25 -0300

    Update so-Anashe Momo.mp3.js

--- plugins/so-Anashe Momo.mp3.js
+++ plugins/so-Anashe Momo.mp3.js
@@ -12,6 +12,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /Nashe|anashei|anashe|anachei|nachei|😏🍔/i 
+handler.customPrefix = /😏🍔|anashe/i 
 handler.command = new RegExp
 export default handler
