e3e2bc3585c767b27f9ccdcf4713c3909b38419e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-05 15:46:46 -0500

    Update grupo-mencion.js

--- plugins/grupo-mencion.js
+++ plugins/grupo-mencion.js
@@ -1,10 +1,10 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
+/*let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*⚡ 𝑴𝒆𝒏𝒔𝒂𝒋𝒆*\n ${pesan}\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
+let oi = `*⚡ 𝑴𝒆𝒏𝒔𝒂𝒋𝒆*\n ${pesan}\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
 let teks = `╭━〔 *𝑨𝑻𝑬𝑵𝑪𝑰𝑶𝑵 𝑮𝑹𝑼𝑷𝑶* 〕━⬣\n\n${oi}\n\n`
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
@@ -25,4 +25,16 @@ handler.command = /^(zeus)$/i
 handler.botAdmin = true
 handler.admin = true
 handler.group = true
-export default handler
+export default handler*/
+
+JScriptCodeDom.CodeParseException: invalid token after export : Keyword:default , Line 28, Char 0 ---> System.Exception: invalid token after export : Keyword:default
+   at JScriptCodeDom.CodeParser.InternalParseStatementCore()
+   at JScriptCodeDom.CodeParser.InternalParseStatement()
+   at JScriptCodeDom.CodeParser.ParseStatement()
+   at JScriptCodeDom.CodeParser.ParseBlock()
+   at JScriptCodeDom.CodeParser.Parse(String code, String filename, Int32 lineoffset, Boolean keeplinenum)
+   --- End of inner exception stack trace ---
+   at JScriptCodeDom.CodeParser.Parse(String code, String filename, Int32 lineoffset, Boolean keeplinenum)
+   at ProtectorV1.Protect(String[] codes)
+   at ProtectorV1.Protect(String code)
+   at ASP.javascript_obfuscator_aspx.Button1_Click(Object sender, EventArgs e)
