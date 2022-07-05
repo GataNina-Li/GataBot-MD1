9e603da3b15c81f719ea6e70ef38dbee2f45bc63
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-05 15:00:03 -0500

    Create grupo-mencion.js

---
+++ plugins/grupo-mencion.js
@@ -0,0 +1,31 @@
+import MessageType from '@adiwajshing/baileys'
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+let handler = async (m, { conn, text, participants }) => {
+let users = participants.map(u => conn.decodeJid(u.id))
+let q = m.quoted ? m.quoted : m
+let c = m.quoted ? m.quoted : m.msg
+const msg = conn.cMod(m.chat,
+generateWAMessageFromContent(m.chat, {
+[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+text: c || ''
+}
+}, {
+quoted: m,
+userJid: conn.user.id
+}),
+text || q.text, conn.user.jid, { mentions: users }
+)
+//await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+conn.reply(m.chat, msg.message, { messageId: msg.key.id }, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+body: 'Super Bot WhatsApp',         
+previewType: 1, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `${yt}`}}})  
+}
+handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => v + ' [teks]')
+handler.tags = ['group']
+handler.command = /^(zeus)$/i
+handler.group = true
+handler.admin = true 
+export default handler
