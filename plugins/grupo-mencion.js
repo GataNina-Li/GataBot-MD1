131a21a7bb4ef6fde4818e0286cbf01a8a63f70f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-05 21:08:00 -0500

    Update grupo-mencion.js

--- plugins/grupo-mencion.js
+++ plugins/grupo-mencion.js
@@ -1,4 +1,4 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
+/*let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
 throw false
@@ -25,4 +25,31 @@ handler.command = /^(zeus)$/i
 handler.botAdmin = true
 handler.admin = true
 handler.group = true
+export default handler*/
+
+
+import MessageType from '@adiwajshing/baileys'
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+let handler = async (m, { conn, text, participants }) => {
+let users = participants.map(u => conn.decodeJid(u.id))
+let q = m.quoted ? m.quoted : m
+let c = m.quoted ? m.quoted : m.msg
+const msg = conn.cMod(m.chat,
+generateWAMessageFromContent(m.chat, {
+[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+text: c || ''
+}
+}, {
+quoted: m,
+userJid: conn.user.id
+}),
+text `Hola` || q.text, conn.user.jid, { mentions: users }
+)
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+}
+handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => v + ' [teks]')
+handler.tags = ['group']
+handler.command = /^(dinamica)$/i
+handler.group = true
+handler.admin = true 
 export default handler
