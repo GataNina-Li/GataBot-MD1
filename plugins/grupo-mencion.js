94ed1c7054b42a63a254360c828316560fea4f1f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-05 21:19:16 -0500

    Update grupo-mencion.js

--- plugins/grupo-mencion.js
+++ plugins/grupo-mencion.js
@@ -34,16 +34,12 @@ let handler = async (m, { conn, text, participants }) => {
 let users = participants.map(u => conn.decodeJid(u.id))
 let q = m.quoted ? m.quoted : m
 let c = m.quoted ? m.quoted : m.msg
-const msg = conn.cMod(m.chat,
-generateWAMessageFromContent(m.chat, {
-[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-text: c || 'Si '
-}
+const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : { text: c || '' }
 }, {
 quoted: m,
 userJid: conn.user.id
 }),
-text || q.text, conn.user.jid, { mentions: users }
+`${text} Hola` || q.text, conn.user.jid, { mentions: users }
 )
 await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
