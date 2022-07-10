153a747dd1a02723207ef1d5b1138be2f693119f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-10 11:22:09 -0500

    Update config-on y off.js

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -197,8 +197,8 @@ if (!/[01]/.test(command)) return await conn.sendMessage(m.chat, listMessage)
 throw false
 }
 conn.sendButton(m.chat, `${rg}*_COMANDO_* *|* ${type} 
-*_ACTUALMENTE*_ *|* ${isEnable ? '✅ ACTIVADO' : '❌ DESACTIVADO'} 
-*_EN ESTE*_ *|* ${isAll ? 'BOT' : isUser ? '' : 'CHAT'}`, wm, null, [[`${isEnable ? '🔕 DESACTIVAR' : '🔔 ACTIVAR'}`, `${isEnable ? `.off ${type}` : `.on ${type}`}`], ['👾 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 👾', '.menu']],m)}
+*_ACTUALMENTE_* *|* ${isEnable ? '✅ ACTIVADO' : '❌ DESACTIVADO'} 
+*_EN ESTE_* *|* ${isAll ? 'BOT' : isUser ? '' : 'CHAT'}`, wm, null, [[`${isEnable ? '🔕 DESACTIVAR' : '🔔 ACTIVAR'}`, `${isEnable ? `.off ${type}` : `.on ${type}`}`], ['👾 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 👾', '.menu']],m)}
 
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
