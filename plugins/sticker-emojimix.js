9778374f71c277b9b5a236ea4c471909dda35024
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-13 15:26:04 -0500

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -4,8 +4,7 @@ import fetch from 'node-fetch'
 import fs from "fs"
 let handler = async (m, { conn, text, args }) => {
 if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝙾 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 #emojimix <emoji 1>&<emoji 2>*\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*#emojimix 🤨&😣*'
-let [emoji1, emoji2] = text.split`&`
-let [emoji1, emoji2] = text.split`$`
+let [emoji1, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
 let stiker = await sticker(false, res.url, global.packname, global.author)
