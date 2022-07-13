f7443e063189a92d00d06e43c01b72bb7960c185
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-13 16:11:46 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/sticker-circulo.js
+++ plugins/sticker-circulo.js
@@ -2,7 +2,7 @@
 
 import uploadImage from '../lib/uploadImage.js'
 import { sticker } from '../lib/sticker.js'
-let handler = async (m, { conn, text, usedPerfix, command }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
@@ -12,7 +12,7 @@ let scircle = global.API('dzx', '/api/canvas/circle', { url })
 let stiker = await sticker(null, scircle, global.packname, global.author)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, { asSticker: true })
 } catch (e) {
-m.reply(`${fg}𝙍𝙀𝘾𝙐𝙀𝙍𝘿𝙀 𝙐𝙎𝘼𝙍 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 *${usedPerfix + command}* 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙄𝙀𝙉𝘿𝙊 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉, 𝙋𝘼𝙍𝘼 𝙃𝘼𝘾𝙀𝙍 𝙐𝙉 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝘾𝙄𝙍𝘾𝙐𝙇𝘼𝙍\n\n𝙍𝙀𝙈𝙀𝙈𝘽𝙀𝙍 𝙏𝙊 𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 *${usedPerfix + command}* 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙄𝙉𝙂 𝙏𝙊 𝘼𝙉 𝙄𝙈𝘼𝙂𝙀, 𝙏𝙊 𝙈𝘼𝙆𝙀 𝘼 𝘾𝙄𝙍𝘾𝙐𝙇𝘼𝙍 𝙎𝙏𝙄𝘾𝙆𝙀𝙍`)
+m.reply(`${fg}𝙍𝙀𝘾𝙐𝙀𝙍𝘿𝙀 𝙐𝙎𝘼𝙍 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 *${usedPrefix + command}* 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙄𝙀𝙉𝘿𝙊 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉, 𝙋𝘼𝙍𝘼 𝙃𝘼𝘾𝙀𝙍 𝙐𝙉 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝘾𝙄𝙍𝘾𝙐𝙇𝘼𝙍\n\n𝙍𝙀𝙈𝙀𝙈𝘽𝙀𝙍 𝙏𝙊 𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 *${usedPerfix + command}* 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙄𝙉𝙂 𝙏𝙊 𝘼𝙉 𝙄𝙈𝘼𝙂𝙀, 𝙏𝙊 𝙈𝘼𝙆𝙀 𝘼 𝘾𝙄𝙍𝘾𝙐𝙇𝘼𝙍 𝙎𝙏𝙄𝘾𝙆𝙀𝙍`)
 }}
 handler.command = /^scircle|circle|círculo|circulo|sircle|redondo|circular$/i
 export default handler
