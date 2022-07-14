31d4e0e237b3be70f60e2e313a3cac9acb9fb401
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-07-14 20:26:58 -0300

    Update convertidor-tovideo.js

--- plugins/convertidor-tovideo.js
+++ plugins/convertidor-tovideo.js
@@ -17,7 +17,7 @@ out = await ffmpeg(media, [
 '-shortest'
 ], 'mp3', 'mp4')
 }
-await conn.sendFile(m.chat, out, 'error.mp4', '*DONE*', m, 0, { thumbnail: out })
+await conn.sendFile(m.chat, out, 'error.mp4', '*LISTO | DONE*', m, 0, { thumbnail: out })
 }
 handler.help = ['tovideo']
 handler.tags = ['sticker']
