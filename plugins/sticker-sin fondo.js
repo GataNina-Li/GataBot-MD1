479d4bbe0ff0ffb33e8cab38eb2aba61db4d2722
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 13:42:36 -0500

    Update sticker-sin fondo.js

--- plugins/sticker-sin fondo.js
+++ plugins/sticker-sin fondo.js
@@ -12,5 +12,5 @@ conn.sendFile(m.chat, stickerr, 'sticker.webp', '', m, { asSticker: true })
 } catch (e) {
 m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾, 𝙽𝙾 𝙾𝙻𝚅𝙸𝙳𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙻𝙰 𝙲𝚄𝙰𝙻 𝚂𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁𝙰 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚂𝙸𝙽 𝙵𝙾𝙽𝙳𝙾*')
 }}
-handler.command = /^sremovebg|removebg$/i
+handler.command = /^sremovebg|removebg|ssinfondo|sinfondo$/i
 export default handler
