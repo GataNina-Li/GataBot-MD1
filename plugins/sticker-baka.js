5b1665dbc6221cd75f874cf311531f3e2c281a04
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 16:38:53 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -1,9 +1,13 @@
 import axios from "axios" 
+import fetch from 'node-fetch'
 let handler = async (m, {command, conn}) => {
 
-let res = await axios("https://source.unsplash.com/featured/?girl,woman")
-let json = res.data
-let url = json.url
+//let res = await axios("https://source.unsplash.com/featured/?girl,woman")
+//let json = res.data
+//let url = json.url
+let res = await fetch('https://source.unsplash.com/featured/?girl,woman')
+let json = await res.json()
+let { url } = json
 conn.sendButton(m.chat, `Chica`.trim(), wm, url, [['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `/${command}`]], m)}
 
 handler.help = ['dado']
