f7819a79197e39ed0466228cf7de90fa6981cfda
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 17:07:00 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -1,6 +1,6 @@
 import axios from "axios" 
 import fetch from 'node-fetch'
-let handler = async (m, {command, conn}) => {
+let handler = async (m, {command, conn, text}) => {
 
 if (command == 'chica') {
 let pp = 'https://source.unsplash.com/featured/?girl,woman'
@@ -12,11 +12,26 @@ conn.sendHydrated(m.chat, wm, null, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈�
 if (command == 'chico') {
 let pp = 'https://source.unsplash.com/featured/?boy,man'
 conn.sendHydrated(m.chat, wm, null, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `.chica`],
+['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `.chico`],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}  
+
+  
+if (command == 'clima') {
+  
+(`https://pt.wttr.in/${text}?format=Cidade%20=%20%l+\n\nEstado%20=%20%C+%c+\n\nTemperatura%20=%20%t+\n\nUmidade%20=%20%h\n\nVento%20=%20%w\n\nLua agora%20=%20%m\n\nNascer%20do%20Sol%20=%20%S\n\nPor%20do%20Sol%20=%20%s`)
+let pp = `https://wttr.in/${text}.png`
+conn.sendHydrated(m.chat, wm, null, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `.chico`],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)}  
+  
+  
+  const clima = await axios.get(`https://pt.wttr.in/${encodeURIComponent(Sliced_Body)}?format=Cidade%20=%20%l+\n\nEstado%20=%20%C+%c+\n\nTemperatura%20=%20%t+\n\nUmidade%20=%20%h\n\nVento%20=%20%w\n\nLua agora%20=%20%m\n\nNascer%20do%20Sol%20=%20%S\n\nPor%20do%20Sol%20=%20%s`)
+					await kill.sendFileFromUrl(from, `https://wttr.in/${encodeURIComponent(Sliced_Body)}.png`, '', mess.wttr(clima), id)
+  
 } 
-handler.help = ['chica', 'chico'] 
-handler.command = ['chica', 'chico'] 
+handler.help = ['chica', 'chico', 'clima'] 
+handler.command = ['chica', 'chico', 'clima'] 
 export default handler
 
