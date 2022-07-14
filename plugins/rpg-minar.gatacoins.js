7baf30567833f2aee40108135f72c0fa67a951b1
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-07-14 18:33:23 -0300

    Update rpg-minar.gatacoins.js

--- plugins/rpg-minar.gatacoins.js
+++ plugins/rpg-minar.gatacoins.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isPrems}) => { //lastmiming
 let minar = `${pickRandom(['Que pro 😎 has minado','🌟✨ Genial!! Obtienes','WOW!! eres un(a) gran Minero(a) ⛏️ Obtienes','Felicidades!! Ahora tienes','⛏️⛏️⛏️ Obtienes'])}`
-let pp = 'https://media.tvalacarta.info/programas/0/clantve/44-gatos/aida-la-gata-robot.jpg'
+//let pp = 'https://media.tvalacarta.info/programas/0/clantve/44-gatos/aida-la-gata-robot.jpg'
 
 let gata = Math.floor(Math.random() * 1000)
 global.db.data.users[m.sender].money += gata * 1  
@@ -8,7 +8,7 @@ global.db.data.users[m.sender].money += gata * 1
 let time = global.db.data.users[m.sender].lastcoins + 600000
 if (new Date - global.db.data.users[m.sender].lastcoins < 600000) throw `*💟 Vuelva en ${msToTime(time - new Date())} para continuar minando ⛏️*`  
 
-conn.sendHydrated(m.chat, `*${minar} ${gata} 𝙂𝙖𝙩𝙖𝘾𝙤𝙞𝙣𝙨*`, wm, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+conn.sendHydrated(m.chat, `*${minar} ${gata} 𝙂𝙖𝙩𝙖𝘾𝙤𝙞𝙣𝙨*`, wm, null, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
 ['𝙈𝙞𝙣𝙖𝙧 𝙀𝙓𝙋 ⚡', `.minar`],
 ['𝙈𝙞𝙣𝙖𝙧 𝘿𝙞𝙖𝙢𝙖𝙣𝙩𝙚𝙨 💎', `.minar3`],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `.menu`]
