dd73c2acd34c25e141de7ff0c743adea0ab91d3d
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-07-14 19:40:17 -0300

    Update grupo-telegram.js

--- plugins/grupo-telegram.js
+++ plugins/grupo-telegram.js
@@ -1,11 +1,11 @@
-//let media = './media/menus/telegramfutabuclub.jpg'
+let media = './media/menus/telegramfutabuclub.jpg'
 let handler = async (m, { conn, command }) => {
 let str = `
 Nuestro grupo de Telegram
 Link:https://t.me/FutabuClub
 `.trim()
   
-conn.sendHydrated(m.chat, str, wm, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}
 
