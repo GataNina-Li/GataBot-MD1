bbccd6fe9530ac13004dd2e08027ec9a836b9eae
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-07-14 19:45:44 -0300

    Update grupo-telegram.js

--- plugins/grupo-telegram.js
+++ plugins/grupo-telegram.js
@@ -6,6 +6,7 @@ Link:https://t.me/FutabuClub
 `.trim()
   
 conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙐𝙣𝙞𝙧𝙢𝙚 | 𝙅𝙤𝙞𝙣 🌟', 'https://t.me/FutabuClub']
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}
 
