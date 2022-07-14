d80d50ccaf43f198f106bba4179077982ccba084
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 16:36:50 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -1,28 +1,13 @@
-/*import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
-import MessageType from '@adiwajshing/baileys'
-let handler = async (m, { conn}) => {
-try {
-if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
-if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('https://source.unsplash.com/featured/?girl,woman')
-let json = await res.json()
-let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está alimentando a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
-} catch (e) { }}
-handler.command = /^(chicago)$/i 
-export default handler*/
+import axios from "axios" 
+let handler = async (m, {command, conn}) => {
 
+let res = await axios("https://source.unsplash.com/featured/?girl,woman")
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `Chica`.trim(), wm, url, [['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `/${command}`]], m)}
 
-const dir = [
-  'https://source.unsplash.com/featured/?girl,woman',
-  'https://source.unsplash.com/featured/?girl,woman',
-];
-let handler = async (m, { conn }) => {
-conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dadu.webp', '', m)
-}
 handler.help = ['dado']
 handler.tags = ['game']
 handler.command = ['chi'] 
 export default handler
+
