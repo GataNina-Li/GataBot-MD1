c9f0502993a31f1c470a0f70bd2042325535aaca
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 15:50:25 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/sticker-alimentar.js
+++ plugins/sticker-alimentar.js
@@ -8,8 +8,8 @@ if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
 let res = await fetch('https://nekos.life/api/v2/img/feed')
 let json = await res.json()
 let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
+let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está alimentando a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 conn.sendFile(m.chat, stiker, null, { asSticker: true })
 } catch (e) { }}
-handler.command = /^(alimentar)$/i 
+handler.command = /^(alimentar|food|alimentándose|alimentando|alimentar)$/i 
 export default handler
