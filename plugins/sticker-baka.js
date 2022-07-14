1fd896842a05f03bb621830ed4a14e8021387681
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 16:43:18 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -5,10 +5,14 @@ let handler = async (m, {command, conn}) => {
 //let res = await axios("https://source.unsplash.com/featured/?girl,woman")
 //let json = res.data
 //let url = json.url
-let res = await fetch('https://source.unsplash.com/featured/?girl,woman')
-let json = await res.json()
-let { url } = json
-conn.sendButton(m.chat, `Chica`.trim(), wm, url, [['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `/${command}`]], m)}
+//let res = await fetch('https://source.unsplash.com/featured/?girl,woman')
+//let json = await res.json()
+//let { url } = json
+//conn.sendButton(m.chat, `Chica`.trim(), wm, url, [['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `/${command}`]], m)}
+let pp = 'https://source.unsplash.com/featured/?girl,woman'
+conn.sendHydrated(m.chat, wm, null, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['Chica', `.chi`]
+], m,)}
 
 handler.help = ['dado']
 handler.tags = ['game']
