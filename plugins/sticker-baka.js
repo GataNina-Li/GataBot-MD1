4a6a3bd84e1107a46120b338028483b05040a331
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 16:13:47 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -1,22 +1,17 @@
 import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
 import MessageType from '@adiwajshing/baileys'
-/*let handler = async (m, { conn}) => {
+import axios from "axios" 
+let handler = async (m, { conn}) => {
 try {
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('http://api.nekos.fun:8080/api/baka')
+let res = await axios.get("http://api.nekos.fun:8080/api/baka")
+//let res = await fetch('http://api.nekos.fun:8080/api/baka')
 let json = await res.json()
 let { url } = json
 let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está diciendo baka a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 conn.sendFile(m.chat, stiker, null, { asSticker: true })
 } catch (e) { }}
-handler.command = /^(baka)$/i  
-export default handler*/
-import axios from "axios" 
-let handler = async (m, { conn}) => {
-let haha = await conn.getFile(`http://api.nekos.fun:8080/api/baka`)
-conn.sendButton(m.chat, `a`.trim(), wm, haha.data, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `.baka`]], m)}   
-
 handler.command = /^(baka)$/i  
 export default handler
