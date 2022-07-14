aec9f33341fe1f87c77e005bc1b886f60f6b415a
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-07-14 19:21:38 -0300

    Create grupo-telegram.js

---
+++ plugins/grupo-telegram.js
@@ -0,0 +1,14 @@
+//let media = './media/menus/Menuvid2.mp4'
+let handler = async (m, { conn, command }) => {
+let str = `
+Nuestro grupo de Telegram
+Link:https://t.me/FutabuClub
+`.trim()
+  
+conn.sendHydrated(m.chat, str, wm, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)}
+
+handler.command = /^telegram|grupodetelegram$/i
+handler.exp = 35
+export default handler
