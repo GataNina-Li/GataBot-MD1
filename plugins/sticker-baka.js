fd7a393c3a67e29f3faea5f02aa2fab28e40882a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 16:25:42 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -7,7 +7,7 @@ try {
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
 //let res = await axios.get("http://api.nekos.fun:8080/api/baka")
-let res = await fetch('https://nekos.fun/storage/baka/baka_009.gif')
+let res = await fetch('https://source.unsplash.com/featured/?girl,woman')
 let json = await res.json()
 let { url } = json
 let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está diciendo baka a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
