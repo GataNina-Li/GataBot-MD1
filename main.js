d594e0351620c2be3ef608bc63f1f0e14e9b7460
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 04:12:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -94,7 +94,8 @@ const { state, saveState } = store.useSingleFileAuthState(global.authFile)
 const connectionOptions = {
   printQRInTerminal: true,
   auth: state,
-  // logger: pino({ level: 'trace' })
+  logger: pino({ level: 'trace' })
+  browser: ['GataBot-MD','Edge','1.0.0']
 }
 
 global.conn = makeWASocket(connectionOptions)
