35fa3306b8697b6ce9820eace49211dec765d46d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 15:58:07 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/sticker-baka.js
@@ -0,0 +1,15 @@
+import { sticker } from '../lib/sticker.js'
+import fetch from 'node-fetch'
+import MessageType from '@adiwajshing/baileys'
+let handler = async (m, { conn}) => {
+try {
+if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
+if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
+let res = await fetch('http://api.nekos.fun:8080/api/baka')
+let json = await res.json()
+let { url } = json
+let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está diciendo baka a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
+conn.sendFile(m.chat, stiker, null, { asSticker: true })
+} catch (e) { }}
+handler.command = /^(baka|baca|bakka)$/i 
+export default handler
