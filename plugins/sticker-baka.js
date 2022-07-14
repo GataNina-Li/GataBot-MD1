a9680881a345fc5cd33082fb6c5977f2afad1f16
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 16:27:06 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -1,19 +1,15 @@
 import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
 import MessageType from '@adiwajshing/baileys'
-import axios from "axios" 
 let handler = async (m, { conn}) => {
 try {
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-//let res = await axios.get("http://api.nekos.fun:8080/api/baka")
 let res = await fetch('https://source.unsplash.com/featured/?girl,woman')
 let json = await res.json()
 let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está diciendo baka a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
+let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está alimentando a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 conn.sendFile(m.chat, stiker, null, { asSticker: true })
 } catch (e) { }}
-handler.command = /^(baka)$/i  
+handler.command = /^(alimentar|food|alimentándose|alimentando|alimentar)$/i 
 export default handler
-
-
