a81171c00d29021851118df3ee2de0cb761e0929
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 16:47:54 -0500

    Update sticker-baka.js

--- plugins/sticker-baka.js
+++ plugins/sticker-baka.js
@@ -2,20 +2,21 @@ import axios from "axios"
 import fetch from 'node-fetch'
 let handler = async (m, {command, conn}) => {
 
-//let res = await axios("https://source.unsplash.com/featured/?girl,woman")
-//let json = res.data
-//let url = json.url
-//let res = await fetch('https://source.unsplash.com/featured/?girl,woman')
-//let json = await res.json()
-//let { url } = json
-//conn.sendButton(m.chat, `Chica`.trim(), wm, url, [['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `/${command}`]], m)}
+if (command == 'chica') {
 let pp = 'https://source.unsplash.com/featured/?girl,woman'
 conn.sendHydrated(m.chat, wm, null, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['Chica', `.chi`]
+['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `.chica`],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}
+  
+if (command == 'chico') {
+let pp = 'https://source.unsplash.com/featured/?boy,man'
+conn.sendHydrated(m.chat, wm, null, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['😻 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `.chica`],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)}  
 
-handler.help = ['dado']
-handler.tags = ['game']
-handler.command = ['chi'] 
+handler.help = ['chica', 'chico'] 
+handler.command = ['chica', 'chico'] 
 export default handler
 
