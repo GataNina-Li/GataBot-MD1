a0156b1950d1177109e9cf73708b25e9678f879d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-14 04:27:59 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ import {
 import yargs from 'yargs';
 import { spawn } from 'child_process';
 import lodash from 'lodash';
+import chalk from 'chalk';
 import syntaxerror from 'syntax-error';
 import { tmpdir } from 'os';
 import { format } from 'util';
