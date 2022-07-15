90f06ad66b15640ba1021276ac7f9dd481e8bba7
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-07-14 21:16:04 -0300

    Update rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -37,7 +37,7 @@ let user = global.db.data.users[m.sender]
 ┃ *FECHA:* *${new Date().toLocaleString('id-ID')}*
 ╰━━━〔 *𓃠 ${vs}* 〕━━━━━⬣
 
-*_Cuanto más interactúes con GataBot-MD, mayor será tu nivel!!_*
+*_Cuanto más interactúes con FutabuBot-MD, mayor será tu nivel!!_*
 `.trim()
         try {
             const img = await levelup(teks, user.level)
