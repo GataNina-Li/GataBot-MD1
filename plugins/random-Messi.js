2ae74855a3da104aef6be7555a5309ad153aaa3c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-15 12:45:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/random-Messi.js
+++ plugins/random-Messi.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/messi")
 let json = res.data
 let url = json.url
-conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+conn.sendButton(m.chat, "*Messi*", wm, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `${usedPrefix + command}`]], m)}
 handler.help = ['messi']
 handler.tags = ['internet']
 handler.command = /^(messi)$/i
