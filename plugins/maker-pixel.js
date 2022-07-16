690e2ad2a7229b337a42b18c0a280fbb2f39b5f2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-16 17:02:33 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/maker-pixel.js
+++ plugins/maker-pixel.js
@@ -4,7 +4,7 @@ conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/pixelate
 avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
 comment: text,
 username: conn.getName(m.sender)
-}), 'error.png', '*¡¡𝙸𝙼𝙰𝙶𝙴𝙽 𝙿𝙸𝚇𝙴𝙻𝙴𝙰𝙳𝙰 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾!!*', m)
+}), 'error.png', '🌀 *AQUÍ ESTA LA IMAGEN PIXELADA!!*\n𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', m)
 }
 handler.help = ['pixel','difuminar']
 handler.tags = ['maker']
