096f192cd2dd8b4743e9910ccc4116984062704b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-16 17:03:53 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/maker-simpcard.js
+++ plugins/maker-simpcard.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/simpcard', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), 'error.png', '*¡¡𝚃𝚄 𝚁𝙴𝙻𝙸𝙶𝙸𝙾𝙽 𝙴𝚂 𝚂𝙴𝚁 𝚄𝙽 𝚂𝙸𝙼𝙿!!*', m)
+}), 'error.png', '*WTF ERES SIMP* 😆', m)
 }
 handler.help = ['simpcard']
 handler.tags = ['maker']  
