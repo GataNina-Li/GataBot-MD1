14641a0fb3d007a0bad5d1fa7dbb96c9df452fa4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-16 14:55:33 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -95,10 +95,13 @@ global.md = 'https://github.com/GataNina-Li/GataBot-MD'
 global.gatabot = md
 
 global.nn = 'https://chat.whatsapp.com/Hahc7UwSouH9jIDStkT5QW'
-global.nngrupo = nn
+global.nngrupo = nn //Grupo 1
 
 global.nnn = 'https://chat.whatsapp.com/J96jlutgGlr1gaKgafBj8b'
-global.nnngrupo = nnn
+global.nnngrupo = nnn //Grupo 2
+
+global.nnnt = 'https://chat.whatsapp.com/LJs5xqqTLrN51npqfAnGXD'
+global.nnngrupo = nnnt //Grupo 3
 
 global.paypal = 'https://paypal.me/OficialGD'
 global.donar = paypal
