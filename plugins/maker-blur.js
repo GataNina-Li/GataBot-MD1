bbb9043b5301aeef1da29dfcb0c8e761d13ffb1f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-16 14:36:28 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/maker-blur.js
+++ plugins/maker-blur.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedprefix }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/blur', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), 'hornycard.png', '*[ ✔ ] 𝙴𝙵𝙴𝙲𝚃𝙾 𝙰𝙿𝙻𝙸𝙲𝙰𝙳𝙾*', m)
+}), 'hornycard.png', '✨ 𝙀𝙎𝙏𝘼 𝙇𝙄𝙎𝙏𝙊!!\n𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', m)
 }
 handler.help = ['blur','difuminar2']
 handler.tags = ['maker']
