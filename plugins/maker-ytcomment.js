791f375746d570805ead78f92f200a2db62b1ccf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-16 17:05:03 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/maker-ytcomment.js
+++ plugins/maker-ytcomment.js
@@ -4,7 +4,7 @@ conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-
 avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
 comment: text,
 username: conn.getName(m.sender)
-}), 'error.png', '*¡¡𝙶𝚁𝙰𝙲𝙸𝙰𝚂 𝙿𝙾𝚁 𝙲𝙾𝙼𝙴𝙽𝚃𝙰𝚁!!*', m)
+}), 'error.png', '*HAS COMENTADO EN YOUTUBE!!* 😳', m)
 }
 handler.help = ['ytcomment <comment>']
 handler.tags = ['maker'] 
