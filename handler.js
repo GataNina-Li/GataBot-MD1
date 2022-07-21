6c8803993cdfc9c6a7808577955492da88c79676
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-21 12:03:49 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -44,7 +44,7 @@ export async function handler(chatUpdate) {
                 if (!isNumber(user.exp))
                     user.exp = 0
                 if (!isNumber(user.limit))
-                    user.limit = 10
+                    user.limit = 20
                 if (!isNumber(user.lastclaim))
                     user.lastclaim = 0
                 if (!('registered' in user))
