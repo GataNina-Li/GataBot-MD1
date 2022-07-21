f05a75ee0c316c68bfb7ae32564148bc23ce7494
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-21 16:15:32 -0500

    Update fun-tictactoe.js

--- plugins/fun-tictactoe.js
+++ plugins/fun-tictactoe.js
@@ -1,10 +1,12 @@
-let handler = m => m
+import { format } from 'util'
+import db from '../lib/database.js'
+
 let debugMode = !1
 
-let winScore = 500
-let playScore = 50
+let winScore = 4999
+let playScore = 99
 
-handler.before = function (m) {
+export async function before(m) {
     let ok
     let isWin = !1
     let isTie = !1
@@ -13,26 +15,31 @@ handler.before = function (m) {
     let room = Object.values(this.game).find(room => room.id && room.game && room.state && room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
     if (room) {
         // m.reply(`[DEBUG]\n${parseInt(m.text)}`)
-        if (!/^([1-9]|(me)?salir|surr?ender)$/i.test(m.text)) return !0
+        if (!/^([1-9]|(me)?nyerah|surr?ender)$/i.test(m.text))
+            return !0
         isSurrender = !/^[1-9]$/.test(m.text)
         if (m.sender !== room.game.currentTurn) { // nek wayahku
-            if (!isSurrender) return !0
+            if (!isSurrender)
+                return !0
         }
-        if (debugMode) m.reply('[DEBUG]\n' + require('util').format({
-            isSurrender,
-            text: m.text
-        }))
+        if (debugMode)
+            m.reply('[DEBUG]\n' + require('util').format({
+                isSurrender,
+                text: m.text
+            }))
         if (!isSurrender && 1 > (ok = room.game.turn(m.sender === room.game.playerO, parseInt(m.text) - 1))) {
             m.reply({
-                '-3': 'El juego ha terminado',
-                '-2': 'Inválido',
-                '-1': 'Posición inválida',
-                0: 'Posición inválida',
+                '-3': 'Game telah berakhir',
+                '-2': 'Invalid',
+                '-1': 'Posisi Invalid',
+                0: 'Posisi Invalid',
             }[ok])
             return !0
         }
-        if (m.sender === room.game.winner) isWin = true
-        else if (room.game.board === 511) isTie = true
+        if (m.sender === room.game.winner)
+            isWin = true
+        else if (room.game.board === 511)
+            isTie = true
         let arr = room.game.render().map(v => {
             return {
                 X: '❌',
@@ -54,39 +61,35 @@ handler.before = function (m) {
         }
         let winner = isSurrender ? room.game.currentTurn : room.game.winner
         let str = `
-⭕️ *GATO / 3 EN RAYAS* ❌
-
 ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
-
-*${isWin ? `@${winner.split('@')[0]} gano la partida!!!* (+${winScore} XP)` : isTie ? `*El Juego ha terminado* (+${playScore} XP)` : `*Es turno de:* ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}
-*❌: @${room.game.playerX.split('@')[0]}*
-*⭕: @${room.game.playerO.split('@')[0]}*
-*- Si la partida aun no acaba puede utilizar la palabra "salir", no escriba ningun prefijo ni las " ni las * ni los ~ y el mensaje debe ser respondiendo al mensaje del Bot en donde salga la tabla del juego. También puede usar el comando #delttt ~nombre de la sala~ para eliminar la sala de juego*
+${isWin ? `@${winner.split('@')[0]} Menang! (+${winScore} XP)` : isTie ? `Game berakhir (+${playScore} XP)` : `Giliran ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}
+❌: @${room.game.playerX.split('@')[0]}
+⭕: @${room.game.playerO.split('@')[0]}
+Ketik *nyerah* untuk nyerah
+Room ID: ${room.id}
 `.trim()
-        let users = global.DATABASE._data.users
+        let users = db.data.users
         if ((room.game._currentTurn ^ isSurrender ? room.x : room.o) !== m.chat)
             room[room.game._currentTurn ^ isSurrender ? 'x' : 'o'] = m.chat
-        if (room.x !== room.o) m.reply(str, room.x, {
-            contextInfo: {
-                mentionedJid: this.parseMention(str)
-            }
-        })
-        m.reply(str, room.o, {
-            contextInfo: {
-                mentionedJid: this.parseMention(str)
-            }
+        const btn = isTie ? ['TicTacToe', '/ttt'] : ['Nyerah', 'nyerah']
+        if (room.x !== room.o)
+            await this.sendButton(room.x, str, author, btn, m, {
+                mentions: this.parseMention(str)
+            })
+        await this.sendButton(room.o, str, author, btn, m, {
+            mentions: this.parseMention(str)
         })
         if (isTie || isWin) {
             users[room.game.playerX].exp += playScore
             users[room.game.playerO].exp += playScore
-            if (isWin) users[winner].exp += winScore - playScore
-            if (debugMode) m.reply('[DEBUG]\n' + require('util').format(room))
-            delete this.game[room.id] 
+            if (isWin)
+                users[winner].exp += winScore - playScore
+            if (debugMode)
+                m.reply('[DEBUG]\n' + format(room))
+            delete this.game[room.id]
         }
     }
     return !0
 }
-
-module.exports = handler
