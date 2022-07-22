9f6c0f6469763df59e9503a710b844da481b8a37
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-22 18:03:25 -0500

    Update s-payaso.js

--- plugins/s-payaso.js
+++ plugins/s-payaso.js
@@ -2,7 +2,8 @@ import { sticker } from '../lib/sticker.js'
 let handler = async(m, { conn }) => {
   
 const s = [
-'https://img-16.stickers.cloud/packs/f73a3b52-02ed-4f9c-83ea-969d698b6200/webp/animated/2887d998-3f0c-424a-aaeb-132bad02da58.webp'     
+//'https://img-16.stickers.cloud/packs/f73a3b52-02ed-4f9c-83ea-969d698b6200/webp/animated/2887d998-3f0c-424a-aaeb-132bad02da58.webp' 
+'https://i.waifu.pics/6Y02AB5.gif'
 ];  
 let stiker = await sticker(null, s, global.packname, global.author)
 
