a65d478bb3b904624ac9ef7e7637db98113db427
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-22 17:56:33 -0500

    Update s-payaso.js

--- plugins/s-payaso.js
+++ plugins/s-payaso.js
@@ -3,7 +3,10 @@ let handler = async(m, { conn }) => {
 const s = [
 'https://img-16.stickers.cloud/packs/f73a3b52-02ed-4f9c-83ea-969d698b6200/webp/animated/2887d998-3f0c-424a-aaeb-132bad02da58.webp'     
 ];  
-    conn.sendFile(m.chat, s, 'sticker.webp', global.packname global.author, m)
+let stiker = await sticker(null, s, global.packname, global.author)
+
+    //conn.sendFile(m.chat, s, 'sticker.webp', '', m)
+    conn.sendFile(m.chat, stiker, null, { asSticker: true })
 }
 handler.customPrefix = /payaso|🤡|pallaso/
 handler.command = new RegExp
