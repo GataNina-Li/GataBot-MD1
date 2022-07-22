3930c52a5b33bddebac5d1220287e4e2ddab274f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-22 17:38:27 -0500

    Create s-payaso.js

---
+++ plugins/s-payaso.js
@@ -0,0 +1,11 @@
+let handler = async(m, { conn }) => {
+  
+const s = [
+'https://img-16.stickers.cloud/packs/f73a3b52-02ed-4f9c-83ea-969d698b6200/webp/animated/2887d998-3f0c-424a-aaeb-132bad02da58.webp'     
+];  
+    conn.sendFile(m.chat, s, 'sticker.webp', '', m)
+
+}
+handler.customPrefix = /payaso|🤡|pallaso/
+handler.command = new RegExp
+export default handler
