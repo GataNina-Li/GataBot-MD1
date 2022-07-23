a78161376bd97e6db8cef9838755885499ae71d0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-23 15:04:53 -0500

    Update s-payaso.js

--- plugins/s-payaso.js
+++ plugins/s-payaso.js
@@ -26,11 +26,12 @@ let handler = async(m, { conn }) => {
 if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 *#on stickers* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘼𝙍𝙀 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on stickers* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
 
 const s = [
-'https://tinyurl.com/jsjdiif'
+'https://img-16.stickers.cloud/packs/f73a3b52-02ed-4f9c-83ea-969d698b6200/webp/animated/2887d998-3f0c-424a-aaeb-132bad02da58.webp'
 ];  
  
  let stiker = await sticker(null, s, global.packname, global.author)
- conn.sendFile(m.chat, stiker, null, { asSticker: true })
+ conn.sendFile(m.chat, s, 'sticker.webp', stiker, m)
+// conn.sendFile(m.chat, stiker, null, { asSticker: true })
  
  }
 handler.customPrefix = /payaso|🤡|pallaso/ 
