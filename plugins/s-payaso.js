105931a4114e471ec9fd37e1d69b12b0c36fee3b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-23 17:09:01 -0500

    Update s-payaso.js

--- plugins/s-payaso.js
+++ plugins/s-payaso.js
@@ -23,7 +23,7 @@ let handler = async(m, { conn }) => {
 if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 *#on stickers* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘼𝙍𝙀 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on stickers* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
 
 const s = [
- 'https://media3.giphy.com/media/l3q2K5jinAlChoCLS/giphy.gif?cid=ecf05e47omrkjn0cpascmilz2lpk74q1u6qnwe7aei0sumfv&rid=giphy.gif&ct=g'
+ 'https://media2.giphy.com/media/awEamY4rvlY0uXf6a8/giphy.gif?cid=ecf05e471flaehmn8xdife8uofelqxdgnrsskq0z3cl1pjfs&rid=giphy.gif&ct=g'
 ];  
  
  let stiker = await sticker(null, s, global.packname, global.author)
