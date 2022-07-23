93e61559680ab7df38ef9eb5a11434991090b724
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-22 19:46:55 -0500

    Update s-payaso.js

--- plugins/s-payaso.js
+++ plugins/s-payaso.js
@@ -1,4 +1,4 @@
-//import { sticker } from '../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
 //import fetch from 'node-fetch'
 
 let handler = async(m, { conn }) => {
@@ -11,11 +11,11 @@ if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾�
 //conn.sendFile(m.chat, s, 'sticker.webp', '', m)
 
      
-const s = [
+const url = [
 //'https://img-16.stickers.cloud/packs/f73a3b52-02ed-4f9c-83ea-969d698b6200/webp/animated/2887d998-3f0c-424a-aaeb-132bad02da58.webp'
      'https://img-15.stickers.cloud/packs/e4e40cc1-9ad1-47c2-aeba-88d246b9b6ac/webp/31d3fc0b-3dc3-43da-bdc8-700a492f8c26.webp'
 ]; 
-     let stiker = await s('sticker.webp', url, global.packname, global.author)
+     let stiker = await stiker('sticker.webp', url, global.packname, global.author)
      conn.sendFile(m.chat, stiker, 'sticker.webp', { asSticker: true })
 }
 handler.customPrefix = /payaso|🤡|pallaso/ 
