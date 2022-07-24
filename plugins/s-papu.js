058fa46e08c2c42b6c27fa380ecfd0a208ef8780
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-23 21:05:42 -0500

    Update s-papu.js

--- plugins/s-papu.js
+++ plugins/s-papu.js
@@ -18,7 +18,7 @@ conn.sendFile(m.chat, stiker, null, { asSticker: true })
 //conn.sendFile(m.chat, s, 'sticker.gif', null, m)
  
  }
-handler.customPrefix = /papu/i 
+handler.customPrefix = /llorar|yorar|llorando|llorando|llorare|llorará|lloremos|llorastes|lloraste|llora|😭|😢|😰/i 
 handler.command = new RegExp
 handler.exp = 50
 export default handler
