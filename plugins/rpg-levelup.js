f43283e4fbfbd6f5185aabcb1e3c8be962d8bd98
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-25 03:24:43 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -124,7 +124,7 @@ handler.help = ['levelup']
 handler.tags = ['xp']
 
 handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
-handler.exp = null
+handler.exp = 0
 export default handler
     
 const more = String.fromCharCode(8206)
