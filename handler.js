abce3256653bb70dc48704ff84f99a26deaf1a64
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-25 02:29:11 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- handler.js
+++ handler.js
@@ -70,7 +70,7 @@ export async function handler(chatUpdate) {
                 if (!('role' in user))
                     user.role = 'Novato'
                 if (!('autonivel' in user)) 
-                    user.autonivel = false
+                    user.autonivel = true
 
                 if (!isNumber(user.money))
                     user.money = 0
@@ -187,7 +187,7 @@ export async function handler(chatUpdate) {
                     warn: 0,
                     level: 0,
                     role: 'Novato',
-                    autonivel: false,
+                    autonivel: true,
 
                     money: 0,
                     health: 100,
