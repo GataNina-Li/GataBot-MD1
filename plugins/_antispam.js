cdda4f64ac43f119d381f62f0149cbb91b325df3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-24 19:40:47 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -6,8 +6,8 @@ export async function all(m) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 10) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 3) {
+            if (this.spam[m.sender].count > 3) {
                 //db.data.users[m.sender].banned = true
                 m.reply('*¡No hagas spam!*')
             }
