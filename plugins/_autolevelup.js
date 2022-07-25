f3275b85c2f8068683204e980d06f698e4b15a3f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-25 01:14:55 -0500

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,18 +1,28 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 export function before(m, { conn }) {
-    let user = global.db.data.users[m.sender]
-    if (!user.autolevelup)
+	
+let user = global.db.data.users[m.sender]
+  if (!user.autolevelup)
         return !0
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier))
-        user.level++
-    if (before !== user.level) {
-        m.reply(`
-Selamat, anda telah naik level!
-${before} -> ${user.level}
-gunakan .profile untuk mengecek
-	`.trim())
+	
+let before = user.level * 1
+while (canLevelUp(user.level, user.exp, global.multiplier))
+user.level++
+  if (before !== user.level) {
+	  
+m.reply(`
+╭━━━[ 𝙉𝙄𝙑𝙀𝙇 | 𝙇𝙀𝙑𝙀𝙇 ]━━━━⬣
+┃ NIVEL ANTERIOR: ${before}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ NIVEL ACTUAL: ${user.level}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ RANGO ${role}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ FECHA: ${new Date().toLocaleString('id-ID')}
+╰━━━〔 𓃠 ${vs} 〕━━━━━⬣
+Cuanto más interactúes con GataBot-MD, mayor será tu nivel!!
+`.trim())
     }
 }
 
