e606b8188736279fde1ae1ea4f1cf79b0a02940e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-25 03:07:51 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -4,7 +4,7 @@ export function before(m, { conn }) {
 //if (!db.data.chats[m.chat].autonivel && m.isGroup) throw 
 	
 let user = global.db.data.users[m.sender]
- if (!user.autolevelup) //throw `${ag}𝙀𝙇 𝘼𝙐𝙏𝙊𝙉𝙄𝙑𝙀𝙇 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊 𝙐𝙎𝙀 *#on autonivel* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝘼𝙐𝙏𝙊𝙉𝙄𝙑𝙀𝙇 𝙏𝙃𝙄𝙎 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on autonivel* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
+ if (!user.autonivel) //throw `${ag}𝙀𝙇 𝘼𝙐𝙏𝙊𝙉𝙄𝙑𝙀𝙇 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊 𝙐𝙎𝙀 *#on autonivel* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝘼𝙐𝙏𝙊𝙉𝙄𝙑𝙀𝙇 𝙏𝙃𝙄𝙎 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on autonivel* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
   return !0
 	
 let before = user.level * 1
