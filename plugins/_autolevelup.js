ad56e8e3ea995c1cd7626e76000ae304dd9b2687
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-25 01:12:30 -0500

    Create _autolevelup.js

---
+++ plugins/_autolevelup.js
@@ -0,0 +1,19 @@
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
+export function before(m, { conn }) {
+    let user = global.db.data.users[m.sender]
+    if (!user.autolevelup)
+        return !0
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier))
+        user.level++
+    if (before !== user.level) {
+        m.reply(`
+Selamat, anda telah naik level!
+${before} -> ${user.level}
+gunakan .profile untuk mengecek
+	`.trim())
+    }
+}
+
+export const disabled = true
