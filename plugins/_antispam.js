ef29c8a48e0ca2e1ba20f11d28017bbd44786894
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-24 19:38:51 -0500

    Create _antispam.js

---
+++ plugins/_antispam.js
@@ -0,0 +1,24 @@
+import db from '../lib/database.js'
+
+export async function all(m) {
+    if (!m.message)
+        return
+    this.spam = this.spam ? this.spam : {}
+    if (m.sender in this.spam) {
+        this.spam[m.sender].count++
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
+            if (this.spam[m.sender].count > 10) {
+                //db.data.users[m.sender].banned = true
+                m.reply('*¡No hagas spam!*')
+            }
+            this.spam[m.sender].count = 0
+            this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
+        }
+    }
+    else
+        this.spam[m.sender] = {
+            jid: m.sender,
+            count: 0,
+            lastspam: 0
+        }
+}
