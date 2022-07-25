1bdd9ca99837a3945de2968d0d6641469e742e29
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-24 21:42:38 -0500

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -36,11 +36,12 @@ let { role } = global.db.data.users[m.sender]
   }
 
 export const disabled = true */
-
+import { spawn } from 'child_process'
 import db from '../lib/database.js'
 import { canLevelUp } from '../lib/levelling.js'
 
-export function before(m, { conn }) {  
+export function levelup before(m, teks, level, { conn }) {  
+	
 	
     //let user = global.db.data.users[m.sender]
     let { user, role } = global.db.data.users[m.sender]
@@ -65,7 +66,7 @@ let str =
 *_Cuanto más interactúes con GataBot-MD, mayor será tu nivel!!_*
 `.trim()
 
-export function levelup(teks, level) {
+//export function levelup(teks, level) {
 
 if (global.support.convert || global.support.magick || global.support.gm) {
 	 
