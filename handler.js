d8a7ce03b197e2f6f3850a21c9d226b3efe1049d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-07-25 02:37:38 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- handler.js
+++ handler.js
@@ -266,6 +266,8 @@ export async function handler(chatUpdate) {
                     chat.modohorny = false
                 if (!('stickers' in chat))
                     chat.stickers = false
+                if (!('autonivel' in chat))
+                    chat.autonivel = false
                 if (!('autosticker' in chat))
                     chat.autosticker = false                    
                 if (!('audios' in chat))
