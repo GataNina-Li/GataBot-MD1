a10b426553a67016619c529cfc89374f4a86d097
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-02 00:26:20 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url'
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
   ['593993684821', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈', true],
+  ['50495086382'],
   ['51993042301'],
   ['593968585383']
 ]
