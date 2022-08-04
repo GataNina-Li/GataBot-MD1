c5dcb645bf2b471230d1ca364662ecbab6d1c350
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-03 22:53:42 -0500

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -1,11 +1,12 @@
-let handler = m => m
+/*let handler = m => m
 import fs from 'fs'
 handler.before = async function (m) {
+    let res = await conn.groupAcceptInvite(code)
     const fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync("./media/menus/Menu3.jpg"), thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),sendEphemeral: true}}}
 
     if (m.isGroup && global.db.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= global.db.data.chats[m.chat].expired) {
-            this.reply(m.chat, `tiempo *${this.user.name}* para dejar el grupo, ¿quieres que este bot permanezca en tu grupo? alquilar al número`, null).then(() => {
+            this.reply(m.chat, `tiempo *${this.user.name}* para dejar el grupo, ¿quieres que este bot permanezca en tu grupo? alquilar al número\n\nEl bot saldrá automáticamente después de: ${msToDate(global.db.data.chats[res].expired - now)}`, null).then(() => {
                 this.sendKontak(m.chat, fkontak, { contexInfo: { forwardingScore: 99999, isForwarded: true } }).then(() => {
                     this.groupLeave(m.chat).then(() => {
                         global.db.data.chats[m.chat].expired = 0
@@ -17,3 +18,47 @@ handler.before = async function (m) {
 }
 
 export default handler
+
+function msToDate(ms) {
+    let temp = ms
+    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    let daysms = ms % (24 * 60 * 60 * 1000);
+    let hours = Math.floor((daysms) / (60 * 60 * 1000));
+    let hoursms = ms % (60 * 60 * 1000);
+    let minutes = Math.floor((hoursms) / (60 * 1000));
+    let minutesms = ms % (60 * 1000);
+    let sec = Math.floor((minutesms) / (1000));
+    return days + " Days ☀️\n" + hours + " Hours 🕐\n" + minutes + " Minute ⏰\n" + sec + " Segundos 🕐";
+    //return days + " Hari " + hours + " Jam " + minutes + " Menit";
+    // +minutes+":"+sec;
+}*/
+
+
+export async function all(m) {
+    let res = await conn.groupAcceptInvite(code)
+    if (!m.isGroup)
+        return
+    let chats = global.db.data.chats[m.chat]
+    if (!chats.expired)
+        return !0
+    if (+new Date() > chats.expired) {
+        let caption = `Bye🖐 *${this.user.name}* quedará del grupo!!\n\nEl bot saldrá automáticamente después de: ${msToDate(global.db.data.chats[res].expired - now)}`
+    await this.sendButton(m.chat, caption, wm, null, [['Eliminar caducado', '/delexpired'], ['Cec caducado', '/cekexpired']], null)
+        await this.groupLeave(m.chat)
+        chats.expired = null
+    }
+}
+
+function msToDate(ms) {
+    let temp = ms
+    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    let daysms = ms % (24 * 60 * 60 * 1000);
+    let hours = Math.floor((daysms) / (60 * 60 * 1000));
+    let hoursms = ms % (60 * 60 * 1000);
+    let minutes = Math.floor((hoursms) / (60 * 1000));
+    let minutesms = ms % (60 * 1000);
+    let sec = Math.floor((minutesms) / (1000));
+    return days + " Days ☀️\n" + hours + " Hours 🕐\n" + minutes + " Minute ⏰\n" + sec + " Segundos 🕐";
+    //return days + " Hari " + hours + " Jam " + minutes + " Menit";
+    // +minutes+":"+sec;
+}
