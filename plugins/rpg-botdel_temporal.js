0ed3ec44732b5cc4ab057d075e4e8016f4947261
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-04 03:25:37 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-botdel_temporal.js
+++ plugins/rpg-botdel_temporal.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     else global.db.data.chats[who].expired = false
     
     let caption = `*Se eliminaron con éxito el vencimiento para este grupo*`
-    conn.sendButton(m.chat, caption, wm, null, [['Add Expired', '/expired'], ['Cek Expired', '/cekexpired']], m)
+    conn.sendButton(m.chat, caption, wm, null, [['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `/menu`]], m)
     
 }
 handler.help = ['delexpired']
