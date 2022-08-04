4bf78256bbc2b11b9ac9ec98f0561e9042a0bece
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-04 18:38:29 -0500

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -272,6 +272,9 @@ handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayud
 handler.exp = 50
 export default handler
 
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
 function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
