8a1d01a5c481d37d2dd17cabb028dce255be9073
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-04 03:38:43 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -43,8 +43,9 @@ export async function all(m) {
         return !0
     if (+new Date() > chats.expired) {
         let caption = `*${this.user.name}* SE VA DEL GRUPO!!!, SI QUIERE QUE VUELVA USE EL COMANDO #bottemporal O INSTALE A GATABOT-MD SIN RESTRICCIONES!!`
+        let pp = './media/menus/Menu2.jpg'
     //await this.sendButton(m.chat, caption, wm, null, [['Eliminar caducado', '/delexpired'], ['Cec caducado', '/cekexpired']], null)
-await this.sendHydrated2(m.chat, str, wm, pp, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, m,)
+await this.sendHydrated2(m.chat, caption, wm, pp, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, m,)
         
         await this.groupLeave(m.chat)
         chats.expired = null
