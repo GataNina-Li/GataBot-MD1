6a73e21a3b016b6015a4743dbc9af86d3ce5bffc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-03 21:40:23 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -68,7 +68,7 @@ let handler = async (m, { conn, args, usedPrefix, command, isPrems, isOwner, isR
 handler.help = ['join <link> <time>']
 handler.tags = ['owner']
 //handler.limit = true
-handler.command = /^(join2)$/i
+handler.command = /^(temporal)$/i
 
 export default handler
 
