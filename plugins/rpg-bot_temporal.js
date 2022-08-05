cf8cc76c390ad3f1678b6a9c0d1aa61d26800d21
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-05 00:42:21 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-bot_temporal.js
+++ plugins/rpg-bot_temporal.js
@@ -18,7 +18,8 @@ let img = 'https://i.imgur.com/8fK4h6F.jpg'
         //user.joincount -= 1
  
 if (!args[1]) throw `${mg}*USE EL COMANDO COMO ESTE EJEMPLO*\n*${usedPrefix + command} enlace y Número de Token(s)*\n\n*EJEMPLO*\n*${usedPrefix + command} ${nn} 1*\n\n*1 TOKEN 🪙 = 10 MINUTOS*`
-let res = await conn.groupAcceptInvite(code) 
+let res = await conn.groupAcceptInvite(code)
+if (user.joincount > 1) throw `${fg}*MÍNIMO 1 TOKEN! 🪙 PARA PODER INVITAR A GATABOT AL GRUPO*`
 conn.reply(m.chat, `${eg}😻 *SE HA UNIDO GATABOT AL GRUPO!!!*`, m).then(async() => { 
   user.joincount -= 2
  var jumlahHari = 600000 * args[1] // 10 minutos | Owner
