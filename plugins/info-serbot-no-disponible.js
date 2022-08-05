66f4237a3f698829b6bf601007c2f791aa2478d9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-05 17:11:43 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/info-serbot-no-disponible.js
+++ plugins/info-serbot-no-disponible.js
@@ -19,7 +19,7 @@ body: 'Super Bot WhatsApp',
 previewType: 1, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
 sourceUrl: `${yt}`}}})  
   
-  let info = `*Ya que no esta disponible el #serbot, Ahora podrás unir el Número de este Bot a tú Grupo de manera Temporal usando el comando*\n#*botemporal o #bottemporal*
+  let info = `*Ya que no esta disponible el #serbot, Ahora podrás unir el Número de este Bot a tú Grupo de manera Temporal usando el comando*\n*#botemporal o #bottemporal*
   `.trim()
   
   await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
