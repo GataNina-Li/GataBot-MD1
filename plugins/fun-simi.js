a32f95297e43f4550124745e3946786384805cb7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-06 04:09:37 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -16,16 +16,3 @@ export default handler
 
 
 
-/*import fetch from 'node-fetch'
-let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola bot*`
-let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
-let json = await res.json()
-m.reply(json.success)}
-handler.help = ['simi', 'bot'].map(v => v + ' <teks>')
-handler.tags = ['fun']
-handler.command = /^((sim)?simi|bot|alexa|cortana)$/i
-export default handler*/
-
-
-
