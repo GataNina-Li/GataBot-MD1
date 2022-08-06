faa9c0062c905cd796b1669ca064b3ceb4630f15
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-06 03:08:55 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -1,13 +1,11 @@
 let handler = m => m
 handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
-
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 50  
     
 if (/^A Bueno master|Bueno master|Bueno Máster|🫂$/i.test(m.text) && chat.audios) {  
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
-global.db.data.users[m.sender].money += 50
-global.db.data.users[m.sender].exp += 50    
-    
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
 let vn = './media/A bueno adios master.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
