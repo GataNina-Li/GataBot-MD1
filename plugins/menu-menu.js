2301669a1e0e35b44d15b6df977f5fa7cfb66844
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-09 22:31:54 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -516,7 +516,7 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')} 
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}  
 
 
 
