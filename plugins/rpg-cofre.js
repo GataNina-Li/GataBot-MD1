9a72dcec39852bb6231c99bf095ae664bc599112
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-10 16:43:47 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -1,8 +1,8 @@
 let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastclaim + 36000000 // 10 Horas //86400000 24 Horas
-if (new Date - global.db.data.users[m.sender].lastclaim < 36000000) throw `𝙔𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙎𝙏𝙀 𝙏𝙐 𝙍𝙀𝙂𝘼𝙇𝙊 🎁\n𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 *${msToTime(time - new Date())}* 𝙋𝘼𝙍𝘼 𝙑𝙊𝙇𝙑𝙀𝙍 𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙍\n\n𝙔𝙊𝙐 𝘼𝙇𝙍𝙀𝘼𝘿𝙔 𝘾𝙇𝘼𝙄𝙈𝙀𝘿 𝙔𝙊𝙐𝙍 𝙂𝙄𝙁𝙏 🎁\n𝘾𝙊𝙈𝙀 𝘽𝘼𝘾𝙆 𝙄𝙉 *${msToTime(time - new Date())}* 𝙏𝙊 𝘾𝙇𝘼𝙄𝙈 𝘼𝙂𝘼𝙄𝙉`
+if (new Date - global.db.data.users[m.sender].lastclaim < 36000000) throw `𝙔𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙎𝙏𝙀 𝙏𝙐 𝘾𝙊𝙁𝙍𝙀 ⚗️\n𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 *${msToTime(time - new Date())}* 𝙋𝘼𝙍𝘼 𝙑𝙊𝙇𝙑𝙀𝙍 𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙍\n\n𝙔𝙊𝙐 𝘼𝙇𝙍𝙀𝘼𝘿𝙔 𝘾𝙇𝘼𝙄𝙈𝙀𝘿 𝙔𝙊𝙐𝙍 𝘾𝙊𝙁𝙁𝙀𝙍 🎁\n𝘾𝙊𝙈𝙀 𝘽𝘼𝘾𝙆 𝙄𝙉 *${msToTime(time - new Date())}* 𝙏𝙊 𝘾𝙇𝘼𝙄𝙈 𝘼𝙂𝘼𝙄𝙉`
 
-let img = 'https://img.freepik.com/vector-gratis/gente-diminuta-enormes-cajas-regalo-ilustracion-vectorial-plana-personas-que-celebran-cumpleanos-envian-o-reciben-regalos-lealtad-o-ideas-brillantes-recompensa-bonificacion-concepto-fiesta_74855-25016.jpg?w=2000'
+let img = 'https://img.freepik.com/vector-gratis/cofre-monedas-oro-piedras-preciosas-cristales-trofeo_107791-7769.jpg?w=2000'
 let dia = Math.floor(Math.random() * 30)
 let tok = Math.floor(Math.random() * 20)
 let gata = Math.floor(Math.random() * 4000)
@@ -14,14 +14,14 @@ let expp = Math.floor(Math.random() * 5000)
   global.db.data.users[m.sender].exp += expp
   
 let texto = `
-╭━━🎁━🎁━🎁━━⬣
-┃✨ 𝙊𝘽𝙏𝙄𝙀𝙉𝙀𝙎 𝙐𝙉 𝙍𝙀𝙂𝘼𝙇𝙊!!
-┃✨ 𝙔𝙊𝙐 𝙂𝙀𝙏 𝘼 𝙂𝙄𝙁𝙏!!
+╭━━🎉━🎉━🎉━━⬣
+┃✨ 𝙊𝘽𝙏𝙄𝙀𝙉𝙀𝙎 𝙐𝙉 𝘾𝙊𝙁𝙍𝙀!!
+┃✨ 𝙔𝙊𝙐 𝙂𝙀𝙏 𝘼 𝘾𝙊𝙁𝙁𝙀𝙍!!
 ┃ ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃🎁 *${dia} Diamantes* 💎
-┃🎁 *${tok} Tokens* 🪙
-┃🎁 *${gata} GataCoins* 🐈
-┃🎁 *${expp} Exp* ⚡
+┃⚗️ *${dia} Diamantes* 💎
+┃⚗️ *${tok} Tokens* 🪙
+┃⚗️ *${gata} GataCoins* 🐈
+┃⚗️ *${expp} Exp* ⚡
 ╰━━〔 𓃠 *${vs}* 〕━━⬣`
 
 const fkontak = {
@@ -39,12 +39,12 @@ const fkontak = {
 	"participant": "0@s.whatsapp.net"
 }
 
-await conn.sendButton(m.chat, texto, wm, img, [['⛰️ 𝘼𝘽𝙍𝙄𝙍 𝘾𝙊𝙁𝙍𝙀 | 𝘾𝙊𝙁𝙁𝙀𝙍 ⛰️', '/cofre'], ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu'] ], fkontak, m)  
+await conn.sendButton(m.chat, texto, wm, img, [['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu'] ], fkontak, m)  
 global.db.data.users[m.sender].lastclaim = new Date * 1
 }
 handler.help = ['daily']
 handler.tags = ['xp']
-handler.command = ['daily', 'reclamar', 'reclamo', 'claim'] 
+handler.command = ['coffer', 'cofre', 'abrircofre', 'cofreabrir'] 
 handler.level = 5
 export default handler
 
