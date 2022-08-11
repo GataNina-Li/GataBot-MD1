714724222cef952cd38986c45dd4f4c9283e593b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-10 21:04:43 -0500

    Create fun-ttt.js

---
+++ plugins/fun-ttt.js
@@ -0,0 +1,53 @@
+import TicTacToe from '../lib/tictactoe.js' 
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+conn.game = conn.game ? conn.game : {}
+if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw 'TODAVIA SE ESTA JUGANDO CON ALGUIEN'
+if (!text) throw `DEBE DE AGREGAR UN NOMBRE A LA SALA POR EJEMPLO ${usedPrefix + command} sala GataBot*`
+let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
+if (room) {
+await m.reply('ALGUIEN SE UNIO YA PUEDEN JUGAR!!')
+room.o = m.chat
+room.game.playerO = m.sender
+room.state = 'PLAYING'
+let arr = room.game.render().map(v => {
+return {
+X: '❎',
+O: '⭕',
+1: '1️⃣',
+2: '2️⃣',
+3: '3️⃣',
+4: '4️⃣',
+5: '5️⃣',
+6: '6️⃣',
+7: '7️⃣',
+8: '8️⃣',
+9: '9️⃣',
+}[v]})
+let str = `
+*JUEGO TRES EN RAYAS*
+
+❎ = @${room.game.playerX.split('@')[0]}
+⭕ = @${room.game.playerO.split('@')[0]}
+
+        ${arr.slice(0, 3).join('')}
+        ${arr.slice(3, 6).join('')}
+        ${arr.slice(6).join('')}
+
+TURNO DE @${room.game.currentTurn.split('@')[0]}
+`.trim()
+if (room.x !== room.o) await conn.sendMessage(room.x, { text: str, mentions: this.parseMention(str)}, { quoted: m })
+await conn.sendMessage(room.o, { text: str, mentions: conn.parseMention(str)}, { quoted: m })
+} else {
+room = {
+id: 'tictactoe-' + (+new Date),
+x: m.chat,
+o: '',
+game: new TicTacToe(m.sender, 'o'),
+state: 'WAITING' }
+if (text) room.name = text     
+let imgplay = `https://cope-cdnmed.agilecontent.com/resources/jpg/8/9/1590140413198.jpg`
+conn.sendButton(m.chat, `*TRES EN RAYAS*\n\nESPERANDO AL SEGUNDO JUGADOR*\n*SI QUIERES ABANDONAR LA SALA USA EL COMANDO ${usedPrefix}delttt*`, wm, imgplay, [['UNIRME A LA PARTIDA', `${usedPrefix + command} ${text}`]], m, { mentions: conn.parseMention(text) })
+conn.game[room.id] = room
+}}
+handler.command = /^(tictactoe|ttc|ttt|xo)$/i
+export default handler
