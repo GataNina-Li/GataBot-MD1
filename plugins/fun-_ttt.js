51de108dacdb7fd80c6065d5e2c9d93ce81804c5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-11 00:29:44 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/fun-_ttt.js
+++ plugins/fun-_ttt.js
@@ -79,14 +79,15 @@ let expp2 = Math.floor(Math.random() * 2500)
 let winner = isSurrender ? room.game.currentTurn : room.game.winner
 let str = `
 🫂 𝙅𝙐𝙂𝘼𝘿𝙊𝙍𝙀𝙎 *:* 𝙋𝙇𝘼𝙔𝙀𝙍𝙎
+┈┈┈┈┈┈┈┈┈
 ❎ = @${room.game.playerX.split('@')[0]}
 ⭕ = @${room.game.playerO.split('@')[0]}
-┈┈┈┈┈┈┈┈┈┈┈┈┈
+┈┈┈┈┈┈┈┈┈
      ${arr.slice(0, 3).join('')}
      ${arr.slice(3, 6).join('')}
      ${arr.slice(6).join('')}
-┈┈┈┈┈┈┈┈┈┈┈┈┈
-${isWin ? `@${(isSurrender ? room.game.currentTurn : room.game.winner).split('@')[0]} 😎 *GANASTE!!, POR HABER GANADO OBTIENES*\n\n💎 *${dia2} Diamantes*\n🪙 *${tok2} Tokens*\n🐈 *${gata2} GataCoins*\n⚡ *${expp2} Exp*` : isTie ? `*EMPATE!! 🤨 POR TERMINAR EN EMPATE AMBOS OBTIENEN*\n\n💎 *${dia} Diamantes*\n🪙 *${tok} Tokens*\n🐈 *${gata} GataCoins*\n⚡ *${expp} Exp*` : `TURNO DE @${room.game.currentTurn.split('@')[0]}`}
+┈┈┈┈┈┈┈┈┈
+${isWin ? `@${(isSurrender ? room.game.currentTurn : room.game.winner).split('@')[0]} 😎🏆 *GANASTE!!\nPOR HABER GANADO OBTIENES*\n\n💎 *${dia2} Diamantes*\n🪙 *${tok2} Tokens*\n🐈 *${gata2} GataCoins*\n⚡ *${expp2} Exp*` : isTie ? `*EMPATE!! 🤨\nPOR TERMINAR EN EMPATE AMBOS OBTIENEN*\n\n💎 *${dia} Diamantes*\n🪙 *${tok} Tokens*\n🐈 *${gata} GataCoins*\n⚡ *${expp} Exp*` : `🪄 *TURNO DE* @${room.game.currentTurn.split('@')[0]}`}
 `.trim()
 let users = global.db.data.users
 if ((room.game._currentTurn ^ isSurrender ? room.x : room.o) !== m.chat)
