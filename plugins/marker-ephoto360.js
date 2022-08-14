50e276f30112148e1618473d2e3194ffc8a757d5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-14 01:49:21 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -22,12 +22,12 @@ const fkontak = {
 try {    
 if (command == 'logocorazon') {
 await conn.reply(m.chat, `${eg}𝙀𝙎𝙋𝙀𝙍𝙀 𝙐𝙉 𝙈𝙊𝙈𝙀𝙉𝙏𝙊 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙎𝙐 𝙇𝙊𝙂𝙊 𝙎𝙀 𝙀𝙎𝙏𝘼 𝘾𝙍𝙀𝘼𝙉𝘿𝙊\n\n`, ftroli,  m)
-let res = `https://violetics.pw/api/ephoto360/heart-flashlight?apikey=beta&text=${response[0]}`
+let res = `https://violetics.pw/api/ephoto360/heart-flashlight?apikey=beta&text=${args[0]}`
 conn.sendFile(m.chat, res, 'error.jpg', null, m)}
   
 if (command == 'logochristmas') {
 await conn.reply(m.chat, `${eg}𝙀𝙎𝙋𝙀𝙍𝙀 𝙐𝙉 𝙈𝙊𝙈𝙀𝙉𝙏𝙊 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙎𝙐 𝙇𝙊𝙂𝙊 𝙎𝙀 𝙀𝙎𝙏𝘼 𝘾𝙍𝙀𝘼𝙉𝘿𝙊\n\n`, ftroli,  m)
-let res = `https://violetics.pw/api/ephoto360/christmas-snow?apikey=beta&text=${response[0]}`
+let res = `https://violetics.pw/api/ephoto360/christmas-snow?apikey=beta&text=${args[0]}`
 conn.sendFile(m.chat, res, 'error.jpg', null, m)}
   
 } catch {
