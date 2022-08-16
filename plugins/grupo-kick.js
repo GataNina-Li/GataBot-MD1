a734a1c7486e83290718d62d4b293c6686612f5a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-16 03:25:23 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -30,9 +30,9 @@ handler.help = ['kick @user']
 handler.tags = ['group']
 handler.command = ['kick', 'sacar', 'ban'] ///^(kick|sacar|\-)$/i
 handler.group = true
-//handler.admin = true
+handler.admin = true
 handler.botAdmin = true
-handler.premium = true
+//handler.premium = true
 
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
