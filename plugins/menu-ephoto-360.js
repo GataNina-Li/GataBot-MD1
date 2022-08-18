c00fc4916cf9a43a2c24514f402e109042316e9b
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-18 00:56:01 -0500

    Update menu-ephoto-360.js

--- plugins/menu-ephoto-360.js
+++ plugins/menu-ephoto-360.js
@@ -131,7 +131,7 @@ conn.sendHydrated(m.chat, menu, wm, pp, 'https://github.com/GataNina-Li/GataBot-
 
 handler.help = ['infomenu'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
-handler.command = /^(logos2)$/i
+handler.command = /^(menulogos2)$/i
 //handler.register = true
 handler.exp = 70
 export default handler
