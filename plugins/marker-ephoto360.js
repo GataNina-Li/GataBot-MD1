ace8d710596c26fa9ae5922db4ebf059028d0f3a
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-18 16:33:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -9,8 +9,8 @@ let handler = async (m, { conn, args, command }) => {
         "participants":"0@s.whatsapp.net",
             "remoteJid": "status@broadcast",
             "fromMe": false,
-            "id": "Halo"
-        },
+            "id": "Halo"    
+       },
         "message": {
             "contactMessage": {
                 "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
