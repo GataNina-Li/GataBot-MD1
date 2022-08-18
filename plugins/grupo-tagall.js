e9f791faf83a21c51cd408168d6754b7bcf80a73
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-18 17:16:34 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -36,10 +36,10 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let teks = `╭━〔 *𝙄𝙉𝙑𝙊𝘾𝘼𝙉𝘿𝙊 𝘼𝙇 𝙂𝙍𝙐𝙋𝙊* 〕━⬣\n\n${oi}\n\n`
 for (let mem of participants) {
-teks += `┣➥ @${mem.id.split('@')[0]}\n`}
-teks += `*└* 𝐆𝐀𝐓𝐀𝐁𝐎𝐓 \n\n*ᴳᴬᵀᴬᴮᴼᵀ-ᴹᴰ*`
+teks += `┃➥ @${mem.id.split('@')[0]}\n`}
+teks += `╰━━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
