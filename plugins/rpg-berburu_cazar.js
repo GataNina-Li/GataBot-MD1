7a109f5f7e5c2952e412285b9d8d1daa27318d03
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-20 12:35:26 -0500

    Update rpg-berburu_cazar.js

--- plugins/rpg-berburu_cazar.js
+++ plugins/rpg-berburu_cazar.js
@@ -15,9 +15,9 @@ let handler = async (m, { conn }) => {
 	"participant": "0@s.whatsapp.net"
 }
   
-	let __timers = (new Date - global.db.data.users[m.sender].lastberbru)
-	let _timers = (100000 - __timers)
-	let timers = clockString(_timers)
+	//let __timers = (new Date - global.db.data.users[m.sender].lastberbru)
+	//let _timers = (100000 - __timers)
+	//let timers = clockString(_timers)
 	let user = global.db.data.users[m.sender]
   
 	//if (new Date - global.db.data.users[m.sender].lastberbru > 100000) {
@@ -84,7 +84,10 @@ let handler = async (m, { conn }) => {
 		global.db.data.users[m.sender].babi += rbrb11
 		global.db.data.users[m.sender].ayam += rbrb12
 	
-if (new Date - global.db.data.users[m.sender].lastberburu < 100000) {
+let time = global.db.data.users[m.sender].lastberburu + 100000
+if (new Date - global.db.data.users[m.sender].lastberburu < 100000) throw conn.sendButton(m.chat, `*Parece que estás cansado*\n*por favor descansa ${msToTime(time - new Date())}*\n*Para poder seguir cazando*\n`, wm, null, [
+['Kandang', '.kandang']], fkontak, m)	
+
 		setTimeout(() => {
 			conn.sendHydrated(m.chat, hsl, fkontak, null, null, null, null, null, [
       [null, null]], null)}, 20000)
@@ -107,8 +110,8 @@ if (new Date - global.db.data.users[m.sender].lastberburu < 100000) {
       [null, null]], null)}, 0)
            global.db.data.users[m.sender].lastberburu = new Date * 1	
 		
-} throw conn.sendButton(m.chat, `*Parece que estás cansado*\n*por favor descansa ${timers}*\n*Para poder seguir cazando*\n`, wm, null, [
-['Kandang', '.kandang']], fkontak, m)
+//} throw conn.sendButton(m.chat, `*Parece que estás cansado*\n*por favor descansa ${timers}*\n*Para poder seguir cazando*\n`, wm, null, [
+//['Kandang', '.kandang']], fkontak, m)
 }
 handler.help = ['berburu']
 handler.tags = ['rpg']
@@ -116,16 +119,16 @@ handler.command = /^(berburu|caza(r)?)$/i
 handler.group = true
 export default handler
 
-function clockString(ms) {
+/*function clockString(ms) {
 	let h = Math.floor(ms / 3600000)
 	let m = Math.floor(ms / 60000) % 60
 	let s = Math.floor(ms / 1000) % 60
 	console.log({ ms, h, m, s })
   
 	return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
+}*/
 
-/*function msToTime(duration) {
+function msToTime(duration) {
   var milliseconds = parseInt((duration % 1000) / 100),
     seconds = Math.floor((duration / 1000) % 60),
     minutes = Math.floor((duration / (1000 * 60)) % 60),
@@ -136,4 +139,4 @@ function clockString(ms) {
   seconds = (seconds < 10) ? "0" + seconds : seconds
 
   return hours + " Hora(s) " + minutes + " Minuto(s)" + seconds + " Segundo(s)"
-}*/
+}
