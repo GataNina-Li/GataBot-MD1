0f3bd22761fdb4dd7ca3003261955ff18d2fe98c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-20 18:24:42 -0500

    Update rpg-inventory.js

--- plugins/rpg-inventory.js
+++ plugins/rpg-inventory.js
@@ -194,7 +194,21 @@ let fgif = {
                                }
                               }
                              }
-await conn.sendMessage(m.chat, listMessage, {quoted: ftroli})
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+}
+await conn.sendMessage(m.chat, listMessage, {quoted: fkontak})
 }
 
 if (args[0] == '1') { // Inventario 1
