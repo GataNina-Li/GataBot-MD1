52cfaf7dc4b3b59e3d08103431eb7c16e1ea10c5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-20 12:00:01 -0500

    Update rpg-berburu_cazar.js

--- plugins/rpg-berburu_cazar.js
+++ plugins/rpg-berburu_cazar.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn }) => {
 	let user = global.db.data.users[m.sender]
 	let time = user.lastclaim + 100000
   
-	if (new Date - global.db.data.users[m.sender].lastberbru > 100000) {
+	//if (new Date - global.db.data.users[m.sender].lastberbru > 100000) {
 		let randomaku1 = `${Math.floor(Math.random() * 10)}`
 		let randomaku2 = `${Math.floor(Math.random() * 10)}`
 		let randomaku4 = `${Math.floor(Math.random() * 10)}`
@@ -107,8 +107,8 @@ let handler = async (m, { conn }) => {
       [null, null]], null)}, 0)
            user.lastberburu = new Date * 1	
 		
-	} if (new Date - user.lastclaim < 100000) throw conn.sendButton(m.chat, `\n*Parece que estás cansado*\n*por favor descansa ${msToTime(time - new Date())}*\n*Para poder seguir cazando*\n`, wm, null, [
-		['Kandang', '.kandang']], fkontak, m)
+if (new Date - user.lastclaim < 100000) throw conn.sendButton(m.chat, `*Parece que estás cansado*\n*por favor descansa ${msToTime(time - new Date())}*\n*Para poder seguir cazando*\n`, wm, null, [
+['Kandang', '.kandang']], fkontak, m)
 }
 handler.help = ['berburu']
 handler.tags = ['rpg']
