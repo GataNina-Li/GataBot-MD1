83bca8b00aa81d9eaff6117a3c77870f882456de
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-20 23:20:10 -0500

    Update rpg-shops.js

--- plugins/rpg-shops.js
+++ plugins/rpg-shops.js
@@ -411,7 +411,7 @@ let imgr = flaaa.getRandom()
 🔖 ɪᴛᴇᴍs ʟɪsᴛ :
 ${Object.keys(listItems).map((v) => {
         let paymentMethod = Object.keys(listItems[v]).find(v => v in user)
-        return `⮕ 1 ${global.rpg.emoticon(v)}${v}\n﹫ ${listItems[v][paymentMethod]}\n${global.rpg.emoticon(paymentMethod)}${paymentMethod}`.trim()
+        return `» 1 ${global.rpg.emoticon(v)}${v}\n﹫ ${listItems[v][paymentMethod]}\n${global.rpg.emoticon(paymentMethod)}${paymentMethod}`.trim()
     }).join('\n')}
 –––––––––––––––––––––––––
 💁🏻‍♂ ᴛɪᴩ :
@@ -424,7 +424,7 @@ ${usedPrefix}${command} potion 10
 🔖 ɪᴛᴇᴍs ʟɪsᴛ :
 ${Object.keys(listItems).map((v) => {
         let paymentMethod = Object.keys(listItems[v]).find(v => v in user)
-        return `⮕ 1 ${global.rpg.emoticon(v)}${v} ﹫ ${listItems[v][paymentMethod]} ${global.rpg.emoticon(paymentMethod)}${paymentMethod}`.trim()
+        return `» 1 ${global.rpg.emoticon(v)}${v}\n﹫ ${listItems[v][paymentMethod]}/n/n${global.rpg.emoticon(paymentMethod)}${paymentMethod}`.trim()
     }).join('\n')}
 –––––––––––––––––––––––––
 💁🏻‍♂ ᴛɪᴩ :
