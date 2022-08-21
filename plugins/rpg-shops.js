fe12896aea8bbac6500e61f032ae69a7ad13c208
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-20 21:49:52 -0500

    Create rpg-shops.js

---
+++ plugins/rpg-shops.js
@@ -0,0 +1,505 @@
+const items = {
+    buy: {
+        limit: {
+            exp: 999
+        },
+        potion: {
+            money: 1250,
+        },
+        aqua: {
+           money: 500
+        },
+        trash: {
+            money: 4,
+        },
+        wood: {
+            money: 700
+        },
+        rock: {
+            money: 850
+        },
+        string: {
+            money: 400
+        },
+        iron: { 
+        	money: 3000
+        },
+        diamond: {
+            money: 7500
+        },
+        emerald: {
+            money: 8500
+        },
+        gold: {
+            money: 3500
+        },
+        coal: {
+            money: 1500
+        },
+        common: {
+            money: 200
+        },
+        uncommon: {
+            money: 2000
+        },
+        mythic: {
+            money: 25000
+        },
+        legendary: {
+            money: 75000
+        },
+        foodpet: {
+            money: 3500
+        },
+        Fox: {
+            money: 40000
+        },
+        naga: {
+            money: 120000
+        },
+        pet: {
+            money: 120000
+        },
+        anggur: {
+            money: 2000
+        },
+        apel: {
+            money: 2000
+        },
+        batu: {
+            money: 2000
+        },
+        berlian: {
+            money: 2000
+        },
+        bibitanggur: {
+            money: 2000
+        },
+        bibitapel: {
+            money: 2000
+        },
+        bibitjeruk: {
+            money: 2000
+        },
+        bibitmangga: {
+            money: 2000
+        },
+        bibitpisang: {
+            money: 2000
+        },
+        botol: {
+            money: 2000
+        },
+        centaur: {
+            money: 2000
+        },
+        eleksirb: {
+            money: 2000
+        },
+        emasbatang: {
+            money: 2000
+        },
+        emasbiasa: {
+            money: 2000
+        },
+        exp: {
+            money: 2000
+        },
+        gardenboc: {
+            money: 2000
+        },
+        gardenboxs: {
+            money: 2000
+        },
+        griffin: {
+            money: 2000
+        },
+        healtmonster: {
+            money: 2000
+        },
+        jeruk: {
+            money: 2000
+        },
+        kaleng: {
+            money: 2000
+        },
+        kardus: {
+            money: 2000
+        },
+        kayu: {
+            money: 2000
+        },
+        ketake: {
+            money: 2000
+        },
+        koinexpg: {
+            money: 2000
+        },
+        kucing: {
+            money: 2000
+        },
+        kuda: {
+            money: 2000
+        },
+        kyubi: {
+            money: 2000
+        },
+        makanancentaur: {
+            money: 2000
+        },
+        makanangriffin: {
+            money: 2000
+        },
+        makanankyubi: {
+            money: 2000
+        },
+        makanannaga: {
+            money: 2000
+        },
+        makananpet: {
+            money: 2000
+        },
+        makananphonix: {
+            money: 2000
+        },
+        mangga: {
+            money: 2000
+        },
+        pancingan: {
+            money: 2000
+        },
+        phonix: {
+            money: 2000
+        },
+        pisang: {
+            money: 2000
+        },
+        rubah: {
+            money: 2000
+        },
+        sampah: {
+            money: 2000
+        },
+        serigala: {
+            money: 2000
+        },
+        sword: {
+            money: 2000
+        },
+        tiketcoin: {
+            money: 2000
+        },
+        umpan: {
+            money: 2000
+        }
+    },
+    sell: {
+        limit: {
+            exp: 999
+        },
+        potion: {
+            money: 1250,
+        },
+        aqua: {
+           money: 500
+        },
+        trash: {
+            money: 4,
+        },
+        wood: {
+            money: 700
+        },
+        rock: {
+            money: 850
+        },
+        string: {
+            money: 400
+        },
+        iron: { 
+        	money: 3000
+        },
+        diamond: {
+            money: 7500
+        },
+        emerald: {
+            money: 8500
+        },
+        gold: {
+            money: 3500
+        },
+        coal: {
+            money: 1500
+        },
+        common: {
+            money: 200
+        },
+        uncommon: {
+            money: 2000
+        },
+        mythic: {
+            money: 25000
+        },
+        legendary: {
+            money: 75000
+        },
+        foodpet: {
+            money: 3500
+        },
+        Fox: {
+            money: 40000
+        },
+        naga: {
+            money: 120000
+        },
+        pet: {
+            money: 120000
+        },
+        anggur: {
+            money: 2000
+        },
+        apel: {
+            money: 2000
+        },
+        batu: {
+            money: 2000
+        },
+        berlian: {
+            money: 2000
+        },
+        bibitanggur: {
+            money: 2000
+        },
+        bibitapel: {
+            money: 2000
+        },
+        bibitjeruk: {
+            money: 2000
+        },
+        bibitmangga: {
+            money: 2000
+        },
+        bibitpisang: {
+            money: 2000
+        },
+        botol: {
+            money: 2000
+        },
+        centaur: {
+            money: 2000
+        },
+        eleksirb: {
+            money: 2000
+        },
+        emasbatang: {
+            money: 2000
+        },
+        emasbiasa: {
+            money: 2000
+        },
+        exp: {
+            money: 2000
+        },
+        gardenboc: {
+            money: 2000
+        },
+        gardenboxs: {
+            money: 2000
+        },
+        griffin: {
+            money: 2000
+        },
+        healtmonster: {
+            money: 2000
+        },
+        jeruk: {
+            money: 2000
+        },
+        kaleng: {
+            money: 2000
+        },
+        kardus: {
+            money: 2000
+        },
+        kayu: {
+            money: 2000
+        },
+        ketake: {
+            money: 2000
+        },
+        koinexpg: {
+            money: 2000
+        },
+        kucing: {
+            money: 2000
+        },
+        kuda: {
+            money: 2000
+        },
+        kyubi: {
+            money: 2000
+        },
+        makanancentaur: {
+            money: 2000
+        },
+        makanangriffin: {
+            money: 2000
+        },
+        makanankyubi: {
+            money: 2000
+        },
+        makanannaga: {
+            money: 2000
+        },
+        makananpet: {
+            money: 2000
+        },
+        makananphonix: {
+            money: 2000
+        },
+        mangga: {
+            money: 2000
+        },
+        pancingan: {
+            money: 2000
+        },
+        phonix: {
+            money: 2000
+        },
+        pisang: {
+            money: 2000
+        },
+        rubah: {
+            money: 2000
+        },
+        sampah: {
+            money: 2000
+        },
+        serigala: {
+            money: 2000
+        },
+        sword: {
+            money: 2000
+        },
+        tiketcoin: {
+            money: 2000
+        },
+        umpan: {
+            money: 2000
+        }
+    }
+}
+
+let handler = async (m, { command, usedPrefix, args }) => {
+let imgr = flaaa.getRandom()
+    let user = global.db.data.users[m.sender]
+    const listItems = Object.fromEntries(Object.entries(items[command.toLowerCase()]).filter(([v]) => v && v in user))
+    
+    let text = ''
+    let footer = ''
+    let image = ''
+    let buttons = ''
+    text = (command.toLowerCase() == 'buy' ?
+(`
+*${htki} BUY ${htka}*
+`.trim()) : 
+(`
+*${htki} SELL ${htka}*
+`.trim())
+)
+    footer = (command.toLowerCase() == 'buy' ?
+(`
+🔖 ɪᴛᴇᴍs ʟɪsᴛ :
+${Object.keys(listItems).map((v) => {
+        let paymentMethod = Object.keys(listItems[v]).find(v => v in user)
+        return `⮕ 1 ${global.rpg.emoticon(v)}${v} ﹫ ${listItems[v][paymentMethod]} ${global.rpg.emoticon(paymentMethod)}${paymentMethod}`.trim()
+    }).join('\n')}
+–––––––––––––––––––––––––
+💁🏻‍♂ ᴛɪᴩ :
+⮕ ᴛᴏ ʙᴜʏ ɪᴛᴇᴍs:
+${usedPrefix}${command} [item] [quantity]
+★ ᴇxᴀᴍᴩʟᴇ:
+${usedPrefix}${command} potion 10
+`.trim()) : 
+(`
+🔖 ɪᴛᴇᴍs ʟɪsᴛ :
+${Object.keys(listItems).map((v) => {
+        let paymentMethod = Object.keys(listItems[v]).find(v => v in user)
+        return `⮕ 1 ${global.rpg.emoticon(v)}${v} ﹫ ${listItems[v][paymentMethod]} ${global.rpg.emoticon(paymentMethod)}${paymentMethod}`.trim()
+    }).join('\n')}
+–––––––––––––––––––––––––
+💁🏻‍♂ ᴛɪᴩ :
+⮕ ᴛᴏ sᴇʟʟ ɪᴛᴇᴍs:
+${usedPrefix}${command} [item] [quantity]
+★ ᴇxᴀᴍᴩʟᴇ:
+${usedPrefix}${command} potion 10
+`.trim())
+)
+    image = (command.toLowerCase() == 'buy' ?
+(imgr + 'buy') : 
+(imgr + 'sell')
+)
+    buttons = (command.toLowerCase() == 'buy' ?
+([
+[`ʙᴜʏ ʟɪᴍɪᴛ`, `${usedPrefix}buy limit`],
+[`ʙᴜʏ ᴩᴏᴛɪᴏɴ`, `${usedPrefix}buy potion`]
+]) : 
+([
+[`sᴇʟʟ ᴩᴏᴛɪᴏɴ`, `${usedPrefix}sell potion`],
+[`sᴇʟʟ ᴛʀᴀsʜ`, `${usedPrefix}sell trash`]
+])
+)
+    const item = (args[0] || '').toLowerCase()
+    const total = Math.floor(isNumber(args[1]) ? Math.min(Math.max(parseInt(args[1]), 1), Number.MAX_SAFE_INTEGER) : 1) * 1
+    if (!listItems[item]) return conn.sendButton(m.chat, text, footer, image, buttons, m)
+    if (command.toLowerCase() == 'buy') {
+        let paymentMethod = Object.keys(listItems[item]).find(v => v in user)
+        if (user[paymentMethod] < listItems[item][paymentMethod] * total) return conn.sendButton(m.chat,
+`*–『 INSUFFICIENT CREDIT 』–*`, 
+`ʏᴏᴜ ɴᴇᴇᴅ ᴇxᴛʀᴀ *${(listItems[item][paymentMethod] * total) - user[paymentMethod]}* ${global.rpg.emoticon(paymentMethod)}${paymentMethod} ᴛᴏ ʙᴜʏ *${total}* ${global.rpg.emoticon(item)}${item}.
+ʏᴏᴜ'ᴠᴇ *${user[paymentMethod]}* ${global.rpg.emoticon(paymentMethod)}${paymentMethod} ɪɴ ʙᴀɢ.
+–––––––––––––––––––––––––
+💁🏻‍♂ ᴛɪᴩ :
+ᴏᴩᴇɴ ᴄʀᴀᴛᴇs & ᴄᴏʟʟᴇᴄᴛ ʀᴇᴡᴀʀᴅs.
+⮕ ᴛᴏ ᴏᴩᴇɴ ᴄʀᴀᴛᴇs:
+.open crate
+⮕ ᴛᴏ ᴄᴏʟʟᴇᴄᴛ ʀᴇᴡᴀʀᴅs:
+.adventure | .daily | .monthly
+`.trim(), imgr + 'lowcredit', [
+[`ᴀsᴋ ᴛᴏ ᴀʟʟ`, `${usedPrefix}tagall sᴏᴍᴇʙᴏᴅʏ ᴩʟᴇᴀsᴇ sᴇɴᴅ *${(listItems[item][paymentMethod] * total) - user[paymentMethod]}* ${global.rpg.emoticon(paymentMethod)}${paymentMethod} ᴛᴏ ᴍᴇ.
+⮕ ᴛᴏ ᴛʀᴀɴsғᴇʀ ${paymentMethod}:
+${usedPrefix}transfer ${paymentMethod} ${(listItems[item][paymentMethod] * total) - user[paymentMethod]} @${conn.getName(m.sender)}`]
+], m)
+        user[paymentMethod] -= listItems[item][paymentMethod] * total
+        user[item] += total
+        return conn.sendButton(m.chat,
+`*––––––『 BOUGHT 』––––––*`,
+`ʏᴏᴜ *ʙᴏᴜɢʜᴛ ${total} ${global.rpg.emoticon(item)}${item}*.
+`.trim(), imgr + 'bought', [
+[`ɪɴᴠᴇɴᴛᴏʀʏ`, `${usedPrefix}inventory`]
+], m)
+    } else {
+        if (user[item] < total) return m.reply(`You don't have enough *${global.rpg.emoticon(item)}${item}* to sell, you only have ${user[item]} items`)
+        user[item] -= total
+        user.money += listItems[item].money * total
+        return conn.sendButton(m.chat,
+`*–––––––『 SOLD 』–––––––*`,
+`ʏᴏᴜ *sᴏʟᴅ ${total} ${global.rpg.emoticon(item)}${item}*.
+`.trim(), imgr + 'sold', [
+[`ɪɴᴠᴇɴᴛᴏʀʏ`, `${usedPrefix}inventory`]
+], m)
+    }
+}
+
+handler.help = ['buy', 'sell'].map(v => v + ' [item] [count]')
+handler.tags = ['rpg']
+handler.command = /^(buyy|sell)$/i
+
+handler.disabled = false
+
+export default handler
+
+function isNumber(number) {
+    if (!number) return number
+    number = parseInt(number)
+    return typeof number == 'number' && !isNaN(number)
+}
