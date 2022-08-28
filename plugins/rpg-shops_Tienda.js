e03a1b329e83b4328ae1b35acb7c0a9da2263d76
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-08-28 00:20:21 -0500

    Update rpg-shops_Tienda.js

--- plugins/rpg-shops_Tienda.js
+++ plugins/rpg-shops_Tienda.js
@@ -244,7 +244,7 @@ ${usedPrefix}transfer ${paymentMethod} ${(listItems[item][paymentMethod] * total
        
       let paymentMethod = Object.keys(listItems[item]).find(v => v in user)
         user[item] -= total
-        user.money += listItems[item].money * total
+        user.[item] += listItems[item].[item] * total
         return conn.sendButton(m.chat,
 `*––『 VENDIDO | SOLD 』––*`,
 `${conn.getName(m.sender)} 
