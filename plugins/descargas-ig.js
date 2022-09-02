2ee1349e5e96ea0cc450ab8bbad625f312b5d750
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-02 00:23:29 -0500

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -7,28 +7,20 @@ const results = await instagramdl(args[0])
 .catch(async _ => await instagramdlv3(args[0]))
 .catch(async _ => await instagramdlv4(args[0]))
 for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `✨ *ENLACE | URL:* ${url}`, m)*/
-import fetch from 'node-fetch'
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+import instagramGetUrl from 'fg-ig'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-try {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    const results = await instagramdl(args[0])
-        .catch(async _ => await instagramdlv2(args[0]))
-        .catch(async _ => await instagramdlv3(args[0]))
-        .catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
-    } catch {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/CU0MhPjBZO2/`
-        let res = await fetch(`https://api.lolhuman.xyz/api/instagram2?apikey=9b817532fadff8fc7cb86862&url=${args[0]}`)
-    let x = await res.json()
-    conn.sendButton(m.chat, `*${htki} instagram ${htka}*`, wm, x.result.media[1], [['Mp4', `.get ${x.result.media[0]}`]],m)
-    }
-  
-} 
-handler.help = ['ig'].map(v => v + ' <url>')
+    if (!args[0]) throw `✳️ Uso del comamdo\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+    m.react(rwait) 
+    let results = (await instagramGetUrl(args[0])).url_list[0]
+    conn.sendFile(m.chat, results, 'instagram.mp4', `✅ Resultado`, m)
+    m.react(done)
+}
+
+handler.help = ['instagram <link ig>']
 handler.tags = ['downloader']
 handler.command =/^(instagram|ig(dl)?)$/i
 handler.limit = 2
 handler.exp = 70
+
 export default handler
