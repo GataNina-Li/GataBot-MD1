b1567b075080bda2ff2202f032bae9a89fdf835a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-02 00:28:56 -0500

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -11,10 +11,10 @@ import instagramGetUrl from 'fg-ig'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ Uso del comamdo\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
-    m.react(wait) 
+    m.reply(wait) 
     let results = (await instagramGetUrl(args[0])).url_list[0]
     conn.sendFile(m.chat, results, 'instagram.mp4', `✅ Resultado`, m)
-    m.react(wait)
+    m.reply(wait)
 }
 
 handler.help = ['instagram <link ig>']
