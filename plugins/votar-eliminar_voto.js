d85117db191e7462bb5964ef5331a8a72ffe516c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-02 21:39:45 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/votar-eliminar_voto.js
+++ plugins/votar-eliminar_voto.js
@@ -1,18 +1,38 @@
 let handler = async (m, { conn, usedPrefix, isAdmin, isOwner }) => {
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+}
+
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) return dfail('admin', m, conn)}
   
 let id = m.chat
 conn.vote = conn.vote ? conn.vote : {}
-if (!(id in conn.vote)) await conn.sendButton(m.chat, `No has votado en este Grupo!!`, author, null, [
+if (!(id in conn.vote)) await conn.sendButton(m.chat, `${fg}𝙉𝙊 𝙎𝙀 𝙃𝘼 𝘾𝙍𝙀𝘼𝘿𝙊 𝙑𝙊𝙏𝘼𝘾𝙄𝙊𝙉 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊\n\n𝙉𝙊 𝙑𝙊𝙏𝙀 𝙃𝘼𝙎 𝘽𝙀𝙀𝙉 𝘾𝙍𝙀𝘼𝙏𝙀𝘿 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋`, `*Si quieres crear una nueva votación usa el comando ${usedPrefix}crearvoto*\n\n*If you want to make a new vote use the command ${usedPrefix}startvoto*\n${wm}`, null, [
 ['votar', `${usedPrefix}+vote`],
-['menu', `${usedPrefix}menu`]], m)
+['menu', `${usedPrefix}menu`]], fkontak, m)
   
 delete conn.vote[id]
-m.reply(`Tu voto ha sido eliminado!!`)}
+await conn.sendButton(m.chat, `${eg}𝙇𝘼 𝙑𝙊𝙏𝘼𝘾𝙄𝙊𝙉 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊 𝙃𝘼 𝙎𝙄𝘿𝙊 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝘿𝘼\n\n𝙑𝙊𝙏𝙄𝙉𝙂 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋 𝙃𝘼𝙎 𝘽𝙀𝙀𝙉 𝙍𝙀𝙈𝙊𝙑𝙀𝘿`, `*Si quieres hacer una nueva votación usa el comando ${usedPrefix}crearvoto*\n\n*If you want to make a new vote use the command ${usedPrefix}startvoto*\n${wm}`, null, [
+['votar', `${usedPrefix}+vote`],
+['menu', `${usedPrefix}menu`]], fkontak, m)}
 
 handler.help = ['hapusvote']
 handler.tags = ['vote']
-handler.command = /^(delete|hapus|-)vote$/i
+handler.command = /^(del|eliminar|delete|hapus|-)voto$/i
+handler.group = true
+handler.botAdmin = true
+handler.admin = true
 
 export default handler
