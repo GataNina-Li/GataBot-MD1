b4fecccf0221d521a63f6f89c8fb7d25fc86a00a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-04 02:13:52 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -1,10 +1,13 @@
-let handler = m => m
-export async function all(m) {
+//let handler = m => m
+//export async function all(m) {
+export async function all(m, { conn, isAdmin, isBotAdmin, isOwner }) {
 let user = global.db.data.users[m.sender]
 if (m.chat.endsWith('broadcast')) return
 if (user.premiumTime != 0 && user.premium) {
 if (new Date() * 1 >= user.premiumTime) {
-await m.reply(`*${m.getName(m.sender)} ¡Se acabó tu tiempo premium!*`)
+await conn.sendButton(m.chat, `*@${m.sender.split`@`[0]} ¡Se acabó tu tiempo premium!*`, wm, null, [
+['☘️ 𝙈𝙀𝙉𝙐', '/menu']], false, { mentions: [m.sender] })}
+//await m.reply(`*${m.getName(m.sender)} ¡Se acabó tu tiempo premium!*`)
 user.premiumTime = 0
 user.premium = false
 }}}
