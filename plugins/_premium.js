582c153225a68389f9449bad01a4282c5858c25d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-04 01:09:43 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -16,9 +16,9 @@ let handler = m => m
 export async function all(m) {
     let user = global.db.data.users[m.sender]
     if (m.chat.endsWith('broadcast')) return
-    if (user.premiumTime != 0 && user.premium) {
-        if (new Date() * 1 <= user.premiumTime) {
-            await m.reply(`waktu premium kamu sudah habis!`)
+    let user = global.db.data.users[m.sender]                              
+    if (new Date() - user.premiumTime > 0) {
+            await m.reply(`¡Se acabó tu tiempo premium!`)
             user.premiumTime = 0
             user.premium = false
         }
