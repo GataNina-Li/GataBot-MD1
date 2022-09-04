4607f2b9dec5daf9e4e35c1f632f74966971b746
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-04 18:37:11 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-obtener_premium.js
+++ plugins/rpg-obtener_premium.js
@@ -1,5 +1,43 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
+let handler = async (m, { conn, text, usedPrefix, command, args }) => {
+let user = global.db.data.users[m.sender]   
+const xpperlimit = 3
+
+let count = command.replace(/^buy/i, '')
+count = count ? /premall/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+count = Math.max(1, count)
+    
+let txt = text.replace('').trim()
+if (!txt) throw `donde el numero de dias?`
+if (isNaN(txt)) return m.reply(`único número!\n\nexample:\n${usedPrefix + command} 7`)
+    
+var jumlahHari = 180000 * txt //86400000 son 24 horas
+var now = new Date() * 1
+if (now < user.premiumTime) user.premiumTime += jumlahHari
+    
+if txt >= xpperlimit * count  {
+else user.premiumTime = now + jumlahHari
+user.premium = true
+    
+if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count
+    //global.db.data.users[m.sender].limit += count
+    
+m.reply(`✔️ Éxito
+*Ha Gastado* :  -${xpperlimit * count} 𝙓𝙋
+📛 *Nombre:* ${user.name}
+📆 *minutos:* ${txt} minutos
+📉 *cuenta regresiva:* ${user.premiumTime - now}`)
+} else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎`, m)
+}  
+handler.help = ['prem [@user] <days>']
+handler.command = ['prem', 'buypremall'] 
+//handler.command = /^(add|tambah|\+)p(rem)?$/i
+handler.group = true
+
+
+export default handler
+    
+    /*let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
@@ -13,6 +51,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     else user.premiumTime = now + jumlahHari
 user.premium = true
     m.reply(`✔️ Éxito
+┃ *Compra Efectuada* : +${count} 💎 
+┃ *Ha Gastado* :  -${xpperlimit * count} 𝙓𝙋
+
 📛 *Nombre:* ${user.name}
 📆 *minutos:* ${txt} minutos
 📉 *cuenta regresiva:* ${user.premiumTime - now}`)
@@ -24,4 +65,4 @@ handler.command = /^(add|tambah|\+)p(rem)?$/i
 handler.group = true
 handler.rowner = true
 
-export default handler
+export default handler*/
