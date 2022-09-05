30133bcf1ca0308970e3f9476b2484c95003f8bb
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-04 19:13:47 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-obtener_premium.js
+++ plugins/rpg-obtener_premium.js
@@ -1,22 +1,28 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
+const xpperlimit = 3
+let user = global.db.data.users[m.sender]
+    //let who
+    //if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    //else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `tag or mention someone!`
-    let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `where the number of days?`
-    if (isNaN(txt)) return m.reply(`only number!\n\nexample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
-    var jumlahHari = 86400000 * txt
+    //if (!who) throw `tag or mention someone!`
+    let txt = text.replace('').trim()
+    if (!txt) throw `donde el numero de dias?`
+    if (txt < xpperlimit) throw `No hay diamantes`
+    user.limit -= xpperlimit
+    if (isNaN(txt)) return m.reply(`only number!\n\nexample:\n${usedPrefix + command} 7`)
+    var jumlahHari = 180000 * txt //86400000 son 24 horas
     var now = new Date() * 1
     if (now < user.premiumTime) user.premiumTime += jumlahHari
     else user.premiumTime = now + jumlahHari
-user.premium = true
-    m.reply(`✔️ Success
-📛 *Name:* ${user.name}
-📆 *Days:* ${txt} days
-📉 *Countdown:* ${user.premiumTime - now}`)
-}
+    user.premium = true
+    
+    m.reply(`✔️ Éxito
+*Ha Gastado* :  -${xpperlimit} 💎
+📛 *Nombre:* ${user.name}
+📆 *minutos:* ${txt} minutos
+📉 *cuenta regresiva:* ${user.premiumTime - now}`)
+}  
 handler.help = ['addprem [@user] <days>']
 handler.tags = ['owner']
 handler.command = /^(add|tambah|\+)p(rem)?$/i
