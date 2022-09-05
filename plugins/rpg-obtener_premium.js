f9696636eb9fcd6f2b6fcbd2ce8ed750039d2e41
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-04 19:07:15 -0500

    Update rpg-obtener_premium.js

--- plugins/rpg-obtener_premium.js
+++ plugins/rpg-obtener_premium.js
@@ -1,4 +1,33 @@
-const xpperlimit = 3
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = global.db.data.users[who]
+    if (!who) throw `tag or mention someone!`
+    let txt = text.replace('@' + who.split`@`[0], '').trim()
+    if (!txt) throw `where the number of days?`
+    if (isNaN(txt)) return m.reply(`only number!\n\nexample:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+    var jumlahHari = 86400000 * txt
+    var now = new Date() * 1
+    if (now < user.premiumTime) user.premiumTime += jumlahHari
+    else user.premiumTime = now + jumlahHari
+user.premium = true
+    m.reply(`✔️ Success
+📛 *Name:* ${user.name}
+📆 *Days:* ${txt} days
+📉 *Countdown:* ${user.premiumTime - now}`)
+}
+handler.help = ['addprem [@user] <days>']
+handler.tags = ['owner']
+handler.command = /^(add|tambah|\+)p(rem)?$/i
+
+handler.group = true
+handler.rowner = true
+
+export default handler
+
+
+/*const xpperlimit = 3
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
 let user = global.db.data.users[m.sender]   
 
@@ -33,7 +62,7 @@ handler.help = ['prem [@user] <days>']
 handler.command = ['obtenerprem', 'buyprem'] 
 //handler.command = /^(add|tambah|\+)p(rem)?$/i
 handler.group = true
-export default handler
+export default handler*/
     
     /*let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
