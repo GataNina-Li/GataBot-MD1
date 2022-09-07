dcb0cf3c82cb761f41fda6f7f668a128fa7fdf72
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-07 02:36:40 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -279,6 +279,7 @@ export async function handler(chatUpdate) {
               if (!isNumber(user.lastcodereg)) user.lastcodereg = 0
               if (!isNumber(user.lastcrusade)) user.lastcrusade = 0
               if (!isNumber(user.lastdagang)) user.lastdagang = 0
+	      if (!isNumber(user.lastdiamantes)) user.lastdiamantes = 0    
               if (!isNumber(user.lastduel)) user.lastduel = 0
               if (!isNumber(user.lastdungeon)) user.lastdungeon = 0
               if (!isNumber(user.lasteasy)) user.lasteasy = 0
@@ -683,6 +684,7 @@ export async function handler(chatUpdate) {
                     lastcrusade: 0,
                     lastdaang: 0,
                     lastdagang: 0,
+		    lastdiamantes: 0,
                     lastduel: 0,
                     lastdungeon: 0,
                     lasteasy: 0,
