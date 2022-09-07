7dfc45bf2d7b462db808bc881cf79703d8f4ff19
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-09-07 16:34:17 -0500

    Update _anti+1.js

--- plugins/_anti+1.js
+++ plugins/_anti+1.js
@@ -1,7 +1,8 @@
 let handler = m => m
 
-export async function (m) {
+handler.before = async function (m) {
    if (m.sender.startsWith('1' || '1')) {
    	global.db.data.users[m.sender].banned = true
    }
    
+export default handler
