7f74831255dd400cc5b51e667c797c9c89e0cff7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-07 17:21:07 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_anti+1.js
+++ plugins/_anti+1.js
@@ -26,8 +26,8 @@ if (isBotAdmin) {
 		
 if (m.sender.startsWith('1' || '1')) {
 global.db.data.users[m.sender].banned = true
-conn.reply(htki + ' Número prohibido ' + htka)
-conn.reply(htki + ' Expulsar al usuario ' + htka)
+return conn.reply(htki + ' Número prohibido ' + htka)
+return conn.reply(htki + ' Expulsar al usuario ' + htka)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 }}}
 export default handler
