b421d4e3ff5ed9023957865b2726b408d129a6bc
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-09-07 15:51:28 -0300

    Update grupo-telegram.js

--- plugins/grupo-telegram.js
+++ plugins/grupo-telegram.js
@@ -5,8 +5,8 @@ Nuestro grupo de Telegram!
 Link: https://t.me/FutabuClub
 `.trim()
   
-conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/ColapsusHD/FutabuBot-MD', '𝙵𝚞𝚝𝚊𝚋𝚞𝙱𝚘𝚝-𝙼𝙳', null, null, [
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', 'https://t.me/FutabuClub']
+conn.sendHydrated(m.chat, str, wm, media, 'https://t.me/FutabuClub', 'ENTRAR | JOIN 🤠', null, null, [
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}
 
 handler.command = /^telegram|grupodetelegram|linktelegram$/i
