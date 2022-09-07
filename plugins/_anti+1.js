b17ad2e9133136af825072db3bdaed812939c714
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-07 17:11:54 -0500

    Update _anti+1.js

--- plugins/_anti+1.js
+++ plugins/_anti+1.js
@@ -8,11 +8,26 @@ handler.before = async function (m) {
 export default handler */
 
 
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
+/*export async function before(m, { conn, isAdmin, isBotAdmin }) {
 let user = global.db.data.users[m.sender]
    if (m.sender.startsWith('1' || '1')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply(htki + ' Número prohibido ' + htka)
 conn.reply(htki + ' Expulsar al usuario ' + htka)
 conn.groupParticipantsUpdate(m.chat, [user], "remove")
-}}
+}}*/
+
+import db from '../lib/database.js'
+let handler = m => m
+handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
+if (!m.isGroup) return !1
+let chat = global.db.data.chats[m.chat]
+if (isBotAdmin) {
+		
+if (m.sender.startsWith('1' || '1')) {
+global.db.data.users[m.sender].banned = true
+conn.reply(htki + ' Número prohibido ' + htka)
+conn.reply(htki + ' Expulsar al usuario ' + htka)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+}}}
+export default handler
