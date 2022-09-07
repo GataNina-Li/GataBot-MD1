e8bf8c0eb9d8049e07270a717bd7e0e8267500ed
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-09-07 15:36:23 -0300

    Update grupo-telegram.js

--- plugins/grupo-telegram.js
+++ plugins/grupo-telegram.js
@@ -1,12 +1,12 @@
 let media = './media/menus/telegramfutabuclub.jpg'
 let handler = async (m, { conn, command }) => {
 let str = `
-Nuestro grupo de Telegram
-Link:https://t.me/FutabuClub
+Nuestro grupo de Telegram!
+Link: https://t.me/FutabuClub
 `.trim()
   
 conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/ColapsusHD/FutabuBot-MD', '𝙵𝚞𝚝𝚊𝚋𝚞𝙱𝚘𝚝-𝙼𝙳', null, null, [
-['ENTRAR | JOIN 🤠', 'https://t.me/+8jpGLLS7_3A5YTIx']
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', 'https://t.me/FutabuClub']
 ], m,)}
 
 handler.command = /^telegram|grupodetelegram|linktelegram$/i
