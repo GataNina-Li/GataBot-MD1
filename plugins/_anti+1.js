66ab2619bbefee00650bca1eb9d7714fa5042ab0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-07 17:01:07 -0500

    Update _anti+1.js

--- plugins/_anti+1.js
+++ plugins/_anti+1.js
@@ -6,3 +6,12 @@ handler.before = async function (m) {
    }
    
 export default handler */
+
+
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+   if (m.sender.startsWith('1' || '1')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply(htki + ' Número prohibido ' + htka)
+conn.reply(htki + ' Expulsar al usuario ' + htka)
+conn.groupParticipantsUpdate(m.chat, [user], "remove")
+}}
