b9799537bb7416ebefb93f2ad550b93f0509e5fb
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-08 01:48:07 -0500

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -92,9 +92,9 @@ let potion = `${pickRandom(['1', '2', '3', '4', '5'])}`
 let potionpremium = `${pickRandom(['2', '4', '6', '9', '12'])}`
 
 const recompensas = {
-  exp: premium ? exppremium : exp,
-  money: premium ? moneypremium : money,
-  potion: premium ? potionpremium : potion,
+  exp: `${premium ? exppremium : exp}`,
+  money: `${premium ? moneypremium : money}`,
+  potion: `${premium ? potionpremium : potion}`,
 }
 
 let time = user.lastclaim + 7200000 //2 Horas 
