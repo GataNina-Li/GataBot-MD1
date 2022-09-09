0fbe2c48898d645dfae806e6a679a26c38d9505a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-09 18:21:48 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -111,10 +111,9 @@ const fkontak = {
 	},
 	"participant": "0@s.whatsapp.net"
 }
-
+await conn.sendMessage(m.chat, listMessage, {quoted: fkontak})
 //await conn.sendMessage(m.chat, listMessage)
 //await conn.sendMessage(m.chat, { text: listMessage, mentions: [m.sender] }, { quoted: m, enlace })
-await conn.sendMessage(m.chat, listMessage, {quoted: fkontak})
 //await conn.sendMessage(m.chat, { text: listMessage, mentions: [m.sender] }, { quoted: fkontak, m })
 }
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
