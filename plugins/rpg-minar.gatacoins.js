1a5a2d9577920905b33a5ac2ba35d302fb9508e4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-09 03:09:35 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-minar.gatacoins.js
+++ plugins/rpg-minar.gatacoins.js
@@ -1,24 +1,3 @@
-/*let handler = async (m, { conn, isPrems}) => { //lastmiming
-let minar = `${pickRandom(['Que pro 😎 has minado','🌟✨ Genial!! Obtienes','WOW!! eres un(a) gran Minero(a) ⛏️ Obtienes','Felicidades!! Ahora tienes','⛏️⛏️⛏️ Obtienes'])}`
-let pp = 'https://media.tvalacarta.info/programas/0/clantve/44-gatos/aida-la-gata-robot.jpg'
-
-let gata = Math.floor(Math.random() * 1000)
-global.db.data.users[m.sender].money += gata * 1  
-//let hasil = Math.floor(Math.random() * 2000)
-let time = global.db.data.users[m.sender].lastcoins + 600000
-if (new Date - global.db.data.users[m.sender].lastcoins < 600000) throw `*💟 Vuelva en ${msToTime(time - new Date())} para continuar minando ⛏️*`  
-
-conn.sendHydrated(m.chat, `*${minar} ${gata} 𝙂𝙖𝙩𝙖𝘾𝙤𝙞𝙣𝙨*`, wm, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙈𝙞𝙣𝙖𝙧 𝙀𝙓𝙋 ⚡', `.minar`],
-['𝙈𝙞𝙣𝙖𝙧 𝘿𝙞𝙖𝙢𝙖𝙣𝙩𝙚𝙨 💎', `.minar3`],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `.menu`]
-], m,)
-global.db.data.users[m.sender].lastcoins = new Date * 1  
-  
-
-//m.reply(`*${minar} *${hasil} 𝙓𝙋*`)
-
-}*/
 let handler = async (m, { conn, isPrems}) => { //lastmiming
 const fkontak = {
         "key": {
