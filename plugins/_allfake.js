5dce412f94b617c369b4bde1765dfbbbd7e86290
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-09 19:02:17 -0500

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -7,10 +7,10 @@ import moment from 'moment-timezone'
 
 let handler = async (m, { conn, args }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+let name = await conn.getName(who)
 let fsizedoc = '1'.repeat(10)
 //let ucapan = ucapan()
 let sap = ['Hai', 'Ohayo', 'Kyaa', 'Halo', 'Nyann']
-let name = await conn.getName(who)
 let sgh = md
 let sgc = nnn
 let gata = 'https://i.imgur.com/EXTbyyn.jpg'
@@ -28,6 +28,7 @@ global.fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "sta
 //VERIFICADO MAS IMAGEN DEL USUARIO
 	
 global.fkontak2 = { key: { participant: '0@s.whatsapp.net' }, message: { contactMessage: { displayName: wm, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${author},;;;\nFN:${author},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, jpegThumbnail: fs.readFileSync('./media/menus/Menu1.jpg'), thumbnail: fs.readFileSync('./media/menus/Menu1.jpg'), sendEphemeral: true }}}
+//CONTACTO CON MENSAJE PERSONALIZADO MAS FOTO DE USUARIO
 	
 global.ftroli = { key: { participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: fsizedoc, status: 1, surface: 1, message: botdate, orderTitle: author, sellerJid: '0@s.whatsapp.net' }}}
 
@@ -55,8 +56,8 @@ let fimgv = {
 				}
 			}
 
-//await conn.reply(m.chat, `*Prueba*`, fimgv, m)
-await conn.sendButton(m.chat, `*Prueba*`, wm, pp, [[' Menu', '/menu']], fkontak2, m)	
+//await conn.reply(m.chat, `*Prueba*`, ftroli, m)
+await conn.sendButton(m.chat, `*Prueba*`, wm, pp, [[' Menu', '/menu']], fimgv, m)	
 }
 
 handler.command = /^(fake)$/i
