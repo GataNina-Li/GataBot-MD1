56311fb28673ca1be2b8a4d1536fc21718c46048
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-10 18:19:06 -0500

    Update config-on y off.js

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -432,6 +432,15 @@ global.doc = pdoc.getRandom()
 global.ucapan = ucapan()
 global.fsizedoc = '1'.repeat(10)
 global.fpagedoc = '1'.repeat(10)
+global.tumhiho = situm
+global.situm = await this.resize(thumbnailUrl.getRandom(), 300, 150)
+global.thumbnailUrl = [
+'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg','https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
+'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg','https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
+'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg','https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
+'https://telegra.ph/file/55e5af5f33fbd57104187.jpg','https://telegra.ph/file/af236598456b95884bd15.jpg',
+'https://telegra.ph/file/de92ed4a729887ffc974c.jpg','https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg']
+
 	
 let adReply = {
 		fileLength: fsizedoc, seconds: fsizedoc,
