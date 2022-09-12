7e7701a261e1502de9fb7e25f99ea576dceb1862
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-12 14:53:40 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rpg-shops_Tienda.js
+++ plugins/rpg-shops_Tienda.js
@@ -74,7 +74,7 @@ const items = {
         makananphonix: { tiketcoin: 16 }
     },
     sell: {
-        exp: { trash: pickRandom([0, 0, 1, 0, 0])},
+        exp: { trash: pickRandom([0, 1])},
         limit: { eleksirb: 4 },
         diamond: { tiketcoin: 2 },
         joincount: { emasbatang: 2, },
