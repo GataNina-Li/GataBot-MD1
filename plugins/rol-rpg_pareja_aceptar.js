e3887d0c6cd3920852aa827d2cee0553bf3313ee
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-13 17:19:51 -0500

    Update rol-rpg_pareja_aceptar.js

--- plugins/rol-rpg_pareja_aceptar.js
+++ plugins/rol-rpg_pareja_aceptar.js
@@ -28,16 +28,17 @@ var user = number + '@s.whatsapp.net'
 let users = m.isGroup ? participants.find(v => areJidsSameUser(v.jid == user)) : {}
 let yo = conn.getName(m.sender)
 let tu = conn.getName(who)
+let me = m.sender
 
 if(!users) return conn.reply(m.chat, `*PERSONA NO ENCONTRADA, DEBE DE ESTAR EN ESTE GRUPO*`, m)
 if(user === m.sender) return conn.reply(m.chat, `*NO PUEDE SER PAREJA USTED MISMO*`, m)
 if(user === conn.user.jid) return conn.reply(m.chat, `*YO NO PUEDO SER SU PAREJA JAJAJ*`, m)
     
 if(global.db.data.users[user].pasangan != m.sender){
-conn.reply(m.chat, `LA SOLICITUD DE @${tu} PARA QUE SEAS SU PAREJA HA CADUCADO`, m, { contextInfo: { mentionedJid: [user]}})
+conn.reply(m.chat, `*NO PUEDES ACEPTAR SI NADIE SE HA DECLARADO, DECLARATE CON ${ number ? @user.split('@')[0] : tu} PARA DIGA SI TE ACEPTA O TE RECHAZA*`, m, { contextInfo: { mentionedJid: [user]}})
 }else{
 global.db.data.users[m.sender].pasangan = user
-conn.reply(m.chat, `Felicitaciones, oficialmente están saliendo. @${tu}\n\nQue dure para siempre y siempre sea feliz @${tu} 💓 @${yo} 🥳🥳🥳`,m, { contextInfo: { mentionedJid: [user, tu, yo]}})
+conn.reply(m.chat, `*Felicitaciones, oficialmente están saliendo. ${ number ? @user.split('@')[0] : tu}*\n\n*Que dure para siempre y siempre sea feliz ${ number ? @user.split('@')[0] : tu} 💓 ${ number ? @me.split('@')[0] : tu} 🥳🥳🥳*`,m, { contextInfo: { mentionedJid: [user, tu, yo]}})
 }}	
 }
 handler.command = /^(aceptar|acepto)$/i
