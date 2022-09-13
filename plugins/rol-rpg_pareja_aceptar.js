2116b76b1532ad20aecf5ee220060f6d139e5ee9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-13 16:29:16 -0500

    Update rol-rpg_pareja_aceptar.js

--- plugins/rol-rpg_pareja_aceptar.js
+++ plugins/rol-rpg_pareja_aceptar.js
@@ -1,37 +1,43 @@
 import { areJidsSameUser } from '@adiwajshing/baileys'
 
 let handler = async (m, { conn, text, participants, groupMetadata }) => {
-	if(isNaN(text)) {
-  	var number = text.split`@`[1]
-  } else if(!isNaN(text)) {
-  	var number = text
-  }
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+//let username = conn.getName(who)
 
-  if(!text && !m.quoted) return conn.reply(m.chat, `Proporcione un número, una etiqueta o responda al mensaje de destino.`, m)
-  
-  if(isNaN(number)) return conn.reply(m.chat, `¡El número que ingresaste está mal!`, m)
-  if(number.length > 15) return conn.reply(m.chat, `Formato incorrecto!`, m)
-  try {
-		if(text) {
-			var user = number + '@s.whatsapp.net'
-		} else if(m.quoted.sender) {
-			var user = m.quoted.sender
-		} else if(m.mentionedJid) {
-  		  var user = number + '@s.whatsapp.net'
-			}  
-		} catch (e) {
-  } finally {
-    let users = m.isGroup ? participants.find(v => areJidsSameUser(v.jid == user)) : {}
-    if(!users) return conn.reply(m.chat, `Destino o Número no encontrado, puede haber dejado o no ser miembro de este grupo.`, m)
-    if(user === m.sender) return conn.reply(m.chat, `¡No puedo salir conmigo mismo!`, m)
-    if(user === conn.user.jid) return conn.reply(m.chat, `No puedes salir conmigo t_t`, m)
-    let me = m.sender
+if(isNaN(text)) {
+var number = text.split`@`[1]
+} else if(!isNaN(text)) {
+var number = text
+}
+
+if(!text && !m.quoted) return conn.reply(m.chat, `*ESCRIBA EL NUMERO, ETIQUETE O RESPONDA AL MENSAJE DE LA PERSONA QUE QUIERE QUE SEA SU PAREJA*`, m) 
+if(isNaN(number)) return conn.reply(m.chat, `*EL NUMERO QUE ESCRIBIÓ NO ES VÁLIDO*`, m)
+if(number.length > 15) return conn.reply(m.chat, `*MUCHOS NÚMEROS, NO ES VÁLIDO*`, m)
+	
+try {
+if(text) {
+var user = number + '@s.whatsapp.net'
+} else if(m.quoted.sender) {
+var user = conn.getName(m.quoted.sender)
+} else if(m.mentionedJid) {
+var user = number + '@s.whatsapp.net'
+}  
+} catch (e) {
+} finally {
+let users = m.isGroup ? participants.find(v => areJidsSameUser(v.jid == user)) : {}
+let yo = conn.getName(m.sender)
+//let tu = conn.getName(who)
+    if(!users) return conn.reply(m.chat, `*PERSONA NO ENCONTRADA, DEBE DE ESTAR EN ESTE GRUPO*`, m)
+    if(user === m.sender) return conn.reply(m.chat, `*NO PUEDE SER PAREJA USTED MISMO*`, m)
+    if(user === conn.user.jid) return conn.reply(m.chat, `*YO NO PUEDO SER SU PAREJA JAJAJ*`, m)
+    
 
     if(global.db.data.users[user].pasangan != m.sender){
-      conn.reply(m.chat, `Maaf @${user.split('@')[0]} no dispararte`, m, { contextInfo: { mentionedJid: [user]}})
+      conn.reply(m.chat, `LA SOLICITUD DE @${conn.getName(m.sender)} PARA QUE SEAS SU PAREJA HA CADUCADO`, m, { contextInfo: { mentionedJid: [user]}})
     }else{
       global.db.data.users[m.sender].pasangan = user
-      conn.reply(m.chat, `Felicitaciones, oficialmente están saliendo. @${user.split('@')[0]}\n\nQue dure para siempre y siempre sea feliz @${user.split('@')[0]} 💓 @${me.split('@')[0]} 🥳🥳🥳`,m, { contextInfo: { mentionedJid: [user, me]}})
+      conn.reply(m.chat, `Felicitaciones, oficialmente están saliendo. @${conn.getName(m.sender)}\n\nQue dure para siempre y siempre sea feliz @${conn.getName(m.sender)} 💓 ${yo} 🥳🥳🥳`,m, { contextInfo: { mentionedJid: [user, yo]}})
     }
 	}	
 }
