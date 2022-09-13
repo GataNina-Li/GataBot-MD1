cdee1b45095d719e7600ff7fb4a8652b3122bcfe
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-13 17:59:09 -0500

    Update rol-rpg_pareja_aceptar.js

--- plugins/rol-rpg_pareja_aceptar.js
+++ plugins/rol-rpg_pareja_aceptar.js
@@ -1,5 +1,4 @@
 import { areJidsSameUser } from '@adiwajshing/baileys'
-
 let handler = async (m, { conn, text, participants, groupMetadata }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
@@ -10,9 +9,9 @@ var number = text.split`@`[1]
 var number = text
 }
 
-if(!text && !m.quoted) return conn.reply(m.chat, `*ESCRIBA EL NUMERO, ETIQUETE O RESPONDA AL MENSAJE DE LA PERSONA QUE QUIERE QUE SEA SU PAREJA*`, m) 
-if(isNaN(number)) return conn.reply(m.chat, `*EL NUMERO QUE ESCRIBIÓ NO ES VÁLIDO*`, m)
-if(number.length > 15) return conn.reply(m.chat, `*MUCHOS NÚMEROS, NO ES VÁLIDO*`, m)
+if(!text && !m.quoted) return conn.reply(m.chat, `*ETIQUETE O RESPONDA AL MENSAJE DE LA PERSONA QUE QUIERE QUE SEA SU PAREJA*`, m) 
+if(isNaN(number)) return conn.reply(m.chat, `*DEBE SOLO DE ETIQUETAR O RESPONDER AL MENSAJE DE LA PERSONA QUE QUIERE QUE SEA SU PAREJA*`, m)
+if(number.length > 2) return conn.reply(m.chat, `*DEBE SOLO DE ETIQUETAR O RESPONDER AL MENSAJE DE LA PERSONA QUE QUIERE QUE SEA SU PAREJA*`, m)
 	
 try {
 if(text) {
@@ -28,19 +27,17 @@ var user = number + '@s.whatsapp.net'
 let users = m.isGroup ? participants.find(v => areJidsSameUser(v.jid == user)) : {}
 let yo = conn.getName(m.sender)
 let tu = conn.getName(who)
-let me = m.sender
 
 if(!users) return conn.reply(m.chat, `*PERSONA NO ENCONTRADA, DEBE DE ESTAR EN ESTE GRUPO*`, m)
 if(user === m.sender) return conn.reply(m.chat, `*NO PUEDE SER PAREJA USTED MISMO*`, m)
 if(user === conn.user.jid) return conn.reply(m.chat, `*YO NO PUEDO SER SU PAREJA JAJAJ*`, m)
     
 if(global.db.data.users[user].pasangan != m.sender){
-conn.reply(m.chat, `*NO PUEDES ACEPTAR SI NADIE SE HA DECLARADO, DECLARATE CON ${ number ? tu : `@${user.split('@')[0]}`} PARA DIGA SI TE ACEPTA O TE RECHAZA*`, m, { contextInfo: { mentionedJid: [user]}})
+conn.reply(m.chat, `*NO PUEDES ACEPTAR SI NADIE SE HA DECLARADO, DECLARATE CON $tu} PARA QUE DIGA SI TE ACEPTA O TE RECHAZA*`, m, { contextInfo: { mentionedJid: [user]}})
 }else{
 global.db.data.users[m.sender].pasangan = user
-conn.reply(m.chat, `*Felicitaciones, oficialmente están saliendo. ${ number ? tu : `@${user.split('@')[0]}`}*\n\n*Que dure para siempre y siempre sea feliz ${ number ? tu : `@${user.split('@')[0]}`} 💓 ${ number ? yo : `@${me.split('@')[0]}`} 🥳🥳🥳*`,m, { contextInfo: { mentionedJid: [user, tu, yo]}})
-}}	
-}
+conn.reply(m.chat, `*FELICITACIONES!!! 🥳😻 {tu}*\n*✅ DE MANERA OFICIAL ESTAN EN UNA RELACION*\n\n*QUE DURE POR SIEMPRE SU AMOR Y FELICIDAD 💖😁\n💟 ${tu} 💞 ${yo}*`,m, { contextInfo: { mentionedJid: [user, tu, yo]}})
+}}}
 handler.command = /^(aceptar|acepto)$/i
 handler.group = true
 export default handler
