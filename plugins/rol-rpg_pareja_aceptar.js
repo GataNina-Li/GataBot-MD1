888902a08562393f8d0ffdfaed8a07df03d4cd4e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-13 16:59:21 -0500

    Update rol-rpg_pareja_aceptar.js

--- plugins/rol-rpg_pareja_aceptar.js
+++ plugins/rol-rpg_pareja_aceptar.js
@@ -3,7 +3,6 @@ import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, text, participants, groupMetadata }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-//let username = conn.getName(who)
 
 if(isNaN(text)) {
 var number = text.split`@`[1]
@@ -25,23 +24,22 @@ var user = number + '@s.whatsapp.net'
 }  
 } catch (e) {
 } finally {
+	
 let users = m.isGroup ? participants.find(v => areJidsSameUser(v.jid == user)) : {}
 let yo = conn.getName(m.sender)
 let tu = conn.getName(who)
-    if(!users) return conn.reply(m.chat, `*PERSONA NO ENCONTRADA, DEBE DE ESTAR EN ESTE GRUPO*`, m)
-    if(user === m.sender) return conn.reply(m.chat, `*NO PUEDE SER PAREJA USTED MISMO*`, m)
-    if(user === conn.user.jid) return conn.reply(m.chat, `*YO NO PUEDO SER SU PAREJA JAJAJ*`, m)
-    
 
-    if(global.db.data.users[user].pasangan != m.sender){
-      conn.reply(m.chat, `LA SOLICITUD DE ${conn.getName(who)} PARA QUE SEAS SU PAREJA HA CADUCADO`, m, { contextInfo: { mentionedJid: [user]}})
-    }else{
-      global.db.data.users[m.sender].pasangan = user
-      conn.reply(m.chat, `Felicitaciones, oficialmente están saliendo. ${conn.getName(who)}\n\nQue dure para siempre y siempre sea feliz ${conn.getName(who)} 💓 @${yo} 🥳🥳🥳`,m, { contextInfo: { mentionedJid: [user, who, yo]}})
-    }
-	}	
+if(!users) return conn.reply(m.chat, `*PERSONA NO ENCONTRADA, DEBE DE ESTAR EN ESTE GRUPO*`, m)
+if(user === m.sender) return conn.reply(m.chat, `*NO PUEDE SER PAREJA USTED MISMO*`, m)
+if(user === conn.user.jid) return conn.reply(m.chat, `*YO NO PUEDO SER SU PAREJA JAJAJ*`, m)
+    
+if(global.db.data.users[user].pasangan != m.sender){
+conn.reply(m.chat, `LA SOLICITUD DE @${tu} PARA QUE SEAS SU PAREJA HA CADUCADO`, m, { contextInfo: { mentionedJid: [user]}})
+}else{
+global.db.data.users[m.sender].pasangan = user
+conn.reply(m.chat, `Felicitaciones, oficialmente están saliendo. @${tu}\n\nQue dure para siempre y siempre sea feliz @${tu} 💓 @${yo} 🥳🥳🥳`,m, { contextInfo: { mentionedJid: [user, tu, yo]}})
+}}	
 }
 handler.command = /^(aceptar|acepto)$/i
 handler.group = true
-handler.fail = null
 export default handler
