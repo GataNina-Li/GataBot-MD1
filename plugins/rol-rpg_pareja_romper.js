017984557607e3ff1c2a131eb22a37f0d325557e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-13 13:20:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/rol-rpg_pareja_romper.js
@@ -0,0 +1,29 @@
+
+let handler = async (m, { conn }) => {
+  var ayg = global.db.data.users[m.sender]
+  var beb = global.db.data.users[global.db.data.users[m.sender].pasangan]
+
+  if(ayg.pasangan == ""){
+    return conn.reply(m.chat,`No tienes pareja.`,m)
+  }
+  if (typeof beb == "undefined"){
+    conn.reply(m.chat,`Rompió con éxito con @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: {
+      mentionedJid: [global.db.data.users[m.sender].pasangan]
+    }})
+    ayg.pasangan = ""
+  }
+
+  if (m.sender == beb.pasangan){
+    conn.reply(m.chat,`Usted Rompió con éxito con @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: {
+      mentionedJid: [global.db.data.users[m.sender].pasangan]
+    }})
+    ayg.pasangan = ""
+    beb.pasangan = ""
+  }else {
+    conn.reply(m.chat,`no tienes pareja.`,m)
+  }
+}
+handler.command = /^(romper)$/i
+handler.group = true
+handler.fail = null
+export default handler
