729003b6f9a665820f470a208979e96749202f8f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-14 17:19:08 -0500

    Update rol-rpg_pareja_romper.js

--- plugins/rol-rpg_pareja_romper.js
+++ plugins/rol-rpg_pareja_romper.js
@@ -1,36 +1,28 @@
-let handler = async (m, { conn, text }) => {
-var number = text.split`@`[1]
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-try {
-if(text) {
-var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-var user = conn.getName(m.quoted.sender)
-} else if(m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-}  
-} catch (e) {
-} finally {
 
-var ayg = global.db.data.users[m.sender] //@${global.db.data.users[m.sender].pasangan.split('@')[0]}
-var beb = global.db.data.users[global.db.data.users[m.sender].pasangan]
-var tu = conn.getName(m.sender)
+let handler = async (m, { conn }) => {
+  var ayg = global.db.data.users[m.sender]
+  var beb = global.db.data.users[global.db.data.users[m.sender].pasangan]
 
-if(ayg.pasangan == ""){
-return conn.reply(m.chat,`No tienes pareja.`,m)}
-  
-if (typeof beb == "undefined"){
-conn.reply(m.chat,`Rompió con éxito con ${tu}`, m, {contextInfo: { mentionedJid: [tu] }})
-ayg.pasangan = ""
+  if(ayg.pasangan == ""){
+    return conn.reply(m.chat,`No tienes pareja.`,m)
+  }
+  if (typeof beb == "undefined"){
+    conn.reply(m.chat,`rompió con éxito con @${global.db.data.users[m.sender].pasangan.split('@')[0]} ${conn.getName(m.beb)}`,m,{contextInfo: {
+      mentionedJid: [global.db.data.users[m.sender].pasangan]
+    }})
+    ayg.pasangan = ""
+  }
+
+  if (m.sender == beb.pasangan){
+    conn.reply(m.chat,`rompió con éxito con @${global.db.data.users[m.sender].pasangan.split('@')[0]} ${conn.getName(m.beb)}`,m,{contextInfo: {
+      mentionedJid: [global.db.data.users[m.sender].pasangan]
+    }})
+    ayg.pasangan = ""
+    beb.pasangan = ""
+  }else {
+    conn.reply(m.chat,`No tienes pareja.`,m)
+  }
 }
-
-if (m.sender == beb.pasangan){
-conn.reply(m.chat,`Usted Rompió con éxito con ${tu}`, m, {contextInfo: { mentionedJid: [tu] }})
-ayg.pasangan = ""
-beb.pasangan = ""
-}else {
-conn.reply(m.chat,`no tienes pareja.`,m)}}}
 
 handler.command = /^(romper)$/i
 handler.group = true
