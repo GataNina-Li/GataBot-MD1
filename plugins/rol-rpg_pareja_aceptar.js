67325f834486cb6d14c2de939d056a9566ceaf4a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-14 18:53:21 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/rol-rpg_pareja_aceptar.js
+++ plugins/rol-rpg_pareja_aceptar.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch' 
 import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, text, participants, groupMetadata }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
