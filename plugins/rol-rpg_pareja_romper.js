d938167067d051d990602fea8c01ced00f255a87
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-14 15:58:36 -0500

    Update rol-rpg_pareja_romper.js

--- plugins/rol-rpg_pareja_romper.js
+++ plugins/rol-rpg_pareja_romper.js
@@ -1,20 +1,20 @@
 
 let handler = async (m, { conn }) => {
-  var ayg = global.db.data.users[m.sender]
+  var ayg = global.db.data.users[m.sender] //@${global.db.data.users[m.sender].pasangan.split('@')[0]}
   var beb = global.db.data.users[global.db.data.users[m.sender].pasangan]
 
   if(ayg.pasangan == ""){
     return conn.reply(m.chat,`No tienes pareja.`,m)
   }
   if (typeof beb == "undefined"){
-    conn.reply(m.chat,`Rompió con éxito con @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: {
+    conn.reply(m.chat,`Rompió con éxito con ${global.db.data.users[m.sender].pasanganconn.getName(m.sender)}`,m,{contextInfo: {
       mentionedJid: [global.db.data.users[m.sender].pasangan]
     }})
     ayg.pasangan = ""
   }
 
   if (m.sender == beb.pasangan){
-    conn.reply(m.chat,`Usted Rompió con éxito con @${global.db.data.users[m.sender].pasangan.split('@')[0]}`,m,{contextInfo: {
+    conn.reply(m.chat,`Usted Rompió con éxito con ${global.db.data.users[m.sender].pasanganconn.getName(m.sender)}`,m,{contextInfo: {
       mentionedJid: [global.db.data.users[m.sender].pasangan]
     }})
     ayg.pasangan = ""
@@ -25,5 +25,5 @@ let handler = async (m, { conn }) => {
 }
 handler.command = /^(romper)$/i
 handler.group = true
-handler.fail = null
+
 export default handler
