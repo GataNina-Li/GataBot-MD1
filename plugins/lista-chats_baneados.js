9579bc9bc8bf2f36ea88b4ecfc33bc7de1fc4537
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 03:28:27 -0500

    Update lista-chats_baneados.js

--- plugins/lista-chats_baneados.js
+++ plugins/lista-chats_baneados.js
@@ -5,13 +5,10 @@ let caption = `
 *╭•·––| 💬 𝘾𝙃𝘼𝙏𝙎 𝘽𝘼𝙉𝙀𝘼𝘿𝙊𝙎 : 𝘽𝘼𝙉𝙉𝙀𝘿 |––·•*
 │ *Total: ${chats.length} Chats* ${chats ? '\n' + chats.map(([jid], i) => `
 │ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Chats Baneados' : conn.getName(jid)}
-│ ${isOwner ? '@' + jid.split`@`[0] : jid + `\n│ - - - - - - - - -`}`.trim()).join('\n') : ''}
-*╰•·–––––––––––––––––––·•*`
+│ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
+*╰•·–––––––––––––––––––·•*`.trim()
 await conn.sendButton(m.chat, caption, wm, null, [ 
 ['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, { mentions: await conn.parseMention(caption) })}
 }
-handler.help = ['banlist']
-handler.tags = ['info']
 handler.command = /^chat(s)?baneado(s)?|list(a)?chat(s)?|list(a)?ban(chat(s)?)?$/i
-handler.owner = false
 export default handler
