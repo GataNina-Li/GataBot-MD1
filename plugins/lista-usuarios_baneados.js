96ae893f3842dbfae439512fc2ab922046c55503
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 13:17:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/lista-usuarios_baneados.js
+++ plugins/lista-usuarios_baneados.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, isOwner }) => {
 let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
 let caption = `
 *╭•·–| 👥 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝘽𝘼𝙉𝙀𝘼𝘿𝙊𝙎 : 𝘽𝘼𝙉𝙉𝙀𝘿 |–·•*
-│
 │ *Total : ${users.length} Usuarios* ${users ? '\n' + users.map(([jid], i) => `
+│
 │ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Usuarios Baneados' : conn.getName(jid)}
 │ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
 *╰•·–––––––––––––––––––·•*`.trim()
