0d4e97d9fe23f368f8b94a0e0e3a3b0cf71fa645
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 17:27:50 -0500

    Update lista-chats_baneados.js

--- plugins/lista-chats_baneados.js
+++ plugins/lista-chats_baneados.js
@@ -4,8 +4,8 @@ let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned
 let caption = `
 *╭•·––| 💬 𝘾𝙃𝘼𝙏𝙎 𝘽𝘼𝙉𝙀𝘼𝘿𝙊𝙎 : 𝘽𝘼𝙉𝙉𝙀𝘿 |––·•*
 │ *Total: ${chats.length} Chats* ${chats ? '\n' + chats.map(([jid, chat], i) => `
-│ *${i + 1}.* ${await conn.getName(jid)}
-│ ${jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
+│ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+│ ${jid}@g.us\n│ - - - - - - - - -`.trim()).join('\n') : ''}
 *╰•·–––––––––––––––––––·•*`
 await conn.sendButton(m.chat, caption, wm, null, [ 
 ['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, { mentions: await conn.parseMention(caption) })}
