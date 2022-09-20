d01b8ca5b7c3a5430d502bbf274436798cef10ec
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 16:55:11 -0500

    Update lista-pareja.js

--- plugins/lista-pareja.js
+++ plugins/lista-pareja.js
@@ -6,9 +6,10 @@ let pareja = global.db.data.users[m.sender].pasangan
 let relacion = Object.entries(global.db.data.users).filter(user => user[1].pasangan)
 let caption = `💝 𝗟𝗜𝗦𝗧𝗔 𝗗𝗘 𝗣𝗔𝗥𝗘𝗝𝗔𝗦 : 𝙍𝙀𝙇𝘼𝙏𝙄𝙊𝙉𝙎𝙃𝙄𝙋
 *╭•·–––––––––––––––––––·•*
-│ *Total : ${relacion.length} Usuarios* ${relacion ? '\n' + relacion.map(([jid], i) => `
+│ *Total : ${relacion.length} Usuarios* ${relacion ? '\n' + relacion.map(([jid, parejas], i) => `
+│
 │ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Pareja' : conn.getName(jid)}
-│ ${isOwner ? '@' + jid.split`@`[0] + ` 💞 ${conn.getName(who)}` : jid + ` 💞 ${conn.getName(who)}`}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
+│ ${isOwner ? '@' + jid.split`@`[0] + ` 💞 ${conn.getName(parejas)}` : jid + ` 💞 ${conn.getName(parejas)}`}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
 *╰•·–––––––––––––––––––·•*`
 await conn.sendButton(m.chat, caption, `💟 𝗠𝗜 𝗣𝗔𝗥𝗘𝗝𝗔 ⇢ ${pareja ? `*${user} 💞 ${conn.getName(pareja)}*` : `❌ *No tiene Pareja*`}\n${wm}`, null, [ 
 [`${pareja ? '✦ 𝘿𝙄𝙎𝙁𝙍𝙐𝙏𝘼𝙍 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦': '✦ 𝘾𝙊𝙈𝙋𝙍𝘼𝙍 𝙋𝘼𝙎𝙀 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦'}`, `${pareja ? '.allmenu': '.pase premium'}`]], m, { mentions: await conn.parseMention(caption) })
