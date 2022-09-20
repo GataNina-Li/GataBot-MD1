5bbb8f7607eb47c8d92d99a390ebfe902a52df0a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-19 19:49:11 -0500

    Update rol-rpg_pareja_sinceridad.js

--- plugins/rol-rpg_pareja_sinceridad.js
+++ plugins/rol-rpg_pareja_sinceridad.js
@@ -1,14 +1,16 @@
-let handler = async (m, { conn }) => {
-  if (global.db.data.users[m.sender].pasangan == "") return conn.reply(m.chat, `No vas a dispararle a nadie!`, m)
+let handler = async (m, { conn }) => { // @${global.db.data.users[m.sender].pasangan.split('@')[0]}
+let persona = global.db.data.users[m.sender].pasangan
+
+  if (global.db.data.users[m.sender].pasangan == "") return conn.reply(m.chat, `HAS RECHAZADO SER SU PAREJA`, m)
   
   if (global.db.data.users[global.db.data.users[m.sender].pasangan].pasangan == m.sender) return conn.reply(m.chat, `has estado saliendo @${global.db.data.users[m.sender].pasangan.split('@')[0]}`, m, {contextInfo: {
     mentionedJid: [global.db.data.users[m.sender].pasangan]
   }})
-  conn.reply(m.chat, `PARECE QUE @${global.db.data.users[m.sender].pasangan.split('@')[0]} NO HA ACEPTADO NI RECHAZADO TU PROPUESTA DE ESTAR AMBOS EN UNA RELACION`, m, {contextInfo: {
+  conn.reply(m.chat, `PARECE QUE ${await conn.getName(persona)} NO HA ACEPTADO NI RECHAZADO TU PROPUESTA DE ESTAR AMBOS EN UNA RELACION`, m, {contextInfo: {
     mentionedJid: [global.db.data.users[m.sender].pasangan]
   }})
   global.db.data.users[m.sender].pasangan = ""
 }
-handler.command = /^(cancelar|sinceridad|rechazar)$/i
+handler.command = /^(cancelar|sinceridad|rechazar|mipareja)$/i
 handler.group = true
 export default handler
