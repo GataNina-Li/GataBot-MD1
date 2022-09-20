c0bdb579a0dc3a12928375326e53eaf2900170c6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 13:09:22 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/lista-usuarios_baneados.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, isOwner }) => {
+let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
+let caption = `
+*╭•·–| 👥 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝘽𝘼𝙉𝙀𝘼𝘿𝙊𝙎 : 𝘽𝘼𝙉𝙉𝙀𝘿 |–·•*
+│ *Total : ${users.length} Usuarios* ${users ? '\n' + users.map(([jid], i) => `
+│ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Usuarios Baneados' : conn.getName(jid)}
+│ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : ''}
+*╰•·–––––––––––––––––––·•*`.trim()
+
+await conn.sendButton(m.chat, caption, `*Estos usuarios no puedes Usar a GataBot*\n\n` + wm, null, [ 
+['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, { mentions: await conn.parseMention(caption) })}
+}
+
+handler.command = /^listabanuser|banuserlista|listausuarios|listbanuser|listabaneados|listban|banlist$/i
+
+export default handler
