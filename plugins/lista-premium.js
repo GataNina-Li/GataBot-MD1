43ba4d8fc27e8a52d6ede0c7b38698246427882d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 01:59:41 -0500

    Update lista-premium.js

--- plugins/lista-premium.js
+++ plugins/lista-premium.js
@@ -1,7 +1,12 @@
-let handler = async (m, { conn }) => {
-let prem = global.db.data.users[m.sender].premium.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-  let caption = `🎟️ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 : 𝙑𝙄𝙋 𝙐𝙎𝙀𝙍𝙎
-` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
+let handler = async (m, { conn, isOwner }) => {
+//let prem = global.db.data.users[m.sender].premium.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
+let prem = Object.entries(global.db.data.users).filter(user => user[1].premium)
+let caption = `🎟️ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 : 𝙑𝙄𝙋 𝙐𝙎𝙀𝙍𝙎
+
+│ Total : ${prem.length} Pengguna${prem ? '\n' + prem.map(([jid], i) => `
+│ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+│ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : ''}`
+//` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
 conn.reply(m.chat, caption, m, { mentions: conn.parseMention(caption) })
 }
 handler.help = ['premlist']
