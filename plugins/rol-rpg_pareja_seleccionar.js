15d08116cddca623d9122ba7cc2c002c83a99663
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-19 22:22:19 -0500

    Update rol-rpg_pareja_seleccionar.js

--- plugins/rol-rpg_pareja_seleccionar.js
+++ plugins/rol-rpg_pareja_seleccionar.js
@@ -79,10 +79,10 @@ global.db.data.users[m.sender].pasangan = user
 await conn.sendButton(m.chat, `Felicitaciones, oficialmente están saliendo. @${user.split('@')[0]}\n\nQue dure para siempre y siempre sea feliz 🥳🥳🥳`, m , { contextInfo: { mentionedJid: [user]}})
 }else {
 	
-global.db.data.users[m.sender].pasangan = user
+global.db.data.users[m.sender].pasangan = m.sender
 await conn.sendButton(m.chat, `${await ktnmbk.getRandom()}\n\n*${conn.getName(m.sender)}* acabas de invitar a *${conn.getName(who)}* Fechado\n\nPor favor espere la respuesta!\n\nEscriba *${usedPrefix}terima @user* untuk menerima\n*${usedPrefix}tolak @user untuk menolak*`, wm, null, [
-['Aceptar', `/aceptar ${conn.getName(m.sender)}`],
-['Rechazar', `/rechazar ${conn.getName(m.sender)}`]], m, { contextInfo: { mentionedJid: [ who, m.sender ]}})
+['Aceptar', `/aceptar @${conn.getName(m.sender)}`],
+['Rechazar', `/rechazar @${conn.getName(m.sender)}`]], m, { contextInfo: { mentionedJid: [ who, m.sender ]}})
 }}}
 
 handler.command = /^(buscarpareja|pareja|elegirpareja)$/i
