b24460df100fbc68f3dea968f06224ed519f96b8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 03:00:08 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/lista-advertidos.js
+++ plugins/lista-advertidos.js
@@ -6,7 +6,7 @@ let caption = `⚠️ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝘼𝘿𝙑𝙀𝙍𝙏�
 *╭•·–––––––––––––––––––·•*
 │ *Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid], i) => `
 │
-│ *${i + 1}.* ${conn.getName(jid)  == undefined ? 'Sin Usuarios' : conn.getName(jid) + ` *(${jid.warn}/4)*`}
+│ *${i + 1}.* ${conn.getName(jid)  == undefined ? 'Sin Usuarios' : conn.getName(jid) + ` *(${user => user[1].warn}/4)*`}
 │ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
 *╰•·–––––––––––––––––––·•*`
 await conn.sendButton(m.chat, caption, `⚠️ 𝗔𝗗𝗩𝗘𝗥𝗧𝗘𝗡𝗖𝗜𝗔 ⇢ ${adv ? `*${warn}/4*` : '*0/4*'}\n${wm}`, null, [ 
