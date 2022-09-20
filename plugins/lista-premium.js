04c2a443acdb52306e7a7f89f8fd274199caae0a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-20 01:51:13 -0500

    Update lista-premium.js

--- plugins/lista-premium.js
+++ plugins/lista-premium.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let prem = global.premium.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
+let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
   let caption = `🎟️ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 : 𝙑𝙄𝙋 𝙐𝙎𝙀𝙍𝙎
 ` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
 conn.reply(m.chat, caption, m, { mentions: conn.parseMention(caption) })
