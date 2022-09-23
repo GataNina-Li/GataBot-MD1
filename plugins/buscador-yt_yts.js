91e16bc9a6c17281b898af3b69494d87859f0748
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-23 18:02:05 -0500

    Update buscador-yt_yts.js

--- plugins/buscador-yt_yts.js
+++ plugins/buscador-yt_yts.js
@@ -36,29 +36,50 @@ handler.limit = 1
 handler.level = 4
 export default handler*/
 
+/* Created by https://github.com/BrunoSobrino */
+       /* Diseño by Yameko-Bot V1*/
 
-
-
-
-import { youtubeSearch } from '@bochilteam/scraper'
-let handler = async(m, { conn, usedPrefix, text, args, command }) => {
+import yts from "yt-search"
+let handler = async (m, { text, conn, args, command, usedPrefix }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-
 if (!text) return conn.reply(m.chat, `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
-let result = await youtubeSearch(text)
-let ytres = result.video
-let listSections = []
-Object.values(ytres).map((v, index) => {
-listSections.push([`${index}┃ ${v.title}`, [
-['𝗔 𝗨 𝗗 𝗜 𝗢', `${usedPrefix}ytmp3 ${v.url} yes`, `𝘿𝙐𝙍𝘼𝘾𝙄𝙊𝙉 ${v.durationH}\n𝙋𝙐𝘽𝙇𝙄𝘾𝘼𝘿𝙊 ${v.publishedTime}\n`],
-['𝗩 𝗜 𝗗 𝗘 𝗢', `${usedPrefix}ytmp4 ${v.url} yes`, `𝘿𝙐𝙍𝘼𝘾𝙄𝙊𝙉 ${v.durationH}\n𝙋𝙐𝘽𝙇𝙄𝘾𝘼𝘿𝙊 ${v.publishedTime}\n`]
-]])
-})
-return conn.sendList(m.chat, `Busqueda sobre ${text}`, `\n🎧 • *Lista de resultados encontrados* :\n ► *${text}*`, wm, `Click Aquí`, listSections, m)
-}
+try {
+let search = await yts(args.join(" "))
+let listAudio = []
+let listVideo = []
+let listAudioDoc = []
+let listVideoDoc = []
+let teskd = `𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖 𝙙𝙚 *${args.join(" ")}*`
+const sections = [{
+title: comienzo + ' 𝗔 𝗨 𝗗 𝗜 𝗢 ' + fin,
+rows: listAudio },
+{              
+title: comienzo + ' 𝗩 𝗜 𝗗 𝗘 𝗢 ' + fin,
+rows: listVideo },
+{              
+title: comienzo + ' 𝗔 𝗨 𝗗 𝗜 𝗢   𝗗 𝗢 𝗖 ' + fin,
+rows: listAudioDoc },
+{              
+title: comienzo + ' 𝗩 𝗜 𝗗 𝗘 𝗢   𝗗 𝗢 𝗖 ' + fin,
+rows: listVideoDoc }]
+const listMessage = {
+text: teskd,
+footer: '𝙀𝙡𝙞𝙟𝙖 𝙨𝙪 𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖 𝙥𝙖𝙧𝙖 𝙥𝙤𝙙𝙚𝙧 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧\n' + wm,
+title: `${htki} *𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎* ${htka}`,
+buttonText: "🔎 𝗕 𝗨 𝗦 𝗖 𝗔 𝗥",
+sections}
+for (let i of search.all) {
+listAudio.push({title: i.title, description: `Autor: ${i.author.name} / ${i.timestamp}`, rowId: `${usedPrefix}ytmp3 ${i.url}`})
+listVideo.push({title: i.title, description: `Autor: ${i.author.name} / ${i.timestamp}`, rowId: `${usedPrefix}ytmp4 ${i.url}`})
+listAudioDoc.push({title: i.title, description: `Autor: ${i.author.name} / ${i.timestamp}`, rowId: `${usedPrefix}ytmp3doc ${i.url}`})
+listVideoDoc.push({title: i.title, description: `Autor: ${i.author.name} / ${i.timestamp}`, rowId: `${usedPrefix}ytmp4doc ${i.url}`})}
+conn.sendMessage(m.chat, listMessage, { quoted: fkontak })
+} catch (e) {
+m.reply(`${fg}𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘿𝙀 𝙉𝙐𝙀𝙑𝙊\n𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`)
+}}
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['tools']
-handler.command = /^ytbuscar|yts(earch)?$/i
+handler.command = /^playlist|ytbuscar|yts(earch)?$/i
 handler.exp = 70
 handler.limit = 1
 handler.level = 4
