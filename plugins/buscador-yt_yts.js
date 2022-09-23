eef77422baba9080ef726c8f458553af85158013
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-23 17:33:56 -0500

    Update buscador-yt_yts.js

--- plugins/buscador-yt_yts.js
+++ plugins/buscador-yt_yts.js
@@ -1,4 +1,4 @@
-import { youtubeSearch } from '@bochilteam/scraper'
+/*import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { text }) => {
 if (!text) throw `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`
 const { video, channel } = await youtubeSearch(text)
@@ -27,6 +27,35 @@ await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜�
 ['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)      
+}
+handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
+handler.tags = ['tools']
+handler.command = /^ytbuscar|yts(earch)?$/i
+handler.exp = 70
+handler.limit = 1
+handler.level = 4
+export default handler*/
+
+
+
+
+
+import { youtubeSearch } from '@bochilteam/scraper'
+let handler = async(m, { conn, usedPrefix, text, args, command }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
+if (!text) return conn.reply(m.chat, `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
+let result = await youtubeSearch(text)
+let ytres = result.video
+let listSections = []
+Object.values(ytres).map((v, index) => {
+listSections.push([`${index}┃ ${v.title}`, [
+['𝗔 𝗨 𝗗 𝗜 𝗢', `${usedPrefix}ytmp3 ${v.url} yes`, `𝘿𝙐𝙍𝘼𝘾𝙄𝙊𝙉 ${v.durationH}\n𝙋𝙐𝘽𝙇𝙄𝘾𝘼𝘿𝙊 ${v.publishedTime}\n`],
+['𝗩 𝗜 𝗗 𝗘 𝗢', `${usedPrefix}ytmp4 ${v.url} yes`, `𝘿𝙐𝙍𝘼𝘾𝙄𝙊𝙉 ${v.durationH}\n𝙋𝙐𝘽𝙇𝙄𝘾𝘼𝘿𝙊 ${v.publishedTime}\n`]
+]])
+})
+m.react('🎧')
+return conn.sendList(m.chat, `Busqueda sobre ${text}`, `\n🎧 • *Lista de resultados encontrados* :\n ► *${text}*`, botname, `Click Aquí`, listSections, m)
 }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['tools']
