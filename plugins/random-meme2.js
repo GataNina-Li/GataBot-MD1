8c35d62c2b659db46c2b4f59cd814a42b8a728d8
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-24 22:04:16 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/random-meme2.js
+++ plugins/random-meme2.js
@@ -1,8 +1,7 @@
 import hispamemes from 'hispamemes'
 let handler = async (m, { conn, usedPrefix, command }) => {
 const meme = hispamemes.meme()
-//await conn.sendFile(m.chat, meme, '', '', m)
-await conn.sendButton(m.chat, `_${command}_`.trim(), wm, meme, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
+await conn.sendButton(m.chat, `*_${command}_*`.trim(), wm, meme, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 handler.command = ['meme2', 'memes2'] 
 handler.level = 3
