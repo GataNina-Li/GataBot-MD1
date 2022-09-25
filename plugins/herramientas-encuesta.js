e8597e8356a79df1a422076fd518d9b5c6da3e46
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-09-25 14:40:58 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/herramientas-encuesta.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {	
+if (!args[0]) throw `⚠️️ *_Ingrese un texto para iniciar la escuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `⚠️️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+let a = []
+let b = text.split('|')
+for (let c = 0; c < b.length; c++) { a.push([b[c]]) }
+return conn.sendPoll(m.chat, `📊 *Encuesta para:*\n\n${text}`, a, m)}
+handler.command = ['poll', 'encuesta', 'crearencuesta', 'startpoll', 'encuestas', 'polls'] 
+export default handler
