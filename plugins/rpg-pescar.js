d3c3167131f17da1646a48b5849baf0bea160480
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-04 01:21:22 -0500

    Update rpg-pescar.js

--- plugins/rpg-pescar.js
+++ plugins/rpg-pescar.js
@@ -66,7 +66,7 @@ if (user.stamina < 9) return conn.sendButton( m.chat, `${ag}𝙉𝙊 𝙏𝙄�
 if (user.pancingan > 0 ) {  
 if (user.pancing > 0 ) {
 //if (user.umpan > 0 ) {
-if (new Date - user.lastfishing > 240000) {
+if (new Date - user.lastfishing > 7200000) {
         	        
 let ikan = `${Math.floor(Math.random() * 30)}`.trim()
 let lele = `${Math.floor(Math.random() * 15)}`.trim() 
