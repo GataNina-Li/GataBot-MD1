b32c87ea30f298de1ae3e5663de00a0a18173fa4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-08 13:55:01 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

---
+++ plugins/_anti-spam.js
@@ -0,0 +1,51 @@
+/*export async function all(m) {
+if (!m.message)
+return
+this.spam = this.spam ? this.spam : {}
+let chat = global.db.data.chats[m.chat]
+let user = global.db.data.users[m.sender]
+if (chat.antiSpam) {
+if (global.owner.includes(m.sender)) throw 'Sin limites !'
+if (m.sender in this.spam) {
+this.spam[m.sender].count++
+if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
+if (this.spam[m.sender].count > 5) {
+user.banned = true
+let caption = `👋 Prohibido *@${m.sender.split("@")[0]}* no spam!`
+this.sendButton(m.chat, caption, wm, null, [['Disable Anti Spam', '/disable antispam']], m, { mentions: this.parseMention(caption) })
+}
+this.spam[m.sender].count = 0
+this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
+}}
+else
+this.spam[m.sender] = {
+jid: m.sender,
+count: 0,
+lastspam: 0
+}}}*/
+
+export async function all(m) {
+if (!m.message)
+return
+this.spam = this.spam ? this.spam : {}
+let chat = global.db.data.chats[m.chat]
+let user = global.db.data.users[m.sender]
+if (chat.antiSpam) {
+if (global.owner.includes(m.sender)) throw 'Sin limites!'
+if (m.sender in this.spam) {
+this.spam[m.sender].count++
+if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
+if (this.spam[m.sender].count > 5) {
+user.banned = true
+let caption = `👋 Prohibido *@${m.sender.split("@")[0]}* no spam!`
+this.sendButton(m.chat, caption, wm, null, [['Desactivar Anti Spam', '/off antispam']], m, { mentions: this.parseMention(caption) })
+}
+this.spam[m.sender].count = 0
+this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
+}}
+else
+this.spam[m.sender] = {
+jid: m.sender,
+count: 0,
+lastspam: 0
+}}}
