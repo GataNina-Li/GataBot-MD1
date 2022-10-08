1f9aa4b3aa1812b7d0a3c8d6f5369ec1c4ab2d75
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-08 14:24:21 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_anti-spam.js
+++ plugins/_anti-spam.js
@@ -30,24 +30,24 @@ let handler = m => m
 handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
 if (!m.message)
 return
-conn.spam = conn.spam ? conn.spam : {}
+this.spam = this.spam ? this.spam : {}
 let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 if (chat.antiSpam) {
 if (global.owner.includes(m.sender)) throw 'Sin limites!'
-if (m.sender in conn.spam) {
-conn.spam[m.sender].antispam++
-if (m.messageTimestamp.toNumber() - conn.spam[m.sender].lastspam > 5) {
-if (conn.spam[m.sender].antispam > 5) {
+if (m.sender in this.spam) {
+this.spam[m.sender].antispam++
+if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
+if (this.spam[m.sender].antispam > 5) {
 user.banned = true
 let caption = `👋 Prohibido *@${m.sender.split("@")[0]}* no spam!`
-conn.sendButton(m.chat, caption, wm, null, [['Desactivar Anti Spam', '/off antispam']], m, { mentions: conn.parseMention(caption) })
+this.sendButton(m.chat, caption, wm, null, [['Desactivar Anti Spam', '/off antispam']], m, { mentions: conn.parseMention(caption) })
 }
-conn.spam[m.sender].antispam = 0
-conn.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
+this.spam[m.sender].antispam = 0
+this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
 }}
 else
-conn.spam[m.sender] = {
+this.spam[m.sender] = {
 jid: m.sender,
 antispam: 0,
 lastspam: 0
