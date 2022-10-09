c245ca736dfc402130d528579da03067d4a14010
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-09 17:26:21 -0500

    Create rpg-colecciones.js

---
+++ plugins/rpg-colecciones.js
@@ -0,0 +1,25 @@
+let handler = async (m, { command, usedPrefix, DevMode, args, conn }) => {
+const categorias = {
+estrategia: {
+exp: {
+categoria: 'Categoria 1',
+descripcion: 'Experiencia',
+ataque: '',
+defensa: '',
+utilidad: 'Muy alta',
+abundancia: 'Muy alta'
+},
+stamina: {
+categoria: 'Categoria 2',
+descripcion: 'Energia',
+ataque: '',
+defensa: '',
+utilidad: 'Medianamente alta',
+abundancia: 'Medianamente poca' 
+},
+}}
+m.reply(categorias[0])
+
+handler.command = /^(colección|coleccion|inforpg|set|collection)$/i
+
+export default handler
