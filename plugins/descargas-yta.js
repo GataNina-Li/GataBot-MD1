8ea40291aa2b4dacdd2012c68323db2f1dcda082
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-15 00:55:23 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -26,46 +26,5 @@ await conn.sendFile(m.chat, lolh.result.link, `${n}.mp3`, null, m, false, { mime
 await conn.reply(m.chat, `${fg}𝙀𝙎 𝙋𝙊𝙎𝙄𝘽𝙇𝙀 𝙌𝙐𝙀 𝙀𝙇 𝘼𝙍𝘾𝙃𝙄𝙑𝙊 𝙎𝙀𝘼 𝙈𝙐𝙔 𝙋𝙀𝙎𝘼𝘿𝙊. 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘾𝙊𝙉 𝙊𝙏𝙍𝘼 𝙊𝙋𝘾𝙄𝙊𝙉 𝘿𝙀 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼\n\n𝙄𝙏 𝙄𝙎 𝙋𝙊𝙎𝙎𝙄𝘽𝙇𝙀 𝙏𝙃𝘼𝙏 𝙏𝙃𝙀 𝙁𝙄𝙇𝙀 𝙄𝙎 𝙑𝙀𝙍𝙔 𝙃𝙀𝘼𝙑𝙔. 𝙏𝙍𝙔 𝘼𝙉𝙊𝙏𝙃𝙀𝙍`, m)}
 }}
 handler.command = /^getaud|yt(a|mp3)$/i
-export default handler
-
-/*let limit = 200
-import fetch from 'node-fetch'
-import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
-let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-if (!args || !args[0]) throw `${mg}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙀𝙇 𝘼𝙐𝘿𝙄𝙊\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://youtu.be/85xI8WFMIUY*\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙏𝙃𝙀 𝘼𝙐𝘿𝙄𝙊\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://youtu.be/c5gJRzCi0f0*`
-try {
-let chat = global.db.data.chats[m.chat]
-const isY = /y(es)/gi.test(args[1])
-const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
-const limitedSize = (isPrems || isOwner ? 350 : 200) * 3074
-let audio, source, res, link, lastError, isLimit
-for (let i in _audio) {
-try {
-audio = _audio[i]
-isLimit = limitedSize < audio.fileSize
-if (isLimit) continue
-link = await audio.download()
-if (link) res = await fetch(link)
-isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
-if (isLimit) continue
-if (res) source = await res.arrayBuffer()
-if (source instanceof ArrayBuffer) break
-} catch (e) {
-audio = link = source = null
-lastError = e
-}}
-conn.sendFile(m.chat, source, title + '.mp3', `
-*𝙏𝙞𝙩𝙪𝙡𝙤 : 𝙏𝙞𝙩𝙡𝙚 »* ${title}
-`.trim(), m, null, {
-asDocument: chat.useDocument
-})
-}catch(e){
-m.reply(`${fg}𝙀𝙎 𝙋𝙊𝙎𝙄𝘽𝙇𝙀 𝙌𝙐𝙀 𝙀𝙇 𝘼𝙍𝘾𝙃𝙄𝙑𝙊 𝙎𝙀𝘼 𝙈𝙐𝙔 𝙋𝙀𝙎𝘼𝘿𝙊. 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘾𝙊𝙉 𝙊𝙏𝙍𝙊\n\n𝙄𝙏 𝙄𝙎 𝙋𝙊𝙎𝙎𝙄𝘽𝙇𝙀 𝙏𝙃𝘼𝙏 𝙏𝙃𝙀 𝙁𝙄𝙇𝙀 𝙄𝙎 𝙑𝙀𝙍𝙔 𝙃𝙀𝘼𝙑𝙔. 𝙏𝙍𝙔 𝘼𝙉𝙊𝙏𝙃𝙀𝙍`)
-console.log(e)
-}}
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
-handler.tags = ['downloader']
-handler.command = /^yt(a|mp3)$/i
 handler.limit = 2
-handler.exp = 40
-export default handler*/
+export default handler
