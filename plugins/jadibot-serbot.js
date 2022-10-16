a98706ceee98e995293c1dafe781ca106d3ee845
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-16 15:46:02 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -1,4 +1,4 @@
-const _0x38d792 = _0x1ff6;
+/*const _0x38d792 = _0x1ff6;
 (function (_0x527dba, _0x3ce37e) {
     const _0x59ee81 = _0x1ff6,
         _0x56055e = _0x527dba();
@@ -184,4 +184,213 @@ function _0x1ff6(_0x34048e, _0x357cbf) {
     }, _0x1ff6(_0x34048e, _0x357cbf);
 }
 handler['help'] = ['jadibot'], handler[_0x38d792(0x220)] = [_0x38d792(0x221)], handler[_0x38d792(0x1f0)] = /^(jadibot|serbot|getcode)/i, handler[_0x38d792(0x1ef)] = ![];
+export default handler;*/
+
+
+
+const _0x1c31f1 = _0x3f83;
+(function (_0x4685ed, _0x4916e9) {
+    const _0x588b7f = _0x3f83,
+        _0x47bd61 = _0x4685ed();
+    while (!![]) {
+        try {
+            const _0x319602 = parseInt(_0x588b7f(0x11a)) / 0x1 * (-parseInt(_0x588b7f(0x126)) / 0x2) + parseInt(_0x588b7f(0x10e)) / 0x3 * (parseInt(_0x588b7f(0x149)) / 0x4) + parseInt(_0x588b7f(0xfd)) / 0x5 * (parseInt(_0x588b7f(0x101)) / 0x6) + parseInt(_0x588b7f(0x123)) / 0x7 + parseInt(_0x588b7f(0x11c)) / 0x8 + parseInt(_0x588b7f(0x124)) / 0x9 + -parseInt(_0x588b7f(0x140)) / 0xa;
+            if (_0x319602 === _0x4916e9) break;
+            else _0x47bd61['push'](_0x47bd61['shift']());
+        } catch (_0x83cc48) {
+            _0x47bd61['push'](_0x47bd61['shift']());
+        }
+    }
+}(_0x5578, 0x28b98));
+const {
+    MessageType,
+    useMultiFileAuthState,
+    DisconnectReason,
+    Browsers
+} = await import('@adiwajshing/baileys');
+import _0x23236c from 'qrcode';
+
+function _0x3f83(_0x39c4d6, _0x5c04a9) {
+    const _0x557893 = _0x5578();
+    return _0x3f83 = function (_0x3f83e2, _0x2d7b9d) {
+        _0x3f83e2 = _0x3f83e2 - 0xfa;
+        let _0x42b77f = _0x557893[_0x3f83e2];
+        return _0x42b77f;
+    }, _0x3f83(_0x39c4d6, _0x5c04a9);
+}
+import _0x1a0a86 from 'fs';
+import _0x737f31 from 'pino';
+import * as _0x35527c from 'ws';
+const {
+    child,
+    spawn,
+    exec
+} = await import(_0x1c31f1(0x14d)), {
+    CONNECTING
+} = _0x35527c;
+import {
+    makeWASocket,
+    protoType,
+    serialize
+} from '../lib/simple.js';
+let check1 = _0x1c31f1(0x136),
+    check2 = _0x1c31f1(0x113),
+    check3 = _0x1c31f1(0x146),
+    check4 = _0x1c31f1(0x10a),
+    check5 = _0x1c31f1(0x121),
+    check6 = _0x1c31f1(0x12b),
+    check8 = _0x1c31f1(0x133),
+    crm1 = 'Y2QgcGx1Z2lucy',
+    crm2 = _0x1c31f1(0x11f),
+    crm3 = _0x1c31f1(0x108),
+    crm4 = 'IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz',
+    drm1 = _0x1c31f1(0x13f),
+    drm2 = 'IHBvciBAQWlkZW5fTm90TG9naWM',
+    rtx = _0x1c31f1(0x145);
+if (global[_0x1c31f1(0x128)] instanceof Array) console['log']();
+else global[_0x1c31f1(0x128)] = [];
+
+function _0x5578() {
+    const _0x169182 = ['groupsUpdate', 'ZThkMmNkOGVlMDFmZD', 'command', '*𝘼𝙃𝙊𝙍𝘼 𝙀𝙎𝙏𝙀 𝙀𝙎 𝙀𝙇 𝙉𝙐𝙀𝙑𝙊 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊!!*\n\n*𝙉𝙊𝙒 𝙏𝙃𝙄𝙎 𝙄𝙎 𝙏𝙃𝙀 𝙉𝙀𝙒 𝙇𝙄𝙉𝙆 𝙊𝙁 𝙏𝙃𝙀 𝙂𝙍𝙊𝙐𝙋!!*\n\n*@revoke*', '5.0', 'jadibot', 'Safari', 'indexOf', '27878COLXHn', 'messages.upsert', '1202344GvfkKd', 'loggedOut', 'onCall', 'A7IG1kNXN1b', 'now', 'NzZjM2ZmMzU2MTEyMzM3OTczOWU5ZmFmMDZjYzUzO', 'data', '902104ujwdaH', '128637LuhJag', '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz', '2wzMWab', 'creds.update', 'conns', 'spromote', '/creds.json', 'DcgIF9hdXRvcmVzcG9uZGVyLmpzCjU5Yzc0ZjFjNmEz', 'statusCode', 'output', 'conn', 'help', 'off', 'participantsUpdate', 'readyState', 'NjNmYmJjYzA1YmFiY2MzZGU4MGRlICBpbmZvLWJvdC5qcwo', 'message.delete', 'log', 'NjBhZGVmZWI4N2M2', '*𝙀𝙇 𝙉𝙐𝙀𝙑𝙊 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝙀𝙎:*\n\n*𝙏𝙃𝙀 𝙉𝙀𝙒 𝙉𝘼𝙈𝙀 𝙊𝙁 𝙏𝙃𝙀 𝙂𝙍𝙊𝙐𝙋 𝙄𝙎:*\n@subject', 'chat', 'credsUpdate', 'sRevoke', 'splice', 'utf-8', 'GataBot-MD - Sub Bot', 'sdemote', 'CkphZGlib3QsIEhlY2hv', '5428380NRrylr', 'push', '*╭┈⊰* @subject *⊰┈ ✦*\n*┃✨ BIENVENIDO(A)!!*\n┃💖 @user\n┃📄 *LEA LA DESCRIPCIÓN DEL GRUPO*\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ✦*\n\n@desc', 'sDesc', 'length', '‿︵ʚɞ『 🐈 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈 』ʚɞ‿︵\n\n🟢 𝗙𝗨𝗡𝗖𝗜𝗢𝗡 𝗦𝗘𝗥 𝗦𝗨𝗕 𝗕𝗢𝗧\n🟢 𝗕𝗘 𝗦𝗨𝗕 𝗕𝗢𝗧 𝗙𝗨𝗡𝗖𝗧𝗜𝗢𝗡 - 𝗘𝗡𝗔𝗕𝗟𝗘𝗗\n\n*➡️ Con otro celular o en la PC escanea este QR para convertirte en un sub bot*\n\n*1️⃣ Haga clic en los tres puntos en la esquina superior derecha*\n\n*2️⃣ Toca WhatsApp Web*\n\n*3️⃣ Escanee este codigo QR para iniciar sesión*\n\n📡 *¡Este código QR expira en 30 segundos!*', 'UzYTI1MTQgIGluZ', 'connectionClosed', 'payload', '12mMzipQ', 'parse', 'existsSync', '*@user 𝘼𝙃𝙊𝙍𝘼 𝙀𝙎 𝘼𝘿𝙈𝙄𝙉 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊!!*\n\n*𝙉𝙊𝙒 𝙃𝙀 𝙄𝙎 𝘼𝘿𝙈𝙄𝙉 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋!!*', 'child_process', 'bind', 'La conexión se cerró, Tendras que conectarte manualmente...', 'from', 'base64', 'error', 'removeAllListeners', 'sendMessage', 'toString', 'welcome', 'close', 'reply', 'user', 'jid', '✅ *CONEXIÓN EXITOSA!!*\n\n✅ *SUCCESSFUL CONNECTION!!*', '𝙀𝙎𝙏𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝙎𝙊𝙇𝙊 𝙁𝙐𝙉𝘾𝙄𝙊𝙉𝘼 𝙀𝙉 𝙀𝙇 𝙉𝙐𝙈𝙀𝙍𝙊 𝙋𝙍𝙄𝙉𝘾𝙄𝙋𝘼𝙇\nWa.me/', 'tags', '*@user 𝘿𝙀𝙅𝘼 𝘿𝙀 𝙎𝙀𝙍 𝘼𝘿𝙈𝙄𝙉 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊!!*\n\n*𝙎𝙏𝙊𝙋 𝘽𝙀𝙄𝙉𝙂 𝘼𝘿𝙈𝙄𝙉 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋!!*', 'handler', 'sSubject', './GataJadiBot/', 'isInit', '380015LoANZM', 'bye', 'call', 'catch', '18LSkJpE', '✅ *CONEXIÓN EXITOSA!!*\nEsta cuenta no se hace responsable del Uso de Sub Bot\n*Por favor leer las Normas de nuestro Servicio*\n\nEn breve se le enviará su ID que servirá para reconectar. *(Guarde su ID)*\n\n✅ *SUCCESSFUL CONNECTION!!*\nThis account is not responsible for the use of Sub Bot\n*Please read the Rules of our Service*\n\nYour ID will be sent shortly, which will be used to reconnect. *(Save your ID)*', 'stringify', 'keys', 'mkdirSync', 'Error! Fallo al comprobar la integridad del bot', 'callUpdate', 'SBpbmZvLWRvbmFyLmpz', 'writeFileSync', 'm8tZG9uYXIuanMK', 'connection.update', 'deleteUpdate', 'onDelete', '216057ENEaRD', 'La conexión se cerró, se intentara reconectar automáticamente...', '../handler.js', '*Estamos en Mantenimiento... y/o Esta función no es valida para este Bot*'];
+    _0x5578 = function () {
+        return _0x169182;
+    };
+    return _0x5578();
+}
+let handler = async (_0x51351b, {
+    conn: _0x1afa8e,
+    args: _0x4e7082,
+    usedPrefix: _0x211c64,
+    command: _0x44f265,
+    isOwner: _0xd914a
+}) => {
+    const _0x431d7b = _0x1c31f1;
+    let _0xdee682 = _0x1afa8e;
+    if (_0x1afa8e[_0x431d7b(0x159)][_0x431d7b(0x15a)] !== global[_0x431d7b(0x12e)][_0x431d7b(0x159)]['jid']) return _0xdee682['reply'](_0x51351b[_0x431d7b(0x138)], _0x431d7b(0x15c) + global[_0x431d7b(0x12e)][_0x431d7b(0x159)]['jid']['split'] `@` [0x0], _0x51351b);
+    const _0x1b7812 = Buffer['from'](crm1 + crm2 + crm3 + crm4, _0x431d7b(0x151));
+    exec(_0x1b7812[_0x431d7b(0x155)](_0x431d7b(0x13c)), async (_0x355f4a, _0x48f961, _0x885437) => {
+        const _0x3ff8fd = _0x431d7b;
+        if (_0x355f4a) return _0x51351b['reply'](_0x3ff8fd(0x106));
+        const _0x49d89c = Buffer[_0x3ff8fd(0x150)](check1 + check2 + check3 + check4 + check5 + check6 + check8 + '=', _0x3ff8fd(0x151)),
+            _0x361a31 = Buffer[_0x3ff8fd(0x150)](drm1 + drm2, _0x3ff8fd(0x151));
+        if (_0x48f961 !== _0x49d89c[_0x3ff8fd(0x155)](_0x3ff8fd(0x13c))) return _0x51351b[_0x3ff8fd(0x158)](_0x3ff8fd(0x111));
+        async function _0x4ad479() {
+            const _0x9e5cef = _0x3ff8fd;
+            
+            function _0x5935bc(_0x5657ad) {
+                const _0x852e5 = _0x3f83;
+                var _0x20404a = _0x852e5(0x125)['split']('');
+                !_0x5657ad && (_0x5657ad = Math['floor'](Math['random']() * _0x20404a[_0x852e5(0x144)]));
+                var _0x4d165d = '';
+                for (var _0x21b71e = 0x0; _0x21b71e < _0x5657ad; _0x21b71e++) {
+                    _0x4d165d += _0x20404a[Math['floor'](Math['random']() * _0x20404a[_0x852e5(0x144)])];
+                }
+                return _0x4d165d;
+            }
+            let _0x30847f = _0x5935bc(0xa);
+            !_0x1a0a86[_0x9e5cef(0x14b)]('./GataJadiBot/' + _0x30847f) && _0x1a0a86[_0x9e5cef(0x105)](_0x9e5cef(0xfb) + _0x30847f, {
+                'recursive': !![]
+            });
+            _0x4e7082[0x0] ? _0x1a0a86[_0x9e5cef(0x109)](_0x9e5cef(0xfb) + _0x30847f + _0x9e5cef(0x12a), JSON[_0x9e5cef(0x103)](JSON[_0x9e5cef(0x14a)](Buffer[_0x9e5cef(0x150)](_0x4e7082[0x0], _0x9e5cef(0x151))[_0x9e5cef(0x155)](_0x9e5cef(0x13c))), null, '\x09')) : '';
+            const {
+                state: _0x45b7ab,
+                saveState: _0x3698ac,
+                saveCreds: _0x99284
+            } = await useMultiFileAuthState('./GataJadiBot/' + _0x30847f), _0x5122de = {
+                'printQRInTerminal': !![],
+                'auth': _0x45b7ab,
+                'logger': _0x737f31({
+                    'level': 'silent'
+                }),
+                'browser': [_0x9e5cef(0x13d), _0x9e5cef(0x118), _0x9e5cef(0x116)]
+            };
+            let _0x715cb7 = makeWASocket(_0x5122de);
+            _0x715cb7[_0x9e5cef(0xfc)] = ![];
+            let _0x7df1d0 = !![];
+            async function _0x16bcb3(_0x34203b) {
+                const _0x30e21a = _0x9e5cef,
+                    {
+                        connection: _0x58e80f,
+                        lastDisconnect: _0x77d293,
+                        isNewLogin: _0x19d7d0,
+                        qr: _0x43983f
+                    } = _0x34203b;
+                if (_0x19d7d0) _0x715cb7[_0x30e21a(0xfc)] = !![];
+                if (_0x43983f) _0xdee682[_0x30e21a(0x154)](_0x51351b[_0x30e21a(0x138)], {
+                    'image': await _0x23236c['toBuffer'](_0x43983f, {
+                        'scale': 0x8
+                    }),
+                    'caption': rtx + _0x361a31['toString']('utf-8')
+                }, {
+                    'quoted': _0x51351b
+                });
+                const _0x5bcffe = _0x77d293?.[_0x30e21a(0x152)]?.[_0x30e21a(0x12d)]?.[_0x30e21a(0x12c)] || _0x77d293?.['error']?.[_0x30e21a(0x12d)]?.[_0x30e21a(0x148)]?.[_0x30e21a(0x12c)];
+                console[_0x30e21a(0x135)](_0x5bcffe);
+                if (_0x5bcffe && _0x5bcffe !== DisconnectReason[_0x30e21a(0x11d)] && _0x715cb7?.['ws'][_0x30e21a(0x132)] !== CONNECTING) {
+                    let _0x129f44 = global[_0x30e21a(0x128)][_0x30e21a(0x119)](_0x715cb7);
+                    if (_0x129f44 < 0x0) return console['log'](await _0x40cf95(!![])['catch'](console['error']));
+                    delete global['conns'][_0x129f44], global[_0x30e21a(0x128)][_0x30e21a(0x13b)](_0x129f44, 0x1), _0x5bcffe !== DisconnectReason[_0x30e21a(0x147)] ? (_0xdee682[_0x30e21a(0x154)](_0x51351b[_0x30e21a(0x138)], {
+                        'text': _0x30e21a(0x10f)
+                    }, {
+                        'quoted': _0x51351b
+                    }), console[_0x30e21a(0x135)](await _0x40cf95(!![])[_0x30e21a(0x100)](console[_0x30e21a(0x152)]))) : _0xdee682['sendMessage'](_0x51351b[_0x30e21a(0x138)], {
+                        'text': _0x30e21a(0x14f)
+                    }, {
+                        'quoted': _0x51351b
+                    });
+                }
+                if (global['db'][_0x30e21a(0x122)] == null) loadDatabase();
+                if (_0x58e80f == 'open') {
+                    _0x715cb7[_0x30e21a(0xfc)] = !![], global['conns'][_0x30e21a(0x141)](_0x715cb7), await _0xdee682[_0x30e21a(0x154)](_0x51351b[_0x30e21a(0x138)], {
+                        'text': _0x4e7082[0x0] ? _0x30e21a(0x15b) : _0x30e21a(0x102)
+                    }, {
+                        'quoted': _0x51351b
+                    }), await sleep(0x1388);
+                    if (!_0x4e7082[0x0]) _0xdee682[_0x30e21a(0x154)](_0x51351b['chat'], {
+                        'text': _0x211c64 + _0x44f265 + ' ' + Buffer['from'](_0x1a0a86['readFileSync'](_0x30e21a(0xfb) + _0x30847f + _0x30e21a(0x12a)), _0x30e21a(0x13c))['toString']('base64')
+                    }, {
+                        'quoted': _0x51351b
+                    });
+                }
+            }
+            setInterval(async () => {
+                const _0x390c7c = _0x9e5cef;
+                if (!_0x715cb7[_0x390c7c(0x159)]) {
+                    try {
+                        _0x715cb7['ws'][_0x390c7c(0x157)]();
+                    } catch {}
+                    _0x715cb7['ev'][_0x390c7c(0x153)]();
+                    let _0x14350a = global[_0x390c7c(0x128)][_0x390c7c(0x119)](_0x715cb7);
+                    if (_0x14350a < 0x0) return;
+                    delete global[_0x390c7c(0x128)][_0x14350a], global[_0x390c7c(0x128)][_0x390c7c(0x13b)](_0x14350a, 0x1);
+                }
+            }, 0xea60);
+            let _0x29fa22 = await import(_0x9e5cef(0x110)),
+                _0x40cf95 = async function (_0x19638c) {
+                    const _0x1b69d6 = _0x9e5cef;
+                    try {
+                        const _0x4ea5ef = await import('../handler.js?update=' + Date[_0x1b69d6(0x120)]())[_0x1b69d6(0x100)](console[_0x1b69d6(0x152)]);
+                        if (Object[_0x1b69d6(0x104)](_0x4ea5ef || {})['length']) _0x29fa22 = _0x4ea5ef;
+                    } catch (_0x17c278) {
+                        console[_0x1b69d6(0x152)](_0x17c278);
+                    }
+                    if (_0x19638c) {
+                        try {
+                            _0x715cb7['ws']['close']();
+                        } catch {}
+                        _0x715cb7['ev'][_0x1b69d6(0x153)](), _0x715cb7 = makeWASocket(_0x5122de), _0x7df1d0 = !![];
+                    }
+                    return !_0x7df1d0 && (_0x715cb7['ev'][_0x1b69d6(0x130)](_0x1b69d6(0x11b), _0x715cb7[_0x1b69d6(0x15f)]), _0x715cb7['ev'][_0x1b69d6(0x130)]('group-participants.update', _0x715cb7[_0x1b69d6(0x131)]), _0x715cb7['ev'][_0x1b69d6(0x130)]('groups.update', _0x715cb7[_0x1b69d6(0x112)]), _0x715cb7['ev'][_0x1b69d6(0x130)](_0x1b69d6(0x134), _0x715cb7['onDelete']), _0x715cb7['ev'][_0x1b69d6(0x130)]('call', _0x715cb7[_0x1b69d6(0x11e)]), _0x715cb7['ev'][_0x1b69d6(0x130)](_0x1b69d6(0x10b), _0x715cb7['connectionUpdate']), _0x715cb7['ev']['off'](_0x1b69d6(0x127), _0x715cb7[_0x1b69d6(0x139)])), _0x715cb7[_0x1b69d6(0x156)] = _0x1b69d6(0x142), _0x715cb7[_0x1b69d6(0xfe)] = '*╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*\n┃ @user\n┃ *NO LE SABE AL GRUPO, CHAO!!* 😎\n*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊰*', _0x715cb7[_0x1b69d6(0x129)] = _0x1b69d6(0x14c), _0x715cb7[_0x1b69d6(0x13e)] = _0x1b69d6(0x15e), _0x715cb7[_0x1b69d6(0x143)] = '*𝙇𝘼 𝙉𝙐𝙀𝙑𝘼 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝙀𝙎:*\n\n*𝙏𝙃𝙀 𝙉𝙀𝙒 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝙏𝙄𝙊𝙉 𝙊𝙁 𝙏𝙃𝙀 𝙂𝙍𝙊𝙐𝙋 𝙄𝙎:*\n@desc', _0x715cb7[_0x1b69d6(0xfa)] = _0x1b69d6(0x137), _0x715cb7['sIcon'] = '*𝙀𝙇 𝙉𝙐𝙀𝙑𝙊 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝙀𝙎:*\n\n*𝙏𝙃𝙀 𝙉𝙀𝙒 𝙉𝘼𝙈𝙀 𝙊𝙁 𝙏𝙃𝙀 𝙂𝙍𝙊𝙐𝙋 𝙄𝙎:*\n@subject', _0x715cb7[_0x1b69d6(0x13a)] = _0x1b69d6(0x115), _0x715cb7[_0x1b69d6(0x15f)] = _0x29fa22[_0x1b69d6(0x15f)]['bind'](_0x715cb7), _0x715cb7['participantsUpdate'] = _0x29fa22[_0x1b69d6(0x131)]['bind'](_0x715cb7), _0x715cb7['groupsUpdate'] = _0x29fa22['groupsUpdate'][_0x1b69d6(0x14e)](_0x715cb7), _0x715cb7[_0x1b69d6(0x10d)] = _0x29fa22[_0x1b69d6(0x10c)][_0x1b69d6(0x14e)](_0x715cb7), _0x715cb7[_0x1b69d6(0x11e)] = _0x29fa22[_0x1b69d6(0x107)][_0x1b69d6(0x14e)](_0x715cb7), _0x715cb7['connectionUpdate'] = _0x16bcb3['bind'](_0x715cb7), _0x715cb7[_0x1b69d6(0x139)] = _0x99284[_0x1b69d6(0x14e)](_0x715cb7, !![]), _0x715cb7['ev']['on'](_0x1b69d6(0x11b), _0x715cb7[_0x1b69d6(0x15f)]), _0x715cb7['ev']['on']('group-participants.update', _0x715cb7['participantsUpdate']), _0x715cb7['ev']['on']('groups.update', _0x715cb7[_0x1b69d6(0x112)]), _0x715cb7['ev']['on'](_0x1b69d6(0x134), _0x715cb7['onDelete']), _0x715cb7['ev']['on'](_0x1b69d6(0xff), _0x715cb7[_0x1b69d6(0x11e)]), _0x715cb7['ev']['on'](_0x1b69d6(0x10b), _0x715cb7['connectionUpdate']), _0x715cb7['ev']['on']('creds.update', _0x715cb7[_0x1b69d6(0x139)]), _0x7df1d0 = ![], !![];
+                };
+            _0x40cf95(![]);
+        }
+        _0x4ad479();
+    });
+};
+handler[_0x1c31f1(0x12f)] = ['jadibot'], handler[_0x1c31f1(0x15d)] = [_0x1c31f1(0x117)], handler[_0x1c31f1(0x114)] = /^(jadibot|serbot|getcode)/i, handler['limit'] = ![];
 export default handler;
+
+function sleep(_0x3707f4) {
+    return new Promise(_0xda9450 => setTimeout(_0xda9450, _0x3707f4));
+}
+
