4adf8ca9812ff277feb71ad4be551743e86fd20b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-16 00:51:22 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/jadibot-parar.js
+++ plugins/jadibot-parar.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
     if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Por qué no vas directamente con el numero del Bot?', m)
     else {
-      await conn.reply(m.chat, 'Adiós Bot :\')', m)
+      await conn.reply(m.chat, 'Adiós Bot :/', m)
       conn.ws.close()
     }
   }
