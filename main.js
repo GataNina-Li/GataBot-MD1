a0d85de45ddf72a667192e15ab5b64bc4a295194
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-17 11:05:43 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ import path, { join } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
 import { platform } from 'process'
 import * as ws from 'ws';
-import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, rmSync, watch } from 'fs';
-//import watch from 'glob-fs'
+import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch } from 'fs';
 import yargs from 'yargs';
 import { spawn } from 'child_process';
 import lodash from 'lodash';
