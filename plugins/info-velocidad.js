3dab735f18c55654d2d8aaee07ee1168897d118f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-20 17:59:14 -0500

    Create info-velocidad.js

---
+++ plugins/info-velocidad.js
@@ -0,0 +1,104 @@
+import { cpus as _cpus, totalmem, freemem } from 'os'
+import util from 'util'
+import os from 'os'
+import fetch from 'node-fetch'
+import osu from 'node-os-utils'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+let format = sizeFormatter({
+std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+decimalPlaces: 2,
+keepTrailingZeroes: false,
+render: (literal, symbol) => `${literal} ${symbol}B`,
+})
+let handler = async (m, { conn, usedPrefix, isRowner}) => {
+let _muptime
+if (process.send) {
+process.send('uptime')
+_muptime = await new Promise(resolve => {
+process.once('message', resolve)
+setTimeout(resolve, 1000)
+}) * 1000
+}
+let muptime = clockString(_muptime)
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+const used = process.memoryUsage()
+const cpus = _cpus().map(cpu => {
+cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+return cpu
+})
+const cpu = cpus.reduce((last, cpu, _, { length }) => {
+last.total += cpu.total
+last.speed += cpu.speed / length
+last.times.user += cpu.times.user
+last.times.nice += cpu.times.nice
+last.times.sys += cpu.times.sys
+last.times.idle += cpu.times.idle
+last.times.irq += cpu.times.irq
+return last
+}, {
+speed: 0,
+total: 0,
+times: {
+user: 0,
+nice: 0,
+sys: 0,
+idle: 0,
+irq: 0
+}})
+
+let old = performance.now()
+await m.reply(`${htjava} *T e s t i n g. . .*`)
+let neww = performance.now()
+let speed = neww - old
+let caption = `*${htki} V E L O C I D A D ${htka}*
+${Math.round(neww - old)} ms
+${speed} ms
+
+*${htjava} R U N T I M E* 
+${muptime}
+${readMore}
+*${htjava} C H A T S*
+• *${groupsIn.length}* Chats grupales
+• *${groupsIn.length}* Grupos Unidos
+• *${groupsIn.length - groupsIn.length}* Grupos restantes
+• *${chats.length - groupsIn.length}* Chats Privados
+• *${chats.length}* Total Chats
+
+*${htjava} S E R V I D O R*
+*🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
+*🔵 RAM Libre:* ${format(freemem())}
+
+*💻 Plataforma :* ${os.platform()}
+*📡 Servidor :* ${os.hostname()}
+${readMore}
+
+*NodeJS Uso de memoria*
+${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
+
+${cpus[0] ? `*Uso total de la CPU*
+${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+
+*CPU Core(s) Uso (${cpus.length} Core CPU)*
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
+`
+
+await conn.sendButton(m.chat, caption, wm, img, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m)
+}
+handler.help = ['ping', 'speed']
+handler.tags = ['info', 'tools']
+
+handler.command = /^(ping2)$/i
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return ['\n' + d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+}
