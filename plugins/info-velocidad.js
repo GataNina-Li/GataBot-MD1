c3be7b2e8828861fd19db26f6665b573f604f998
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-20 18:31:59 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/info-velocidad.js
+++ plugins/info-velocidad.js
@@ -12,14 +12,21 @@ keepTrailingZeroes: false,
 render: (literal, symbol) => `${literal} ${symbol}B`,
 })
 let handler = async (m, { conn, usedPrefix, isRowner}) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let grupos = [nna, nn, nnn, nnnt, nnntt, nnnttt]
+let gata = [img5, img6, img7, img8, img9]
+let enlace = { contextInfo: { externalAdReply: {title: wm + ' 🐈', body: 'support group' , sourceUrl: grupos.getRandom(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
+let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: '😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(img)).buffer(), sourceUrl: yt }}}
+let dos = [enlace, enlace2]
+
 let _muptime
 if (process.send) {
 process.send('uptime')
 _muptime = await new Promise(resolve => {
 process.once('message', resolve)
 setTimeout(resolve, 1000)
-}) * 1000
-}
+}) * 1000 }
+  
 let muptime = clockString(_muptime)
 const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
 const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
@@ -28,6 +35,7 @@ const cpus = _cpus().map(cpu => {
 cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
 return cpu
 })
+
 const cpu = cpus.reduce((last, cpu, _, { length }) => {
 last.total += cpu.total
 last.speed += cpu.speed / length
@@ -37,6 +45,7 @@ last.times.sys += cpu.times.sys
 last.times.idle += cpu.times.idle
 last.times.irq += cpu.times.irq
 return last
+  
 }, {
 speed: 0,
 total: 0,
@@ -49,32 +58,32 @@ irq: 0
 }})
 
 let old = performance.now()
-await m.reply(`${htjava} *T e s t i n g. . .*`)
+await conn.sendButton(m.chat, wm, `${eg}🚀 𝙋𝙍𝙐𝙀𝘽𝘼 𝘿𝙀 𝙑𝙀𝙇𝙊𝘾𝙄𝘿𝘼𝘿...\n🚀 𝙎𝙋𝙀𝙀𝘿 𝙏𝙀𝙎𝙏...`, null, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], fkontak, m)  
 let neww = performance.now()
 let speed = neww - old
-let caption = `*${htki} V E L O C I D A D ${htka}*
-${Math.round(neww - old)} ms
-${speed} ms
+let caption = `*${htki} 𝙑𝙀𝙇𝙊𝘾𝙄𝘿𝘼𝘿 : 𝙎𝙋𝙀𝙀𝘿 ${htka}*
+*${Math.round(neww - old)}* ms
+*${speed}* ms
 
-*${htjava} R U N T I M E* 
+*${htjava}* 𝙏𝙄𝙀𝙈𝙋𝙊 𝘿𝙀 𝙀𝙅𝙀𝘾𝙐𝘾𝙄𝙊𝙉 : 𝙍𝙐𝙉𝙏𝙄𝙈𝙀 
 ${muptime}
 ${readMore}
 *${htjava} C H A T S*
-• *${groupsIn.length}* Chats grupales
-• *${groupsIn.length}* Grupos Unidos
-• *${groupsIn.length - groupsIn.length}* Grupos restantes
-• *${chats.length - groupsIn.length}* Chats Privados
-• *${chats.length}* Total Chats
+• *${groupsIn.length}* \`\`\`Grupales : Groups\`\`\`
+• *${groupsIn.length}* \`\`\`Grupos Unidos : Join Groups\`\`\`
+• *${groupsIn.length - groupsIn.length}* \`\`\`Grupos Restantes : Groups Left\`\`\`
+• *${chats.length - groupsIn.length}* \`\`\`Chats Privados : Chats Private\`\`\`
+• *${chats.length}* \`\`\`Total Chats\`\`\`
 
-*${htjava} S E R V I D O R*
-*🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
-*🔵 RAM Libre:* ${format(freemem())}
+*${htjava} S E R V I D O R : S E R V E R*
+*🟢 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
+*🟡 RAM Libre:* ${format(freemem())}
 
-*💻 Plataforma :* ${os.platform()}
-*📡 Servidor :* ${os.hostname()}
+*💻 Plataforma :* \`\`\`${os.platform()}\`\`\`
+*📡 Servidor :* _${os.hostname()}_
 ${readMore}
 
-*NodeJS Uso de memoria*
+*NodeJS Uso de memoria : Memory Usage*
 ${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
 
 ${cpus[0] ? `*Uso total de la CPU*
@@ -84,7 +93,7 @@ ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type =>
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
 `
 
-await conn.sendButton(m.chat, caption, wm, img, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m)
+await conn.sendButton(m.chat, rg, caption, gata, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, dos.getRandom())
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
@@ -100,5 +109,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return ['\n' + d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+  return ['\n ' + d, ' *Días : Days ☀️*\n ', h, ' *Horas : Hours 🕐*\n ', m, ' *Minuto : Minute ⏰*\n ', s, ' *Segundo : Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
 }
