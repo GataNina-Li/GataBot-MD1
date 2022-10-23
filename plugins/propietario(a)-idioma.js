6844da72f1d48357bb6f2eb68eeb5dbd555e6563
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-23 11:15:51 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/propietario(a)-idioma.js
+++ plugins/propietario(a)-idioma.js
@@ -2,7 +2,7 @@ import * as fs from 'fs'
 import { en, es } from '../lib/idiomas/total-idiomas.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  
+try {  
 if (args[0] == 'es'){
 global.lenguajeGB = es
 m.reply('EXITO!!\nIdioma de GataBot cambiado al Español')
@@ -19,9 +19,12 @@ ${usedPrefix + command} es
 *To add the Spanish Language Use:*
 ${usedPrefix + command} en
 
-`\`\`\`Solo los comandos no cambiaran de Idioma
+\`\`\`Solo los comandos no cambiaran de Idioma
 
-Commands will not change language\`\`\`\``, wm, null, [[`𝗠 𝗘 𝗡 𝗨 😽`, `${usedPrefix}menu`]], m)}}
+Commands will not change language\`\`\`\``, wm, null, [[`𝗠 𝗘 𝗡 𝗨 😽`, `${usedPrefix}menu`]], m)}
+} catch { 
+await m.reply(`${fg}\`\`\`NO SE LOGRÓ CAMBIAR DE IDIOMA, REPORTE ESTE COMANDO ${usedPrefix + command} CON EL COMANDO ${usedPrefix}reporte\`\`\``)    
+}}
 
 handler.command = /^(idioma)$/i
 handler.owner = true
