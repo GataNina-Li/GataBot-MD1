71dce20a24ed3378ba5c0094807a53320965b7ea
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-01 16:57:27 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -4,25 +4,14 @@ export function before(m, { conn }) {
 //if (!db.data.chats[m.chat].autonivel && m.isGroup) throw 
 	
 let user = global.db.data.users[m.sender]
-if (!user.autolevelup) //throw `${ag}𝙀𝙇 𝘼𝙐𝙏𝙊𝙉𝙄𝙑𝙀𝙇 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊 𝙐𝙎𝙀 *#on autonivel* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝘼𝙐𝙏𝙊𝙉𝙄𝙑𝙀𝙇 𝙏𝙃𝙄𝙎 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on autonivel* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
-  return !0
+let chat = global.db.data.chats[m.chat]
+if (!chat.autolevelup)
+return !0
 	
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
  if (before !== user.level) {
-	  
-/*m.reply(`
-*╭━━━[ 𝙉𝙄𝙑𝙀𝙇 | 𝙇𝙀𝙑𝙀𝙇 ]━━━━⬣*
-*┃ NIVEL ANTERIOR: ${before}*
-*┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈*
-*┃ NIVEL ACTUAL: ${user.level}*
-*┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈*
-*┃ FECHA: ${new Date().toLocaleString('id-ID')}*
-*╰━━━〔 𓃠 ${vs} 〕━━━━━⬣*
-
-*_Cuanto más interactúes con GataBot-MD, mayor será tu nivel!!_*
-`.trim())*/
-	 
+	  	 
 conn.sendButton(m.chat, '🐈 ' + wm, `*╭━━━[ 𝙉𝙄𝙑𝙀𝙇 | 𝙇𝙀𝙑𝙀𝙇 ]━━━⬣*
 *┃ NIVEL ANTERIOR: ${before}*
 *┃┈┈┈┈┈┈┈┈┈┈┈┈┈*
