e131def3d284f0c48d2edd11311eb5a7745c0082
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-02 13:02:08 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -29,7 +29,7 @@ this.spam[m.sender].spam = 0
 this.spam[m.sender].lastspam = new Date * 1
 let tiempo = 60000 * 1
 let time = user.antispam + tiempo * 1
-let texto = `*@${m.sender.split("@")[0]} ${lenguajeGB['smsNoSpam']()}*` 
+let texto = `*@${m.sender.split("@")[0]} ${DDDD}${lenguajeGB['smsNoSpam']()}*` 
 
 if (new Date - user.antispam < tiempo * 1) return
 await conn.reply(m.chat, texto,  m, { mentions: this.parseMention(texto) })
@@ -45,7 +45,7 @@ this.spam[m.sender].lastspam = new Date * 1
   
 } catch (e) {
 console.log(e)
-m.reply(`${lenguajeGB['smsAvisoFG']()}*OCURRIÓ UN ERROR INESPERADO*`)
+m.reply(lenguajeGB.smsMalError())
 }}}
 export default handler
 
