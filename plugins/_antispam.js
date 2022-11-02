74a5651e21963f825c2fd1ed13341b9079395403
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-01 21:46:26 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,14 +1,12 @@
 //CRÉDITOS: https://github.com/Abiguelreyes75
 
-let handler = async (m, { conn, usedPrefix }) => { 
-let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[this.user.jid] || {}
-
-if (chat.antiSpam) {
-//if (m.isGroup) return !1
+let handler = m => m
+handler.all = async function (m) {
   
+let chat = global.db.data.chats[m.chat]
 let delet = m.key.participant
 let bang = m.key.id
+let bot = global.db.data.settings[this.user.jid] || {}
 let user = global.db.data.users[m.sender]
 
 this.spam = this.spam ? this.spam : {}
@@ -30,13 +28,13 @@ this.spam[m.sender].spam = 0
 this.spam[m.sender].lastspam = new Date * 1
 let tiempo = 60000 * 1
 let time = user.antispam + tiempo * 1
-let texto = `*@${m.sender.split("@")[0]} 🤨 NO HAGAS SPAM, NO PODRÁ USAR A ${packname} POR ${tiempo / 1000 - 59} MINUTO*`
+let texto = `*@${m.sender.split("@")[0]} 🤨 NO HAGAS SPAM, NO PODRÁ USAR A ${packname} POR ${tiempo / 1000 - 59} MINUTO*` 
 
 if (new Date - user.antispam < tiempo * 1) return
 await conn.reply(m.chat, texto,  m, { mentions: this.parseMention(texto) })
 user.banned = true
-//await conn.updateBlockStatus(m.chat, 'block') ⚠⚠⚠ 
-//await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+  
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 user.antispam = new Date * 1  
   
 } else {
@@ -47,7 +45,7 @@ this.spam[m.sender].lastspam = new Date * 1
 } catch (e) {
 console.log(e)
 m.reply(`${lenguajeGB['smsAvisoFG']()}*OCURRIÓ UN ERROR INESPERADO*`)
-}}}
+}}
 export default handler
 
 function msToTime(duration) {
