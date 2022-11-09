19ce804abe1aa083d19ab40cdb5811e7a1139f35
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-09 13:56:35 -0500

    Update config-on y off.js

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import fs from 'fs'
+import fs from 'fs' 
 
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner, text }) => { 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
