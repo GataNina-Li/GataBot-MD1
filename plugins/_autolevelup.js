8259eb2de15b627e63168bdb729eea400771d373
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-10 20:04:54 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -46,9 +46,9 @@ let normalCant3 = [1, 3, 3, 3, 4, 4, 2, 2, 4, 4, 4, 4, 1].getRandom()
 if (user.level == 5){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 5!!* 🏆
 𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 1} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 1} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 1} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 1
 user[especial2] += especialCant2 * 1
 user[especial3] += especialCant3 * 1
@@ -57,9 +57,9 @@ user[especial4] += especialCant4 * 1
 }else if (user.level == 10){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 10!!* 🏆
 𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 1} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 1} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 1} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 1} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 1
 user[especial2] += especialCant2 * 1
 user[especial3] += especialCant3 * 1
@@ -68,9 +68,9 @@ user[especial4] += especialCant4 * 1
 }else if (user.level == 15){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 15!!* 🏆
 𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 2} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 2} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 2} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 2
 user[especial2] += especialCant2 * 2
 user[especial3] += especialCant3 * 2
@@ -79,9 +79,9 @@ user[especial4] += especialCant4 * 2
 }else if (user.level == 20){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 20!!* 🏆
 𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 2} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 2} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 2} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 2} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 2
 user[especial2] += especialCant2 * 2
 user[especial3] += especialCant3 * 2
@@ -90,9 +90,9 @@ user[especial4] += especialCant4 * 2
 }else if (user.level == 25){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 25!!* 🏆
 𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 3} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 3} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 3} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 3
 user[especial2] += especialCant2 * 3
 user[especial3] += especialCant3 * 3
@@ -101,9 +101,9 @@ user[especial4] += especialCant4 * 3
 }else if (user.level == 30){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 30!!* 🏆
 𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 3} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 3} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 3} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 3} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 3
 user[especial2] += especialCant2 * 3
 user[especial3] += especialCant3 * 3
@@ -112,9 +112,9 @@ user[especial4] += especialCant4 * 3
 }else if (user.level == 35){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 35!!* 🏆
 𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 4} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 4} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 4} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 4
 user[especial2] += especialCant2 * 4
 user[especial3] += especialCant3 * 4
@@ -123,9 +123,9 @@ user[especial4] += especialCant4 * 4
 }else if (user.level == 40){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 40!!* 🏆
 𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 4} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 4} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 4} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 4
 user[especial2] += especialCant2 * 4
 user[especial3] += especialCant3 * 4
@@ -134,9 +134,9 @@ user[especial4] += especialCant4 * 4
 }else if (user.level == 45){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 45!!* 🏆
 𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 4} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 4} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 4} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 4} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 4
 user[especial2] += especialCant2 * 4
 user[especial3] += especialCant3 * 4
@@ -145,9 +145,9 @@ user[especial4] += especialCant4 * 4
 }else if (user.level == 50){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 50!!* 🏆
 𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 5} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 5} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 5} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 5
 user[especial2] += especialCant2 * 5
 user[especial3] += especialCant3 * 5
@@ -156,9 +156,9 @@ user[especial4] += especialCant4 * 5
 }else if (user.level == 55){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 55!!* 🏆
 𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 5} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 5} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 5} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 5
 user[especial2] += especialCant2 * 5
 user[especial3] += especialCant3 * 5
@@ -167,9 +167,9 @@ user[especial4] += especialCant4 * 5
 }else if (user.level == 60){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 60!!* 🏆
 𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 5} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 5} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 5} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 5
 user[especial2] += especialCant2 * 5
 user[especial3] += especialCant3 * 5
@@ -178,9 +178,9 @@ user[especial4] += especialCant4 * 5
 }else if (user.level == 65){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 65!!* 🏆
 𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 5} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 5} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 5} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 5} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 5
 user[especial2] += especialCant2 * 5
 user[especial3] += especialCant3 * 5
@@ -189,9 +189,9 @@ user[especial4] += especialCant4 * 5
 }else if (user.level == 70){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 70!!* 🏆
 𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 6} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 6} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 6} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 6
 user[especial2] += especialCant2 * 6
 user[especial3] += especialCant3 * 6
@@ -200,9 +200,9 @@ user[especial4] += especialCant4 * 6
 }else if (user.level == 75){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 75!!* 🏆
 𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 6} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 6} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 6} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 6} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 6
 user[especial2] += especialCant2 * 6
 user[especial3] += especialCant3 * 6
@@ -211,9 +211,9 @@ user[especial4] += especialCant4 * 6
 }else if (user.level == 80){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 80!!* 🏆
 𓃠 *${especialCant * 7} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 7} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 7} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 7} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 7} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 7} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 7} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 7
 user[especial2] += especialCant2 * 7
 user[especial3] += especialCant3 * 7
@@ -222,9 +222,9 @@ user[especial4] += especialCant4 * 7
 }else if (user.level == 85){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 85!!* 🏆
 𓃠 *${especialCant * 8} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 8} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 8} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 8} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 8} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 8} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 8} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 8
 user[especial2] += especialCant2 * 8
 user[especial3] += especialCant3 * 8
@@ -233,9 +233,9 @@ user[especial4] += especialCant4 * 8
 }else if (user.level == 90){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 90!!* 🏆
 𓃠 *${especialCant * 9} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 9} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 9} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 9} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 9} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 9} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 9} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 9
 user[especial2] += especialCant2 * 9
 user[especial3] += especialCant3 * 9
@@ -244,9 +244,9 @@ user[especial4] += especialCant4 * 9
 }else if (user.level == 95){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 95!!* 🏆
 𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 10} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 10} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 10} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 10
 user[especial2] += especialCant2 * 10
 user[especial3] += especialCant3 * 10
@@ -255,9 +255,9 @@ user[especial4] += especialCant4 * 10
 }else if (user.level == 100){
 m.reply(`*${lenguajeGB.smsAutoLv7()} 100!!* 🏆
 𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*
-𓃠 *${especialCant * 10} ${global.rpgshop.emoticon(especial)}*`)
+𓃠 *${especialCant2 * 10} ${global.rpgshop.emoticon(especial2)}*
+𓃠 *${especialCant3 * 10} ${global.rpgshop.emoticon(especial3)}*
+𓃠 *${especialCant4 * 10} ${global.rpgshop.emoticon(especial4)}*`)
 user[especial] += especialCant * 10
 user[especial2] += especialCant2 * 10
 user[especial3] += especialCant3 * 10
