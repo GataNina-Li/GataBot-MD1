9b548dcffb8447eb01879c9a5698e5832033eb26
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-13 02:13:51 -0500

    Update audio-efecto-menu.js

--- plugins/audio-efecto-menu.js
+++ plugins/audio-efecto-menu.js
@@ -57,7 +57,7 @@ readmore: readMore
 }
 text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
   
-let pp = './media/menus/Menuvid1.mp4'
+let pp = './media/menus/Menuvid2.mp4'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 let username = conn.getName(who)
