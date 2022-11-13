c8d12df6d789a85e63f1607ec4a0fdc26c8b3c35
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-13 18:15:11 -0500

    Update propietario(a)-notificar.js

--- plugins/propietario(a)-notificar.js
+++ plugins/propietario(a)-notificar.js
@@ -1,4 +1,4 @@
-//let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+/*//let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 let linkRegex = /chat.whatsapp.com\/Eb5vV0LQdSV5twN4cSwN19/
 //let linkRegex = 'chat.whatsapp.com/Eb5vV0LQdSV5twN4cSwN19'
 let handler = async (m, { conn, text, isOwner, usedPrefix, command, groupMetadata }) => {
@@ -22,6 +22,30 @@ await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)	
 }}
+
+handler.command = ['mensaje'] 
+handler.owner = true
+
+export default handler*/
+
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+
+let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+
+  let fakegif = { key: {participant: `0@s.whatsapp.net`, ...("6289643739077-1613049930@g.us" ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": 'lolibot', "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': wm, 'jpegThumbnail': false }}}
+
+  if (!text) throw `${fg}\n𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝙶𝚁𝚄𝙿𝙾 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿`
+  let [_, code, expired] = text.match(linkRegex) || []
+  if (!code) throw 'El link es invalido'
+  let res = await conn.groupAcceptInvite(code)
+  //await m.reply(`${JSON.stringify(res, null, 1)}`)
+  await m.reply(`Enviando mensaje . . .`)
+// Debes de poner tus enlaces abajo.
+await conn.sendMessage(res, { text: 'TEXTO QUE ENVIARA AL GRUPO', mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fakegif })
+  await m.reply(`✅ 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾`)
+}
+
 
 handler.command = ['mensaje'] 
 handler.owner = true
