744d7c7235e567c031f94a7d75cc62b21116f65b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-13 20:06:11 -0500

    Update propietario(a)-notificar.js

--- plugins/propietario(a)-notificar.js
+++ plugins/propietario(a)-notificar.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
-const grupo = 'https://chat.whatsapp.com/Eb5vV0LQdSV5twN4cSwN19' //nna
+const grupo = 'https://chat.whatsapp.com/Lq6iC0wtPz222IIobgCED1' //nna
 
 let handler = async (m, { conn, text, usedPrefix, command, groupMetadata }) => {
 let users = m.sender.split`@`[0]
