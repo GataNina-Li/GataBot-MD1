dcb5ef047ea184695d5dab7a4db58c07178cd0f9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-13 21:29:31 -0500

    Update propietario(a)-notificar.js

--- plugins/propietario(a)-notificar.js
+++ plugins/propietario(a)-notificar.js
@@ -2,7 +2,9 @@ import fs from 'fs'
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 const grupo = 'https://chat.whatsapp.com/Lq6iC0wtPz222IIobgCED1' //nna
 
-let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => { 
+let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => {
+let users = m.sender.split`@`[0]
+if ( users == 593993684821 || users == 593968585383 ) {
 let fkontak2 = { key: { participant: '0@s.whatsapp.net' }, message: { contactMessage: { displayName: `~ 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${author},;;;\nFN:${author},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, jpegThumbnail: fs.readFileSync('./media/menus/Menu1.jpg'), thumbnail: fs.readFileSync('./media/menus/Menu1.jpg'), sendEphemeral: true }}}
 if (!text) throw `*Falta Texto*`
 let [_, code] = grupo.match(linkRegex) || []
@@ -11,13 +13,15 @@ try{
 let res = await conn.groupAcceptInvite(code)
 await conn.sendMessage(res, { text: text, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak2 })
 await m.reply(`✅ *MENSAJE ENVIADO CON ÉXITO* `)
+}else{
+await m.reply(`NOOO`) 
 
 } catch (e) {
 await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)	
   
-}}
+}}}
 handler.command = ['mensaje']
 handler.owner = true
 
