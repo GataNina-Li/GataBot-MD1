347377761e9bda3efd3cf21aa474c0ba4bea9534
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-17 17:14:52 -0500

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -2,16 +2,12 @@ import fetch from 'node-fetch'
 let handler = m => m
 
 handler.before = async (m) => {
-let q = m.quoted ? await m.getQuotedObj() : m
 let chat = global.db.data.chats[m.chat]
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-if (!m.text) return
 let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
 let data = await ressimi.json();
-if (m.react) return !0 // return this.sendMessage(m.chat, { react: { text: '😵‍', key: q.key }}, { quoted: q })
-if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') await m.reply("😵‍💫")
-                                                                                                    
+if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') await m.reply("😵‍💫")                                                                                                   
 await m.reply(data.success)
 return !0
 }
