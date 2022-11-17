db1bf46775658b46899d47ac69e40372f13ed872
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-17 16:09:58 -0500

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -8,7 +8,10 @@ handler.before = async (m) => {
         if (!m.text) return
         let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
         let data = await ressimi.json();
-        if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') await m.reply("que xd")
+        if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') { await m.reply("que xd")
+                                                                                                    }else{
+                                                                                                        m.reply("Error")}
+        
         await m.reply(data.success)
         return !0
     }
