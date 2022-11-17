06c89a53196d44065a4aa2e4f64dc0883afbb795
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-17 10:20:37 -0300

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -4,6 +4,7 @@ if (m.isBaileys && m.fromMe)
 return !0
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
+let delet = m.key.participant
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
@@ -15,6 +16,7 @@ if (m.text.includes(linkThisGroup)) return !0
 }    
 await conn.sendButton(m.chat, `${lenguajeGB['smsEnlaceWat']()} ${await this.getName(m.sender)} ${isBotAdmin ? '' : `\n\n${lenguajeGB['smsAllAdmin']()}`}`, wm, [`${lenguajeGB['smsApagar']()}`, '/disable antilink'], m)    
 if (isBotAdmin) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 } else if (!bot.restrict) return m.reply(`${lenguajeGB['smsSoloOwner']()}`)
 }
