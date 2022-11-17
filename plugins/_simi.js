2d71e12300ab34be0699f737b5e9b05c3752deae
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-17 16:15:03 -0500

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -2,19 +2,18 @@ import fetch from 'node-fetch'
 let handler = m => m
 
 handler.before = async (m) => {
-    let chat = global.db.data.chats[m.chat]
-    if (chat.simi) {
-        if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-        if (!m.text) return
-        let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
-        let data = await ressimi.json();
-        if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') { await m.reply("que xd")
-                                                                                                    }else{
-                                                                                                        m.reply("Error")}
-        
-        await m.reply(data.success)
-        return !0
-    }
-    return true
+let chat = global.db.data.chats[m.chat]
+if (chat.simi) {
+if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+if (!m.react) return  m.reply("Error") 
+if (!m.text) return
+let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
+let data = await ressimi.json();
+if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') await m.reply("😵‍💫")
+                                                                                                    
+await m.reply(data.success)
+return !0
+}
+return true
 }
 export default handler
