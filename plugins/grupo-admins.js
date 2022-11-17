87c3a5bfeea94a6321dc922c44d8eef5aca7d343
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-17 18:24:29 -0500

    Update grupo-admins.js

--- plugins/grupo-admins.js
+++ plugins/grupo-admins.js
@@ -1,10 +1,10 @@
-let handler = async (m, { conn, participants, groupMetadata, args, usedPrefix, command }) => {
+let handler = async (m, { conn, participants, groupMetadata, texto, usedPrefix, command }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.jpg'
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `» ${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-let pesan = args ? args.join` ` : `${lenguajeGB.smsAddB6()}`
-let oi = `${lenguajeGB.smsAddB5()} _${pesan}_}`
+let pesan = texto ? texto.join` ` : `${lenguajeGB.smsAddB6()}`
+let oi = `${lenguajeGB.smsAddB5()} _${pesan}_`
 
 let textoA = 
 `*⊱ ──── 《.⋅ 🐈 ⋅.》 ──── ⊰*
