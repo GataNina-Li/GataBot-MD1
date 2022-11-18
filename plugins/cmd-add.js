451a4fec3f285fc399e916016adc191a66dcde4d
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-18 19:23:49 -0300

    Update cmd-add.js

--- plugins/cmd-add.js
+++ plugins/cmd-add.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 global.db.data.sticker = global.db.data.sticker || {}
-if (!m.quoted) throw '*[❗INFO❗] RESPONDE AL STICKER O IMAGEN AL CUAL QUIERE AGREGA UN CONANDO O TEXTO*'
+if (!m.quoted) throw '*[❗INFO❗] RESPONDE AL STICKER O IMAGEN AL CUAL QUIERE AGREGA UN COMANDO O TEXTO*'
 if (!m.quoted.fileSha256) throw '*[❗INFO❗] SOLO PUEDES ASIGNAR COMANDOS O TEXTOS A STICKERS E IMAGEN*'
 if (!text) throw `*[❗INFO❗] ERROR, LOS USARTE MAL,\n USA DE ESTA MANERA:*\n*—◉ ${usedPrefix + command} <texto> <responder a sticker o imagen>*\n\n*EJEMPLOS:*\n*—◉ ${usedPrefix + command} <#menu> <responder a sticker o imagen>*`
 let sticker = global.db.data.sticker
