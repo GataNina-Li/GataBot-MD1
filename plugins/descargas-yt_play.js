012e61817e7dc0caa563dfb6ca06d2dad0d1f3d2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-19 23:29:03 -0400

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -69,5 +69,5 @@ handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
 handler.exp = 20
-handler.money = 1
+handler.money = 200
 export default handler
