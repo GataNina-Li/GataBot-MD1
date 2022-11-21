96aa064ad4c35a7b6869d33928f37b85b942fb21
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-21 14:03:53 -0300

    Add files via upload

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -5,6 +5,7 @@ handler.before = async (m) => {
 let chat = global.db.data.chats[m.chat]
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+if (!m.text) return
 let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
 let data = await ressimi.json();
 if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') await m.reply("😵‍💫")                                                                                                   
