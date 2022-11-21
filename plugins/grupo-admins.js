0783f0988e761b5221cc819809264035d3c66286
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-21 16:26:50 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/grupo-admins.js
+++ plugins/grupo-admins.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, participants, groupMetadata, args, usedPrefix, command }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.jpg'
 const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `» ${i + 1}. @${v.id.split('@')[0]}`).join('\n')
+const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
 let oi = `${lenguajeGB.smsAddB5()} _${pesan}_`
@@ -13,7 +13,7 @@ let textoA =
 *⊱ ──── 《.⋅ ${vs} ⋅.》 ──── ⊰*`
 
 let textoB = 
-`*${listAdmin}*
+`${listAdmin}
 
 ⛔ ${lenguajeGB.smsAddB4()} ⛔`.trim()
 await conn.sendButton(m.chat, textoA, textoB, pp, [[lenguajeGB.smsConMenu(), `.menu`]], m, { mentions: [...groupAdmins.map(v => v.id), owner] })
