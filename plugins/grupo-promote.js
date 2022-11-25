f81da61b4c621f90f835599c2b7c5923a46aec45
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-24 23:47:53 -0500

    Update grupo-promote.js

--- plugins/grupo-promote.js
+++ plugins/grupo-promote.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn,usedPrefix, text, command }) => {
+/*let handler = async (m, { conn,usedPrefix, text, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if(isNaN(text) && !text.match(/@/g)){
 	
@@ -28,4 +28,4 @@ handler.group = true
 handler.admin = true
 handler.botAdmin = true
 handler.fail = null
-export default handler
+export default handler*/
