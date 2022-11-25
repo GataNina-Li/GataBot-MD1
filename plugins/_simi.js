d40bfef5235b7c1160e8bcacbb05096cec1bc096
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-25 00:15:18 -0500

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch'
 let handler = m => m
 
-handler.before = async (m) => {
+//handler.before = async (m) => {
+handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
@@ -10,8 +11,8 @@ let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent
 let data = await ressimi.json();
 if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return 0
 await m.reply(data.success)
-return 0
+return !0
 }
-return 0
+return true
 } 
 export default handler
