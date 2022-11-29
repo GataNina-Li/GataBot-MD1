a0d2492c3c7abc215074ade41589da4f5dec0bbd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-28 21:52:23 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
