1d5a22293cb9156b99f1ee4d89436d8d92b31644
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-11-30 18:47:54 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -4,13 +4,14 @@ let handler = m => m
 handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
 global.db.data.users[m.sender].money += 50
-global.db.data.users[m.sender].exp += 50  
+global.db.data.users[m.sender].exp += 50     
+let frep = { contextInfo: { externalAdReply: {title: wm, body: author, sourceUrl: md, thumbnail: fs.readFileSync('./media/menus/Menu3.jpg')}}}
     
-if (/^A Bueno master|Bueno master|Bueno Máster|🫂$/i.test(m.text) && chat.audios) {  
+if (/^bueno master|bueno máster|🫂$/i.test(m.text) && chat.audios) {  
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
 let vn = './media/A bueno adios master.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
+this.sendFile(m.chat, vn, 'error.mp3', null, frep, m, true, {type: 'audioMessage', ptt: true})}   
 
 if (chat.audios && m.text.match(/(bienveni|🥳|🤗|👋)/gi)) {
 let vn = './media/Bienvenido.mp3'
