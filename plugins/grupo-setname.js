98d94f1aca7fee58342904206b286ff0d80edfdf
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-03 15:20:56 -0500

    Update grupo-setname.js

--- plugins/grupo-setname.js
+++ plugins/grupo-setname.js
@@ -4,13 +4,14 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
 if (!text) return conn.reply(m.chat, lenguajeGB['smsNam2'](), fkontak, m)
 //try {
+if (text.length < 25) return conn.reply(m.chat, lenguajeGB['smsNam3'](), fkontak, m)
 let text = args.join` `
 if(!args || !args[0]) {
 //} else {
 conn.groupUpdateSubject(m.chat, text)}
 //conn.sendButton(m.chat, wm, lenguajeGB.smsNam1(), pp, [[lenguajeGB.smsConMenu(), `/menu`]], fkontak, m)}
 //} catch (e) { 
-if (text.length < 25) return conn.reply(m.chat, lenguajeGB['smsNam3'](), fkontak, m)
+
 }
 handler.command = /^(setname|newnombre|nuevonombre)$/i
 handler.group = true
