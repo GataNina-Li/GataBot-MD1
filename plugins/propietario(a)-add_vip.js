d51f44db8954c2fd397384b0555577505c281b71
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-02 22:49:36 -0500

    Update propietario(a)-add_vip.js

--- plugins/propietario(a)-add_vip.js
+++ plugins/propietario(a)-add_vip.js
@@ -3,8 +3,8 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 //let who
 //if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 //else who = m.chat
-let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
-//let res = [];
+let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
+let res = [];
 let user = global.db.data.users[who]
 if (!who) throw `Menciona a la Persona`
 let txt = text.replace('@' + who.split`@`[0], '').trim()
@@ -28,7 +28,7 @@ let tiempo = [ hora1, hora3, hora5, dia1, semana1, mes1]
 let comando = [ 'premium1', `${command[2]}`, `${command[3]}`, `${command[4]}`, `${command[5]}`, `${command[6]}`]
 
 let sections = Object.keys(titulo, nombre, descripción, tiempo, comando).map((v, index) => ({ title: `${titulo[v]}`,
-rows: [{ title: `${nombre[v]}`, description: `${1 + index}. ${descripción[v]}`, rowId: usedPrefix + comando[v] + ' ' + `${'@' + who.split("@")[0]}` + ' ' + txt, }], }))
+rows: [{ title: `${nombre[v]}`, description: `${1 + index}. ${descripción[v]}`, rowId: usedPrefix + comando[v] + ' ' + `${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}` + ' ' + txt, }], }))
 
 const listMessage = {
 text: `*ELIJA EL TIEMPO PREMIUM*
