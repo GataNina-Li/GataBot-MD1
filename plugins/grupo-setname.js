6e430d1c93e20bd7892238765208cc981dd115bc
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-03 15:19:32 -0500

    Update grupo-setname.js

--- plugins/grupo-setname.js
+++ plugins/grupo-setname.js
@@ -3,15 +3,15 @@ let handler  = async (m, { conn, args, text }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
 if (!text) return conn.reply(m.chat, lenguajeGB['smsNam2'](), fkontak, m)
-try {
+//try {
 let text = args.join` `
 if(!args || !args[0]) {
-} else {
+//} else {
 conn.groupUpdateSubject(m.chat, text)}
 //conn.sendButton(m.chat, wm, lenguajeGB.smsNam1(), pp, [[lenguajeGB.smsConMenu(), `/menu`]], fkontak, m)}
-} catch (e) { 
-return conn.reply(m.chat, lenguajeGB['smsNam3'](), fkontak, m)
-}}
+//} catch (e) { 
+if (text.length < 25) return conn.reply(m.chat, lenguajeGB['smsNam3'](), fkontak, m)
+}
 handler.command = /^(setname|newnombre|nuevonombre)$/i
 handler.group = true
 handler.admin = true
