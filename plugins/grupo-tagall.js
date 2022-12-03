025d4ea0b7a9b44b5b98192011efb888b2471a14
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-03 12:35:17 -0500

    𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,34 +1,3 @@
-/*let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let pesan = args.join` `
-let oi = `*ღ 𝙈𝙀𝙉𝙎𝘼𝙅𝙀:* ${pesan}`
-let teks = `╭━〔 *𝙄𝙉𝙑𝙊𝘾𝘼𝙉𝘿𝙊 𝘼𝙇 𝙂𝙍𝙐𝙋𝙊* 〕━⬣\n\n${oi}\n\n`
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-let username = conn.getName(who)
-for (let username of participants) {
-teks += `┃➥ @${username.id.split('@')[0]}\n`}
-teks += `╰━━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
-//conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
-  
-conn.sendHydrated(m.chat, teks, `𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨 | ${wm}`, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙄𝙣𝙫𝙤𝙘𝙖𝙧 𝙤𝙩𝙧𝙖 𝙫𝙚𝙯 📣', `${usedPrefix + command}`],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘', '.menu']
-], m, { mentions: participants.map(a => a.id) })  
-}
-handler.help = ['tagall <mesaje>','invocar <mesaje>']
-handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-handler.botAdmin = true
-handler.admin = true
-handler.group = true
-export default handler */
-
-
-
 let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
@@ -36,14 +5,14 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*ღ 𝙈𝙀𝙉𝙎𝘼𝙅𝙀:* ${pesan}`
-let teks = `╭━〔 *𝙄𝙉𝙑𝙊𝘾𝘼𝙉𝘿𝙊 𝘼𝙇 𝙂𝙍𝙐𝙋𝙊* 〕━⬣\n\n${oi}\n\n`
+let teks = `╭━〔 *${lenguajeGB['smstagaa']()}* 〕━⬣\n\n${oi}\n\n`
 for (let mem of participants) {
-teks += `┃➥ @${mem.id.split('@')[0]}\n`}
+teks += `┃⊹ @${mem.id.split('@')[0]}\n`}
+teks += `┃`
+teks += `┃ ${wm}`
 teks += `╰━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
-handler.help = ['tagall <mesaje>','invocar <mesaje>']
-handler.tags = ['group']
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
 handler.admin = true
 handler.group = true
