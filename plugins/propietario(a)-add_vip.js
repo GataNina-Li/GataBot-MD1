061f261316ac81fa128b7c0b6ac74ab828c9dd6b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-02 22:41:14 -0500

    Update propietario(a)-add_vip.js

--- plugins/propietario(a)-add_vip.js
+++ plugins/propietario(a)-add_vip.js
@@ -28,7 +28,7 @@ let tiempo = [ hora1, hora3, hora5, dia1, semana1, mes1]
 let comando = [ 'premium1', `${command[2]}`, `${command[3]}`, `${command[4]}`, `${command[5]}`, `${command[6]}`]
 
 let sections = Object.keys(titulo, nombre, descripción, tiempo, comando).map((v, index) => ({ title: `${titulo[v]}`,
-rows: [{ title: `${nombre[v]}`, description: `${1 + index}. ${descripción[v]}`, rowId: usedPrefix + comando[v] + ' ' + `${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}` + ' ' + txt, }], })).then(() => {res.push(who);
+rows: [{ title: `${nombre[v]}`, description: `${1 + index}. ${descripción[v]}`, rowId: usedPrefix + comando[v] + ' ' + `${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}` + ' ' + txt, }], })).then(() => {res.push(who)});
 
 const listMessage = {
 text: `*ELIJA EL TIEMPO PREMIUM*
