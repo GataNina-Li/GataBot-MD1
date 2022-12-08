2346f8f0668f17ed219cdf9c3eb72266af34b31a
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-08 16:58:15 -0300

    Add files via upload

---
+++ plugins/chatbot-gata.js
@@ -0,0 +1,15 @@
+let handler  = async (m, { conn }) => {
+let name = conn.getName(m.sender)
+let teks = `
+${pickRandom([` _Hola ${name}_\n\n_Estoy aquí😎_`, `_Si?👀_`, `_q te pica_`, `_que_`, `_q quieres_`, `que paso mirey`, `_q pasa conmigo_`, `_aqui estoy😎_`, `q pasa mi amor`, 'no estoy😸', 'aca estoy🥳'])}
+`.trim()
+conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
+}
+handler.customPrefix = /Gatabot|GataBot-MD|gatabot|al bot|elbot|el bot|haybot/i
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
