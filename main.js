881adfc1e2852687d10b5045ff9d3b08b4c205cd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-12 08:46:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -91,7 +91,12 @@ function clearTmp() {
 const tmp = [tmpdir(), join(__dirname, './tmp')]
 const filename = []
 tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-
+return filename.map(file => {
+const stats = statSync(file)
+if (stats.isFile() && (Date.now() - stats.mtimeMs >= 1000 * 60 * 3)) return unlinkSync(file) // 3 minutes
+return false
+})}
+    
 /*if (!opts['test']) {
 if (global.db) setInterval(async () => {
 if (global.db.data) await global.db.write()
