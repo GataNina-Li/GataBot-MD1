274959d0ed8b86b5eef731de5e7a7644aa46e751
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-12-12 12:49:04 -0300

    Update game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -2,6 +2,8 @@ import fs from 'fs'
 
 let timeout = 6000
 let poin = 500
+let time = global.db.data.users[m.sender].lastwork + 600000
+if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `*Ya jugo! espere unos ${msToTime(time - new Date())} para volver a trabajar!!*`
 
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
@@ -35,3 +37,15 @@ handler.tags = ['game']
 handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i
 
 export default handler
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+
+return minutes + " m " + seconds + " s " 
+}
