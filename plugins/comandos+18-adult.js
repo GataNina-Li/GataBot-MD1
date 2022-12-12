07a12f4e340b5e85e3fc0c5f2df5cafc2b8af554
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2022-12-12 11:53:28 -0300

    Update comandos+18-adult.js

--- plugins/comandos+18-adult.js
+++ plugins/comandos+18-adult.js
@@ -112,7 +112,7 @@ let json = res.data
 let url = json.url
 conn.sendButton(m.chat, `_${command}_`.trim(), wm, url, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `/${command}`]], m)}
 if (command == 'fut4') {
-let res = await axios("https://meme-api.herokuapp.com/gimme/yuri")
+let res = await axios("https://api.rule34.xxx/index.php?page=dapi&s=tag&q=index&tags=futanari&json=1")
 let json = res.data
 let url = json.url
 conn.sendButton(m.chat, `_${command}_`.trim(), wm, url, [['🥵 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏', `/${command}`]], m)}
