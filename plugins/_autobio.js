f91d55b2dd034689bb8e3bdc74ab1e8924470e3b
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-15 21:41:15 -0300

    Add files via upload

---
+++ plugins/_autobio.js
@@ -0,0 +1,28 @@
+
+let handler = m => m
+handler.all = async function (m) {
+	let setting = global.db.data.settings[this.user.jid]
+	
+let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+		let bio = `GataBot-MD 🐈 | Activo ✅️:  ${muptime} | usa el comando #estado para saber del mi y #menu para vez mi lista del comando! 😸`
+		await this.updateProfileStatus(bio).catch(_ => _)
+		setting.status = new Date() * 1
+
+}
+export default handler
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, ' Días ️', h, ' Horas ', m, ' Minutos'].map(v => v.toString().padStart(2, 0)).join('')
+}
