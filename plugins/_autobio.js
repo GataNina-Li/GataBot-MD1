740b9324ff91c18eb52572d5ed26ce036233b18d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-17 01:04:42 -0400

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -21,5 +21,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' » ', h, ':', m, ':', s].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s)'].map(v => v.toString().padStart(2, 0)).join('')
 }
