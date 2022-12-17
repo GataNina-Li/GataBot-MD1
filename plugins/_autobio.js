571ab94e7ad01180948921fe8d8f78d1d7283a85
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-12-16 23:42:26 -0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -3,11 +3,11 @@ handler.all = async function (m) {
 let setting = global.db.data.settings[this.user.jid]
 	
 let _muptime
-if (process.send) {	
+if (process.send) {
 process.send('uptime')
-_muptime = await new Promise(resolve => { 
+_muptime = await new Promise(resolve => {
 process.once('message', resolve)
-setTimeout(resolve, 1000)}) * 1000 //5 min
+setTimeout(resolve, 1000)}) * 1000
 }
 let muptime = clockString(_muptime)
 let bio = `${global.packname} ║ ✅ ${muptime} ║ #estado #menu #jadibot`
@@ -21,6 +21,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' d ️', h, ' h ', m, ' m'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' » ', h, ':', m, ':', s].map(v => v.toString().padStart(2, 0)).join('')
 }
-
